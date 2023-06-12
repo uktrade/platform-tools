4bcd7734e2ce58965958178758f577232e486140
Yusuf <yusuf@madetech.com>
2023-06-12 17:08:34 +0100

    Add `--version` option

---
+++ tests/test_copilot_helper.py
@@ -0,0 +1,18 @@
+import re
+
+from click.testing import CliRunner
+
+from copilot_helper import cli
+
+
+class TestCopilotHelperCli:
+    def test_check_version(self):
+        runner = CliRunner()
+
+        result = runner.invoke(cli, ["--version"])
+
+        name, version = result.output.split()
+
+        assert result.exit_code == 0
+        assert name == "dbt-copilot-tools"
+        assert (re.compile(r"^\d+(\.\d+){2,}$")).match(version)
