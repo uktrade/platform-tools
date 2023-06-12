d63d80e0645532fbfad455ba966648f5df8e8edc
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-06-12 16:17:43 +0100

    Patch valid_checks

--- commands/check_cloudformation.py
+++ commands/check_cloudformation.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
-from pathlib import Path
-
 import click
-
-BASE_DIR = Path(__file__).parent.parent
-
 
 @click.group()
 def copilot():
     pass
+
+
+def valid_checks():
+    return ["all", "lint"]
 
 
 @copilot.command()
@@ -20,9 +19,9 @@ def check_cloudformation(check):
     Valid checks are: all and lint
     """
 
-    valid_checks = ["all", "lint"]
-
-    if not valid_checks.__contains__(check):
+    if not valid_checks().__contains__(check):
         raise ValueError(f"Invalid value ({check}) for 'CHECK'")
 
     click.echo(f"\n>>> Running checks: {check}\n")
+
+    # Call the method
--- tests/test_check_cloudformation.py
+++ tests/test_check_cloudformation.py
@@ -1,7 +1,9 @@
-import pytest
+from unittest.mock import patch
+
 from click.testing import CliRunner
 
-from commands.check_cloudformation import check_cloudformation as check_cloudformation_command
+import commands
+from commands.check_cloudformation import check_cloudformation as check_cloudformation_command, valid_checks
 
 
 class TestCheckCommand:
@@ -21,3 +23,18 @@ class TestCheckCommand:
         assert result.exit_code == 1
         assert isinstance(result.exception, ValueError)
         assert str(result.exception).__contains__("Invalid value (does-not-exist) for 'CHECK'")
+
+    @patch("commands.check_cloudformation.valid_checks")
+    def test_runs_specific_check_when_given_check(self, valid_checks_mock):
+        runner = CliRunner()
+
+        something_else = ["all", "one"]
+
+        valid_checks_mock.return_value = something_else
+
+        result = runner.invoke(check_cloudformation_command, ["one"])
+
+        assert result.exit_code == 0
+        assert result.output.__contains__("Running checks: one")
+
+    # Todo: test_runs_all_checks_when_given_all
