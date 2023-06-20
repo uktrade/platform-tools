38a97a56ec1987eaf7a21c9be874df73952d9b7c
Yusuf <47318342+yusufsheiqh@users.noreply.github.com>
2023-06-20 11:16:03 +0100

    Add `Dockerfile.test` documentation (#74)

--- copilot_helper.py
+++ copilot_helper.py
@@ -8,6 +8,7 @@ from commands.bootstrap_cli import bootstrap as bootstrap_commands
 from commands.codebuild_cli import codebuild as codebuild_commands
 from commands.copilot_cli import copilot as copilot_commands
 from commands.dns_cli import domain as domain_commands
+from commands.waf_cli import waf as waf_commands
 
 
 @click.group()
@@ -23,6 +24,7 @@ cli.add_command(bootstrap_commands)
 cli.add_command(copilot_commands)
 cli.add_command(codebuild_commands)
 cli.add_command(domain_commands)
+cli.add_command(waf_commands)
 
 if __name__ == "__main__":
     cli()
