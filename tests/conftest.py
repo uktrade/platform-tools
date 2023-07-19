9a00d8eb0a2d30c0661b1dbf0494e7c81b2bcb85
Yusuf <47318342+yusufsheiqh@users.noreply.github.com>
2023-07-19 17:07:55 +0100

    Add `templates` folder to `dbt-copilot-tools` package (#104)

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,7 +19,7 @@ yaml.add_multi_constructor("!", lambda loader, suffix, node: None, Loader=yaml.S
 @pytest.fixture
 def fakefs(fs):
     """Mock file system fixture with the templates and schemas dirs retained."""
-    fs.add_real_directory(BASE_DIR / "templates")
+    fs.add_real_directory(BASE_DIR / "commands/templates")
     fs.add_real_directory(BASE_DIR / "schemas")
     fs.add_real_file(BASE_DIR / "storage-plans.yml")
     fs.add_real_file(BASE_DIR / "default-storage.yml")
