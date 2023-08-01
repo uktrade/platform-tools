fe4c4bf490b28de64740c3118c3cfc5db7529057
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-08-01 15:24:50 +0100

    More real_line_breaks()

--- tests/test_bootstrap_cli.py
+++ tests/test_bootstrap_cli.py
@@ -116,10 +116,10 @@ def test_make_config(tmp_path):
         assert workspace.read() == "application: test-app"
 
     with open(str(tmp_path / "copilot/environments/test/manifest.yml"), "rb") as test:
-        assert test.read() == test_environment_manifest
+        assert real_line_breaks(test.read()) == real_line_breaks(test_environment_manifest)
 
     with open(str(tmp_path / "copilot/environments/production/manifest.yml"), "rb") as production:
-        assert production.read() == production_environment_manifest
+        assert real_line_breaks(production.read()) == real_line_breaks(production_environment_manifest)
 
     with open(str(tmp_path / "copilot/test-service/manifest.yml"), "rb") as service:
         assert real_line_breaks(service.read()) == real_line_breaks(test_service_manifest)
