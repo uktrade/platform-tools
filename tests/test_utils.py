32e9ca3486229c3c5d5d5a9333994163c39e8307
Yusuf <yusuf@madetech.com>
2023-08-09 16:06:21 +0100

    Update unit test for `set_ssm_param`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -54,4 +54,12 @@ def test_set_ssm_param(alias_session, aws_credentials):
 
     result = mocked_ssm.get_parameters_by_path(**params)["Parameters"][0]
 
-    assert result["Name"] == "/copilot/test-application/development/secrets/TEST_SECRET"
+    expected_response = {
+        "ARN": "arn:aws:ssm:eu-west-2:123456789012:parameter/copilot/test-application/development/secrets/TEST_SECRET",
+        "Name": "/copilot/test-application/development/secrets/TEST_SECRET",
+        "Type": "SecureString",
+        "Value": "random value",
+    }
+
+    # assert result is a superset of expected_response
+    assert result.items() >= expected_response.items()
