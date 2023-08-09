750e331aca2eb725d314e8702938edd86f75cdd7
Yusuf <yusuf@madetech.com>
2023-08-09 15:56:12 +0100

    Add unit test for `set_ssm_param` utils method

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ from moto import mock_ssm
 
 from commands.utils import check_aws_conn
 from commands.utils import get_ssm_secrets
+from commands.utils import set_ssm_param
 
 
 def test_check_aws_conn_profile_not_configured(capsys):
@@ -28,3 +29,29 @@ def test_get_ssm_secrets():
     result = get_ssm_secrets("test-application", "development")
 
     assert result == [("/copilot/test-application/development/secrets/TEST_SECRET", "test value")]
+
+
+@mock_ssm
+def test_set_ssm_param(alias_session, aws_credentials):
+    mocked_ssm = boto3.client("ssm")
+
+    set_ssm_param(
+        "test-application",
+        "development",
+        "/copilot/test-application/development/secrets/TEST_SECRET",
+        "random value",
+        False,
+        False,
+        "Created for testing purposes.",
+    )
+
+    params = dict(
+        Path="/copilot/test-application/development/secrets/",
+        Recursive=False,
+        WithDecryption=True,
+        MaxResults=10,
+    )
+
+    result = mocked_ssm.get_parameters_by_path(**params)["Parameters"][0]
+
+    assert result["Name"] == "/copilot/test-application/development/secrets/TEST_SECRET"
