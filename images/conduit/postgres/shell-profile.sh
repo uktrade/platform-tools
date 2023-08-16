d95165955213991dae2701a97ea1566522fc5343
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2023-08-16 09:15:35 +0100

    Add an intro message confirming the database host and name

--- images/conduit/postgres/shell-profile.sh
+++ images/conduit/postgres/shell-profile.sh
@@ -1,3 +1,7 @@
+echo "Welcome to the client container for postgres services."
+echo $DB_SECRET | jq -rc '"Connecting to database \"\(.dbname)\" on \"\(.host)\""'
+echo
+
 psql "$(echo $DB_SECRET | jq -rc '"postgres://\(.username):\(.password)@\(.host):\(.port)/\(.dbname)"')"
 
 if [[ `ps -e -o pid,comm | grep 'psql' | wc -l` == "0" ]]; then
