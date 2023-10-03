2248ceba685095ebf84319a4b83e8a00aaa7fab0
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-10-03 14:57:39 +0100

    WIP

--- dbt_copilot_helper/commands/svc.py
+++ dbt_copilot_helper/commands/svc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
+import re
 
-import subprocess
-
+import boto3
 import click
 
 from dbt_copilot_helper.utils.click import ClickDocOptGroup
@@ -23,11 +23,35 @@ def svc():
 def deploy(env, name, image_tag):
     """Deploy image tag to a service, default to image tagged latest."""
 
-    # Todo: If --image-tag is unset or latest, figure out the image tag from AWS ECR or blow up
+    def get_commit_tag_for_latest_image():
+        ecr_client = boto3.client("ecr")
+        response = ecr_client.describe_images(
+            registryId="854321987474",
+            repositoryName="demodjango",
+            imageIds=[
+                {"imageTag": "latest"},
+            ],
+        )
+        print(response)
+
+        image_tags = response["imageDetails"][0]["imageTags"]
+        print(image_tags)
+        filtered = filter(lambda tag: re.match("([a-f0-9]7)", tag), image_tags)
+        print(list(filtered))
+
+        # breakpoint()
+        image_tag = re.findall("([a-f0-9]*)", image_tags)
+
+        return image_tag
+
+    if image_tag == "latest":
+        image_tag = get_commit_tag_for_latest_image()
+
+    print(image_tag)
 
     command = f"IMAGE_TAG={image_tag} copilot svc deploy --env {env} --name {name}"
     print("Running: ", command)
-    subprocess.call(
-        command,
-        shell=True,
-    )
+    # subprocess.call(
+    #     command,
+    #     shell=True,
+    # )
