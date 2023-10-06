26604d04580856165ed0276e461e5828b785461b
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2023-10-06 11:57:02 +0100

    S3 object custom resource fails when fields are missing
    Co-Authored-By: Ant <anthony.roy@digital.trade.gov.uk>

---
+++ dbt_copilot_helper/custom_resources/s3_object.py
@@ -0,0 +1,113 @@
+import json
+from urllib import request
+
+import boto3
+
+s3_client = boto3.client("s3")
+
+# Initial code taken from "https://github.com/awslabs/aws-cloudformation-templates/blob
+# /a11722da8379dd52726ecfcd552f7983e9bb563f/aws/services/CloudFormation/MacrosExamples/S3Objects
+# /lambda/resource.py"
+
+
+def send_response(event, context, status, message):
+    bucket = event["ResourceProperties"].get("Target", {}).get("Bucket")
+    key = event["ResourceProperties"].get("Target", {}).get("Key")
+
+    body = json.dumps(
+        {
+            "Status": status,
+            "Reason": message,
+            "StackId": event["StackId"],
+            "RequestId": event["RequestId"],
+            "LogicalResourceId": event["LogicalResourceId"],
+            "PhysicalResourceId": f"s3://{bucket}/{key}",
+            "Data": {
+                "Bucket": bucket,
+                "Key": key,
+            },
+        }
+    ).encode()
+
+    send = request.Request(event["ResponseURL"], data=body)
+    send.get_method = lambda: "PUT"
+    request.urlopen(send)
+
+    #
+    # request = Request(, data=body)
+    # request.add_header('Content-Type', '')
+    # request.add_header('Content-Length', str(len(body)))
+    # request.get_method = lambda: 'PUT'
+    #
+    # opener = build_opener(HTTPHandler)
+    # opener.open(request)
+
+
+def handler(event, context):
+    properties = event["ResourceProperties"]
+    required_properties = [
+        "CopilotApplication",
+        "CopilotEnvironment",
+        "S3Bucket",
+        "S3ObjectBody",
+        "S3ObjectKey",
+    ]
+    missing_properties = [p for p in required_properties if p not in properties]
+
+    if missing_properties:
+        missing_properties.sort()
+        return send_response(
+            event, context, "FAILED", f"Missing required properties: {missing_properties}"
+        )
+
+    # if "Target" not in properties or all(prop not in properties for prop in ["Body", "Base64Body", "Source"]):
+    #     return sendResponse(event, context, "FAILED", "Missing required parameters")
+    #
+    # target = properties["Target"]
+    #
+    # if request in ("Create", "Update"):
+    #     if "Body" in properties:
+    #         target.update({
+    #             "Body": properties["Body"],
+    #         })
+    #
+    #         s3_client.put_object(**target)
+    #
+    #     elif "Base64Body" in properties:
+    #         try:
+    #             body = base64.b64decode(properties["Base64Body"])
+    #         except:
+    #             return sendResponse(event, context, "FAILED", "Malformed Base64Body")
+    #
+    #         target.update({
+    #             "Body": body
+    #         })
+    #
+    #         s3_client.put_object(**target)
+    #
+    #     elif "Source" in properties:
+    #         source = properties["Source"]
+    #
+    #         s3_client.copy_object(
+    #             CopySource=source,
+    #             Bucket=target["Bucket"],
+    #             Key=target["Key"],
+    #             MetadataDirective="COPY",
+    #             TaggingDirective="COPY",
+    #             ACL=target["ACL"],
+    #         )
+    #
+    #     else:
+    #         return sendResponse(event, context, "FAILED", "Malformed body")
+    #
+    #     return sendResponse(event, context, "SUCCESS", "Created")
+    #
+    # if request == "Delete":
+    #     s3_client.delete_object(
+    #         Bucket=target["Bucket"],
+    #         Key=target["Key"],
+    #     )
+    #
+    #     return sendResponse(event, context, "SUCCESS", "Deleted")
+    #
+    # return sendResponse(event, context, "FAILED", "Unexpected: {}".format(request))
---
+++ tests/custom_resources/test_s3_object.py
@@ -0,0 +1,72 @@
+import json
+import unittest
+from unittest.mock import patch
+
+from parameterized import parameterized
+
+from dbt_copilot_helper.custom_resources.s3_object import handler
+
+
+class TestS3ObjectCustomResource(unittest.TestCase):
+    def setUp(self):
+        self._resource_properties = {
+            "Name": "app-test-s3-objects",
+            "CopilotApplication": "app",
+            "CopilotEnvironment": "test",
+            "S3Bucket": "bucket-name",
+            "S3ObjectKey": "object-with-contents",
+            "S3ObjectBody": "test content",
+        }
+
+        self._create_event = {
+            "RequestType": "Create",
+            "LogicalResourceId": "servicePrefixS3Object0",
+            "StackId": "1906d5fb-e2b3-426c-b308-14dd13efd918",
+            "RequestId": "ccccb225-37f6-4959-bedb-f0cc8502a95f",
+            "ResponseURL": "https://example.com/cf-response",
+            "ResourceProperties": self._resource_properties,
+        }
+
+    @parameterized.expand(
+        [
+            (["CopilotApplication"],),
+            (["CopilotEnvironment"],),
+            (["S3Bucket"],),
+            (["S3ObjectKey"],),
+            (["S3ObjectBody"],),
+            (["CopilotApplication", "CopilotEnvironment"],),
+            (["S3Bucket", "S3ObjectKey", "S3ObjectBody"],),
+            (
+                [
+                    "CopilotApplication",
+                    "CopilotEnvironment",
+                    "S3Bucket",
+                    "S3ObjectKey",
+                    "S3ObjectBody",
+                ],
+            ),
+        ]
+    )
+    @patch("urllib.request.urlopen", return_value=None)
+    def test_creating_an_object_without_required_parameters_sends_failure_status(
+        self, missing_properties, urlopen
+    ):
+        with_missing_keys = self._resource_properties.copy()
+        missing_properties.sort()
+
+        for missing_key in missing_properties:
+            del with_missing_keys[missing_key]
+
+        event = self._create_event.copy()
+        event["ResourceProperties"] = with_missing_keys
+        handler(event, {})
+        sent_request = urlopen.call_args_list[0].args[0]
+        sent_body = json.loads(sent_request.data.decode())
+
+        self.assertEqual("https://example.com/cf-response", sent_request.full_url)
+        self.assertEqual("FAILED", sent_body["Status"])
+        self.assertEqual(f"Missing required properties: {missing_properties}", sent_body["Reason"])
+
+
+if __name__ == "__main__":
+    unittest.main()
