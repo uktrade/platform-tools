8dfdeb07c1d212b17e14b54506ae2ab0e69ad097
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2023-10-10 15:00:36 +0100

    Refactoring the svc deploy command

--- dbt_copilot_helper/commands/svc.py
+++ dbt_copilot_helper/commands/svc.py
@@ -28,6 +28,54 @@ def svc():
 def deploy(env, name, image_tag):
     """Deploy image tag to a service, defaults to image tagged latest."""
 
+    repository_name = validate_service_manifest_and_return_repository(name)
+
+    image_tags = get_all_tags_for_image(image_tag, repository_name)
+
+    if image_tag == "latest":
+        image_tag = get_commit_tag_for_latest_image(image_tags)
+
+    command = f"IMAGE_TAG={image_tag} copilot svc deploy --env {env} --name {name}"
+    click.echo(f"Running: {command}")
+    subprocess.call(
+        command,
+        shell=True,
+    )
+
+
+def get_all_tags_for_image(image_tag_needle, repository_name):
+    registry_id = boto3.client("sts").get_caller_identity()["Account"]
+    ecr_client = boto3.client("ecr")
+    try:
+        response = ecr_client.describe_images(
+            registryId=registry_id,
+            repositoryName=repository_name,
+            imageIds=[
+                {"imageTag": image_tag_needle},
+            ],
+        )
+        return response["imageDetails"][0]["imageTags"]
+    except ecr_client.exceptions.ImageNotFoundException:
+        click.secho(
+            f"""No image exists with the tag "{image_tag_needle}".""",
+            fg="red",
+        )
+        exit(1)
+
+
+def get_commit_tag_for_latest_image(image_tags_haystack):
+    try:
+        filtered = [tag for tag in image_tags_haystack if re.match("(commit-[a-f0-9]{7,32})", tag)]
+        return filtered[0]
+    except IndexError:
+        click.secho(
+            """The image tagged "latest" does not have a commit tag.""",
+            fg="red",
+        )
+        exit(1)
+
+
+def validate_service_manifest_and_return_repository(name):
     service_manifest = Path("copilot") / name / "manifest.yml"
     try:
         service_name_in_manifest = get_service_name_from_manifest(service_manifest)
@@ -51,46 +99,4 @@ def deploy(env, name, image_tag):
         )
         exit(1)
 
-    def get_all_tags_for_image(image_tag_needle):
-        registry_id = boto3.client("sts").get_caller_identity()["Account"]
-        ecr_client = boto3.client("ecr")
-        try:
-            response = ecr_client.describe_images(
-                registryId=registry_id,
-                repositoryName=repository_name,
-                imageIds=[
-                    {"imageTag": image_tag_needle},
-                ],
-            )
-            return response["imageDetails"][0]["imageTags"]
-        except ecr_client.exceptions.ImageNotFoundException:
-            click.secho(
-                f"""No image exists with the tag "{image_tag_needle}".""",
-                fg="red",
-            )
-            exit(1)
-
-    def get_commit_tag_for_latest_image(image_tags_haystack):
-        try:
-            filtered = [
-                tag for tag in image_tags_haystack if re.match("(commit-[a-f0-9]{7,32})", tag)
-            ]
-            return filtered[0]
-        except IndexError:
-            click.secho(
-                """The image tagged "latest" does not have a commit tag.""",
-                fg="red",
-            )
-            exit(1)
-
-    image_tags = get_all_tags_for_image(image_tag)
-
-    if image_tag == "latest":
-        image_tag = get_commit_tag_for_latest_image(image_tags)
-
-    command = f"IMAGE_TAG={image_tag} copilot svc deploy --env {env} --name {name}"
-    click.echo(f"Running: {command}")
-    subprocess.call(
-        command,
-        shell=True,
-    )
+    return repository_name
