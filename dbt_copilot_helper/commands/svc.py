48a205714435d28c2926f632c41c8794e969da05
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2023-10-10 13:59:28 +0100

    WIP

--- dbt_copilot_helper/commands/svc.py
+++ dbt_copilot_helper/commands/svc.py
@@ -29,7 +29,6 @@ def deploy(env, name, image_tag):
     """Deploy image tag to a service, defaults to image tagged latest."""
 
     service_manifest = Path("copilot") / name / "manifest.yml"
-
     try:
         service_name_in_manifest = get_service_name_from_manifest(service_manifest)
         if service_name_in_manifest != name:
@@ -38,17 +37,23 @@ def deploy(env, name, image_tag):
                 fg="red",
             )
             exit(1)
-
     except FileNotFoundError:
         click.secho(
             f"Service manifest for {name} could not be found at path {service_manifest}", fg="red"
         )
         exit(1)
 
+    repository_name = get_repository_name_from_manifest(service_manifest)
+    if service_manifest not in repository_name:
+        click.secho(
+            f"The image location does not contain the service name ({name}) in the service manifest.",
+            fg="red",
+        )
+        exit(1)
+
     def get_all_tags_for_image(image_tag_needle):
         registry_id = boto3.client("sts").get_caller_identity()["Account"]
         ecr_client = boto3.client("ecr")
-        repository_name = get_repository_name_from_manifest(service_manifest)
         try:
             response = ecr_client.describe_images(
                 registryId=registry_id,
