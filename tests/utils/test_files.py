2a356b932ec7a369558fe95105af194fa609c0f3
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2023-10-16 14:53:34 +0100

    Created happy path tests for mkfile

--- tests/utils/test_files.py
+++ tests/utils/test_files.py
@@ -4,6 +4,7 @@ import yaml
 from dbt_copilot_helper.utils.files import BOOTSTRAP_SCHEMA
 from dbt_copilot_helper.utils.files import PIPELINES_SCHEMA
 from dbt_copilot_helper.utils.files import load_and_validate_config
+from dbt_copilot_helper.utils.files import mkfile
 from tests.conftest import FIXTURES_DIR
 
 
@@ -25,3 +26,30 @@ def test_load_and_validate_config_valid_file(schema, yaml_file):
         conf = yaml.safe_load(fd)
 
     assert validated == conf
+
+
+@pytest.mark.parametrize(
+    "file_exists, overwrite, expected",
+    [
+        (False, False, "File test_file.txt created"),
+        (False, True, "File test_file.txt created"),
+        (True, True, "File test_file.txt overwritten"),
+    ],
+)
+def test_mkfile_creates_or_overrides_the_file(tmp_path, file_exists, overwrite, expected):
+    filename = "test_file.txt"
+    file_path = tmp_path / filename
+    if file_exists:
+        file_path.touch()
+
+    content = "The content"
+
+    message = mkfile(tmp_path, filename, content, overwrite)
+
+    assert file_path.exists()
+    assert file_path.read_text() == content
+    assert message == expected
+
+
+def test_mkfile_fails_with_exception_if_file_already_exists_but_override_is_false(tmp_path):
+    pass
