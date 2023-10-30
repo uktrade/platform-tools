57ff97473add357e642b01a7db43210ab56b5d28
Yusuf <yusuf.sheikh@digital.trade.gov.uk>
2023-10-30 13:57:25 +0000

    Add command to setup application codebase

--- copilot_helper.py
+++ copilot_helper.py
@@ -8,6 +8,7 @@ from dbt_copilot_helper.commands.bootstrap import bootstrap as bootstrap_command
 from dbt_copilot_helper.commands.check_cloudformation import (
     check_cloudformation as check_cloudformation_command,
 )
+from dbt_copilot_helper.commands.codebase import codebase as codebase_commands
 from dbt_copilot_helper.commands.codebuild import codebuild as codebuild_commands
 from dbt_copilot_helper.commands.conduit import conduit as conduit_commands
 from dbt_copilot_helper.commands.config import config as config_commands
@@ -30,6 +31,7 @@ def copilot_helper():
 
 copilot_helper.add_command(bootstrap_commands)
 copilot_helper.add_command(check_cloudformation_command)
+copilot_helper.add_command(codebase_commands)
 copilot_helper.add_command(codebuild_commands)
 copilot_helper.add_command(conduit_commands)
 copilot_helper.add_command(config_commands)
---
+++ dbt_copilot_helper/commands/codebase.py
@@ -0,0 +1,58 @@
+import subprocess
+from pathlib import Path
+
+import click
+import requests
+import yaml
+
+from dbt_copilot_helper.utils.click import ClickDocOptGroup
+from dbt_copilot_helper.utils.files import mkfile
+from dbt_copilot_helper.utils.template import setup_templates
+from dbt_copilot_helper.utils.versioning import (
+    check_copilot_helper_version_needs_update,
+)
+
+
+@click.group(chain=True, cls=ClickDocOptGroup)
+def codebase():
+    """Codebase commands."""
+    check_copilot_helper_version_needs_update()
+
+
+@codebase.command()
+def prepare():
+    templates = setup_templates()
+
+    builder_configuration_url = "https://raw.githubusercontent.com/uktrade/ci-image-builder/main/image_builder/configuration/builder_configuration.yml"
+    builder_configuration_response = requests.get(builder_configuration_url)
+    builder_configuration_content = yaml.safe_load(
+        builder_configuration_response.content.decode("utf-8")
+    )
+    builder_versions = next(
+        (
+            item
+            for item in builder_configuration_content["builders"]
+            if item["name"] == "paketobuildpacks/builder-jammy-base"
+        ),
+        None,
+    )
+    builder_version = max(x["version"] for x in builder_versions["versions"])
+
+    repository = (
+        subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True)
+        .stdout.split("/")[-1]
+        .strip()
+    )
+
+    config_contents = templates.get_template(f".copilot/config.yml").render(
+        repository=repository, builder_version=builder_version
+    )
+
+    Path("./.copilot/phases").mkdir(parents=True, exist_ok=True)
+
+    click.echo(mkfile(Path("."), ".copilot/config.yml", config_contents, overwrite=True))
+
+    for phase in ["build", "install", "post_build", "pre_build"]:
+        phase_contents = templates.get_template(f".copilot/phases/{phase}.sh").render()
+
+        click.echo(mkfile(Path("./.copilot"), f"phases/{phase}.sh", phase_contents, overwrite=True))
