638675261739d03737f6d83b13d44d4081a3cd05
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2023-11-06 10:14:16 +0000

    Extract get_application_name() function

--- dbt_copilot_helper/commands/pipeline.py
+++ dbt_copilot_helper/commands/pipeline.py
@@ -5,16 +5,15 @@ from os import makedirs
 from pathlib import Path
 
 import click
-import yaml
 from yaml.parser import ParserError
 
+from dbt_copilot_helper.utils.application import get_application_name
 from dbt_copilot_helper.utils.aws import get_codestar_connection_arn
 from dbt_copilot_helper.utils.click import ClickDocOptGroup
 from dbt_copilot_helper.utils.files import load_and_validate_config
 from dbt_copilot_helper.utils.files import mkfile
 from dbt_copilot_helper.utils.messages import abort_with_error
 from dbt_copilot_helper.utils.template import setup_templates
-from dbt_copilot_helper.utils.validation import BOOTSTRAP_SCHEMA
 from dbt_copilot_helper.utils.validation import PIPELINES_SCHEMA
 from dbt_copilot_helper.utils.versioning import (
     check_copilot_helper_version_needs_update,
@@ -33,7 +32,7 @@ def generate():
     pipelines."""
     templates = setup_templates()
 
-    app_name = _get_application_name()
+    app_name = get_application_name()
 
     pipeline_config = _safe_load_config("pipelines.yml", PIPELINES_SCHEMA)
 
@@ -141,25 +140,3 @@ def _get_git_remote():
     _, repo = git_repo.split("@")[1].split(":")
 
     return re.sub(r".git$", "", repo)
-
-
-def _get_application_name():
-    app_name = None
-
-    try:
-        app_config = load_and_validate_config("bootstrap.yml", BOOTSTRAP_SCHEMA)
-        app_name = app_config["app"]
-    except (FileNotFoundError, ParserError):
-        pass
-
-    try:
-        if app_name is None:
-            app_config = yaml.safe_load(Path("copilot/.workspace").read_text())
-            app_name = app_config["application"]
-    except (FileNotFoundError, ParserError):
-        pass
-
-    if app_name is None:
-        abort_with_error("No valid bootstrap.yml or copilot/.workspace file found")
-
-    return app_name
