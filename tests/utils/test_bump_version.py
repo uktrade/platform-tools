ebaf0df48e8a519cc98e7e30641611d561c0f521
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2023-11-10 10:21:30 +0000

    Added bump version hook

--- tests/utils/test_bump_version.py
+++ tests/utils/test_bump_version.py
@@ -2,49 +2,66 @@ import pytest
 
 from tests.copilot_helper.conftest import BASE_DIR
 from utils.bump_version import bump_version_if_required
-from utils.bump_version import get_next_version
 from utils.bump_version import get_pyproject_version
+from utils.bump_version import version_should_be_bumped
 
 PYPROJECT_CONTENT = """
 [tool.poetry]
 name = "dbt-copilot-tools"
-version = "1.2.3"
+version = "%s"
 """
 
 
 def test_get_pyproject_version(fs):
-    fs.create_file(f"{BASE_DIR}/pyproject.toml", contents=PYPROJECT_CONTENT)
+    fs.create_file(f"{BASE_DIR}/pyproject.toml", contents=PYPROJECT_CONTENT % "1.2.3")
     version = get_pyproject_version()
 
     assert version == "1.2.3"
 
 
 @pytest.mark.parametrize(
-    "versions, expected",
+    "version, bump_version, expected_version,expected_return_code",
     [
-        (["1.1.1", "1.1.3"], "1.2.3"),
-        (["1.1.1", "1.1.3", "1.2.2"], "1.2.3"),
-        (["1.1.1", "1.1.3", "1.2.3"], "1.2.4"),
-        (["1.1.1", "1.2.3", "1.2.4"], "1.2.5"),
+        ("1.2.3", True, "1.2.5", 1),
+        ("1.2.3", False, "1.2.4", 1),
+        ("1.2.4", True, "1.2.5", 1),
+        ("1.2.4", False, "1.2.4", 0),
+        ("1.3.0", True, "1.3.0", 0),
+        ("1.3.0", False, "1.3.0", 0),
     ],
 )
-def test_get_next_version(versions, expected):
-    assert get_next_version("1.2.3", versions) == expected
+def test_bump_version_if_required(
+    version, bump_version, expected_version, expected_return_code, fs
+):
+    versions = ["1.1.1", "1.1.2", "1.2.3", "1.2.4"]
+    fs.create_file(f"{BASE_DIR}/pyproject.toml", contents=PYPROJECT_CONTENT % version)
 
-
-@pytest.mark.parametrize(
-    "versions, expected_version,expected_return_code",
-    [
-        (["1.1.1", "1.1.3"], "1.2.3", 0),
-        (["1.1.1", "1.1.3", "1.2.2"], "1.2.3", 0),
-        (["1.1.1", "1.1.3", "1.2.3"], "1.2.4", 1),
-        (["1.1.1", "1.2.3", "1.2.4"], "1.2.5", 1),
-    ],
-)
-def test_bump_version_if_required(versions, expected_version, expected_return_code, fs):
-    fs.create_file(f"{BASE_DIR}/pyproject.toml", contents=PYPROJECT_CONTENT)
-
-    return_code = bump_version_if_required(versions)
+    return_code = bump_version_if_required(versions, bump_version)
 
     assert return_code == expected_return_code
     assert get_pyproject_version() == expected_version
+
+
+@pytest.mark.parametrize(
+    "files, bump_expected",
+    [
+        ([], False),
+        (["dbt_copilot_helper/utils/aws.py"], True),
+        (["dbt_copilot_helper/templates/env/manifest.yml"], True),
+        (["dbt_copilot_helper/README.md"], False),
+        (["README.md"], False),
+        (["pyproject.toml"], True),
+        (["tests/utils/test_check_pypi.py"], False),
+        (
+            [
+                "dbt_copilot_helper/utils/aws.py",
+                "dbt_copilot_helper/templates/env/manifest.yml",
+                "dbt_copilot_helper/README.md",
+                "tests/utils/test_check_pypi.py",
+            ],
+            True,
+        ),
+    ],
+)
+def test_bump_version(files, bump_expected):
+    assert version_should_be_bumped(files) == bump_expected
--- utils/bump_version.py
+++ utils/bump_version.py
@@ -1,7 +1,10 @@
+import subprocess
 from pathlib import Path
 
 import semver
 import tomlkit
+
+from utils.check_pypi import get_releases
 
 
 def get_pyproject_version():
@@ -11,17 +14,19 @@ def get_pyproject_version():
         return project["tool"]["poetry"]["version"]
 
 
-def get_next_version(version, versions):
+def _get_next_version(version, versions, bump_version):
     sorted_versions = sorted([semver.Version.parse(v) for v in versions])
     current_version = sorted_versions[-1]
     if semver.Version.parse(version) > current_version:
         return version
-    return str(current_version.bump_patch())
+    if bump_version:
+        return str(current_version.bump_patch())
+    return str(current_version)
 
 
-def bump_version_if_required(versions):
+def bump_version_if_required(versions, bump_version):
     pyproject_version = get_pyproject_version()
-    correct_version = get_next_version(pyproject_version, versions)
+    correct_version = _get_next_version(pyproject_version, versions, bump_version)
 
     if correct_version == pyproject_version:
         return 0
@@ -38,6 +43,32 @@ def bump_version_if_required(versions):
     return 1
 
 
+def version_should_be_bumped(files):
+    for f in files:
+        filepath = Path(f)
+        if filepath.name == "README.md":
+            continue
+        parents = filepath.parents
+        # parents[-1] is always "." and the list is in the reverse order from what you'd expect.
+        if len(parents) > 1 and parents[-2].name == "tests":
+            continue
+
+        return True
+
+    return False
+
+
+def get_changed_files():
+    merge_base = subprocess.run(
+        ["git", "merge-base", "HEAD", "main"], capture_output=True, text=True
+    ).stdout.strip()
+    changed_files = subprocess.run(
+        ["git", "diff", "--name-only", merge_base], capture_output=True, text=True
+    ).stdout.strip()
+
+    return changed_files.split("\n")
+
+
 if __name__ == "__main__":
-    pass
-    # exit(bump_version_if_required(get_releases()))
+    bump_required = version_should_be_bumped(get_changed_files())
+    exit(bump_version_if_required(get_releases(), bump_required))
