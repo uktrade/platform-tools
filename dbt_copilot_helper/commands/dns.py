851ff879b2cf02a993339bbb2aaca39c65bfdb88
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2023-12-14 16:58:38 +0000

    move prod domain to live aws

--- dbt_copilot_helper/commands/dns.py
+++ dbt_copilot_helper/commands/dns.py
@@ -28,7 +28,7 @@ from dbt_copilot_helper.utils.versioning import (
 AVAILABLE_DOMAINS = {
     "great.gov.uk": "live",
     "trade.gov.uk": "live",
-    "prod.uktrade.digital": "dev",
+    "prod.uktrade.digital": "live",
     "uktrade.digital": "dev",
 }
 AWS_CERT_REGION = "eu-west-2"
