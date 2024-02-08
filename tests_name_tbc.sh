f8e1fa06038fb772b7e16125f5df1623e27c8540
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-08 14:29:41 +0000

    Comment out stuff to speed up feedback loop

--- tests_name_tbc.sh
+++ tests_name_tbc.sh
@@ -3,18 +3,13 @@
 # exit early if something goes wrong
 set -e
 
-echo "Build and install copilot-helper"
-poetry build --no-interaction --format sdist --no-ansi
-pip install "dist/$(ls -t1 dist | head -1)"
-copilot-helper --version
-
-jq
-
-aws
+#echo "Build and install copilot-helper"
+#poetry build --no-interaction --format sdist --no-ansi
+#pip install "dist/$(ls -t1 dist | head -1)"
+#copilot-helper --version
 
 echo "Get CodeStar connection details"
 # todo: break this up to get account ID and ARM
-codestarArn=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']" | jq -r ".[0].ConnectionArn")
 codestarArn=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']" | jq -r ".[0].ConnectionArn")
 echo "$codestarArn"
 
