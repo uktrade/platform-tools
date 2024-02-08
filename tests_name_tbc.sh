d938e3c1d7d7e09c327c033dd4bd9475c26a28f4
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-08 12:37:19 +0000

    Start getting CodeStar details

---
+++ tests_name_tbc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Build and install copilot-helper"
+poetry build --no-interaction --format sdist --no-ansi
+pip install "dist/$(ls -t1 dist | head -1)"
+copilot-helper --version
+
+echo "Get CodeStar connection details"
+# todo: break this up to get account ID and ARM
+codestarArn=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']" | jq -r ".[0].ConnectionArn")
+echo "$codestarArn"
+
+codestarArnFromAccount=$(echo "${codestarArn#*arn:aws:codestar-connections:eu-west-2:}")
+echo "$codestarArnFromAccount"
+
+codestarId=$("${codestarArnFromAccount#*/}")
+echo "$codestarId"
+
+echo "Clone demodjango_deploy"
+#git clone "https://codestar-connections.eu-west-2.amazonaws.com/git-http/AWS ACCOUNT/" \
+#        "eu-west-2/CSID/uktrade/demodjango-deploy.git"
