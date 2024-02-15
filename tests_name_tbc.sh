b1113da66b3871241019834f16b27ffe2773fc5f
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-15 17:03:45 +0000

    Do some reverting

--- tests_name_tbc.sh
+++ tests_name_tbc.sh
@@ -3,8 +3,6 @@
 # exit early if something goes wrong
 set -e
 
-export AWS_PROFILE="platform-tools"
-export DEMODJANGO_AWS_PROFILE="platform-sandbox"
 export COPILOT_HELPER_DISABLE_VERSION_CHECK="true"
 
 echo "Build and install copilot-helper"
@@ -13,7 +11,7 @@ pip install "dist/$(ls -t1 dist | head -1)"
 copilot-helper --version
 
 echo "Get CodeStar connection details"
-codestarConnections=$(aws codestar-connections list-connections --profile "$DEMODJANGO_AWS_PROFILE}" --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']")
+codestarConnections=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']")
 awsAccount=$(echo "$codestarConnections" | jq -r ".[0].OwnerAccountId")
 codestarArn=$(echo "$codestarConnections" | jq -r ".[0].ConnectionArn")
 codestarConnectionId=$(echo "${codestarArn##*/}")
@@ -29,6 +27,7 @@ cd ./demodjango-deploy/
 
 # make-addons
 echo "Run make-addons from copilot-helper"
+export AWS_PROFILE="platform-tools"
 aws configure --profile "$AWS_PROFILE" set account_id "$PLATFORM_TOOLS_ACCOUNT_ID"
 aws configure --profile "$AWS_PROFILE" set region "eu-west-2"
 aws configure --profile "$AWS_PROFILE" set output "json"
