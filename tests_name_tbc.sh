13ac099776e12d15ef7a665a8869b2ff11911432
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-19 17:29:52 +0000

    Add assume role

--- tests_name_tbc.sh
+++ tests_name_tbc.sh
@@ -6,39 +6,39 @@ set -e
 # Todo: Make this do something...
 export COPILOT_HELPER_DISABLE_VERSION_CHECK="true"
 
-echo "Build and install copilot-helper"
+echo -e "\nBuild and install copilot-helper"
 poetry build --no-interaction --format sdist --no-ansi
 pip install "dist/$(ls -t1 dist | head -1)"
 copilot-helper --version
 
-echo "Get CodeStar connection details"
+echo -e "\nGet CodeStar connection details"
 codestarConnections=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']")
 awsAccount=$(echo "$codestarConnections" | jq -r ".[0].OwnerAccountId")
 codestarArn=$(echo "$codestarConnections" | jq -r ".[0].ConnectionArn")
 codestarConnectionId=$(echo "${codestarArn##*/}")
 
-echo "Clone demodjango_deploy"
+echo -e "\nClone demodjango_deploy"
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git clone "https://codestar-connections.eu-west-2.amazonaws.com/git-http/$awsAccount/eu-west-2/$codestarConnectionId/uktrade/demodjango-deploy.git"
 
-# change working directory
-echo "cd demodjango-deploy"
+echo -e "\ncd demodjango-deploy"
 cd ./demodjango-deploy/
 
-# make-addons
-echo "Run make-addons from copilot-helper"
+echo -e "\nRun make-addons from copilot-helper"
 export AWS_PROFILE="platform-tools"
 aws configure --profile "$AWS_PROFILE" set account_id "$PLATFORM_TOOLS_ACCOUNT_ID"
 aws configure --profile "$AWS_PROFILE" set region "eu-west-2"
 aws configure --profile "$AWS_PROFILE" set output "json"
-
-# TODO: copilot-tools is set to 1.0.2 should be 1.1.2 / skipped / disable version check
-
 copilot-helper copilot make-addons
 ls ./copilot/environments/addons
 
-echo "Run copilot env init"
+echo -e "\nAssume demodjango-executionrole"
+aws sts assume-role \
+    --role-arn "arn:aws:iam::$PLATFORM_TOOLS_ACCOUNT_ID:role/demodjango-executionrole" \
+    --role-session-name "copilot-tools-regression-pipeline-$CODEBUILD_BUILD_NUMBER"
+
+echo -e "\nRun copilot env init"
 copilot env init --name toolspr --profile $AWS_PROFILE --default-config
 
 # deploy env
