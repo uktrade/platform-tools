1d9064550081ed43640a923498b1cb87c4f864d0
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-02-20 13:20:52 +0000

    Comment out the rest of the stuff for faster IAM test feedback

--- tests_name_tbc.sh
+++ tests_name_tbc.sh
@@ -4,34 +4,34 @@
 set -e
 
 # Todo: Make this do something...
-export COPILOT_HELPER_DISABLE_VERSION_CHECK="true"
-
-echo -e "\nBuild and install copilot-helper"
-poetry build --no-interaction --format sdist --no-ansi
-pip install "dist/$(ls -t1 dist | head -1)"
-copilot-helper --version
-
-echo -e "\nGet CodeStar connection details"
-codestarConnections=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']")
-awsAccount=$(echo "$codestarConnections" | jq -r ".[0].OwnerAccountId")
-codestarArn=$(echo "$codestarConnections" | jq -r ".[0].ConnectionArn")
-codestarConnectionId=$(echo "${codestarArn##*/}")
-
-echo -e "\nClone demodjango_deploy"
-git config --global credential.helper '!aws codecommit credential-helper $@'
-git config --global credential.UseHttpPath true
-git clone "https://codestar-connections.eu-west-2.amazonaws.com/git-http/$awsAccount/eu-west-2/$codestarConnectionId/uktrade/demodjango-deploy.git"
-
-echo -e "\ncd demodjango-deploy"
-cd ./demodjango-deploy/
-
-echo -e "\nRun make-addons from copilot-helper"
-export AWS_PROFILE="platform-tools"
-aws configure --profile "$AWS_PROFILE" set account_id "$AWS_ACCOUNT_ID"
-aws configure --profile "$AWS_PROFILE" set region "eu-west-2"
-aws configure --profile "$AWS_PROFILE" set output "json"
-copilot-helper copilot make-addons
-ls ./copilot/environments/addons
+#export COPILOT_HELPER_DISABLE_VERSION_CHECK="true"
+#
+#echo -e "\nBuild and install copilot-helper"
+#poetry build --no-interaction --format sdist --no-ansi
+#pip install "dist/$(ls -t1 dist | head -1)"
+#copilot-helper --version
+#
+#echo -e "\nGet CodeStar connection details"
+#codestarConnections=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']")
+#awsAccount=$(echo "$codestarConnections" | jq -r ".[0].OwnerAccountId")
+#codestarArn=$(echo "$codestarConnections" | jq -r ".[0].ConnectionArn")
+#codestarConnectionId=$(echo "${codestarArn##*/}")
+#
+#echo -e "\nClone demodjango_deploy"
+#git config --global credential.helper '!aws codecommit credential-helper $@'
+#git config --global credential.UseHttpPath true
+#git clone "https://codestar-connections.eu-west-2.amazonaws.com/git-http/$awsAccount/eu-west-2/$codestarConnectionId/uktrade/demodjango-deploy.git"
+#
+#echo -e "\ncd demodjango-deploy"
+#cd ./demodjango-deploy/
+#
+#echo -e "\nRun make-addons from copilot-helper"
+#export AWS_PROFILE="platform-tools"
+#aws configure --profile "$AWS_PROFILE" set account_id "$AWS_ACCOUNT_ID"
+#aws configure --profile "$AWS_PROFILE" set region "eu-west-2"
+#aws configure --profile "$AWS_PROFILE" set output "json"
+#copilot-helper copilot make-addons
+#ls ./copilot/environments/addons
 
 echo -e "\nAssume demodjango-executionrole"
 aws sts assume-role \
