6681bf1cd0831e110b6b92eb9a6338c050dd8f13
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-02-28 16:07:50 +0000

    Testing generate

--- tests/copilot_helper/test_command_generate.py
+++ tests/copilot_helper/test_command_generate.py
@@ -5,13 +5,11 @@ from click.testing import CliRunner
 from dbt_copilot_helper.commands.generate import generate as copilot_helper_generate
 
 
-@patch("dbt_copilot_helper.commands.copilot.make_addons", return_value=None)
-@patch("dbt_copilot_helper.commands.pipeline.generate", return_value=None)
+@patch("dbt_copilot_helper.commands.generate.make_addons", return_value=None)
+@patch("dbt_copilot_helper.commands.generate.pipeline_generate", return_value=None)
 def test_copilot_helper_generate_creates_the_pipeline_configuration_and_addons(
-    mock_generate, mock_make_addons, fakefs
+    mock_generate, mock_make_addons
 ):
-    fakefs.create_dir("copilot")
-
     CliRunner().invoke(copilot_helper_generate)
 
     assert mock_generate.called
