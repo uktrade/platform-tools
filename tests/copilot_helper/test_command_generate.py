d7b8eb7a85cd1ac2237b5870ebf05bcc5c45ae42
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-02-28 15:56:29 +0000

    Mocking not working

--- tests/copilot_helper/test_command_generate.py
+++ tests/copilot_helper/test_command_generate.py
@@ -7,11 +7,12 @@ from dbt_copilot_helper.commands.generate import generate as copilot_helper_gene
 
 @patch("dbt_copilot_helper.commands.copilot.make_addons", return_value=None)
 @patch("dbt_copilot_helper.commands.pipeline.generate", return_value=None)
-def test_pipeline_full_generate_creates_the_pipeline_configuration_and_addons(
-    generate, make_addons, fakefs
+def test_copilot_helper_generate_creates_the_pipeline_configuration_and_addons(
+    mock_generate, mock_make_addons, fakefs
 ):
     fakefs.create_dir("copilot")
+
     CliRunner().invoke(copilot_helper_generate)
 
-    make_addons.assert_called()
-    generate.assert_called()
+    assert mock_generate.called
+    assert mock_make_addons.called
