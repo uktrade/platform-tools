4fc037eb7244d1b0062399e30555f20575a412d0
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-02-28 12:03:11 +0000

    Tests?

--- tests/copilot_helper/test_copilot_helper.py
+++ tests/copilot_helper/test_copilot_helper.py
@@ -28,7 +28,8 @@ class TestCopilotHelperCli:
             "domain",
             "cdn",
             "environment",
+            "generate",
             "pipeline",
             "waf",
-            "application"
+            "application",
         ]
