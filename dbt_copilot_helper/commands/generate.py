f2e8d12ba950d4f5184a3e41851d2f6c400130ee
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-02-28 10:50:56 +0000

    More wip

--- dbt_copilot_helper/commands/generate.py
+++ dbt_copilot_helper/commands/generate.py
@@ -3,10 +3,15 @@ import click
 
 from dbt_copilot_helper.commands.copilot import make_addons
 from dbt_copilot_helper.commands.pipeline import generate as pipeline_generate
-from dbt_copilot_helper.utils.click import ClickDocOptGroup
 
 
-@click.group(chain=True, cls=ClickDocOptGroup)
+@click.command()
 def generate():
+    """
+    Given a pipelines.yml file, generate environment and service deployment
+    pipelines and generate addons CloudFormation for each environment.
+
+    Wraps pipeline generate and make-addons.
+    """
     pipeline_generate()
     make_addons()
