c3bbb5774cf63f39e31db4b46ad3aa3b6b245af5
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-03-12 10:17:48 +0000

    Testing make-addons and pipeline generate separately

--- tests/copilot_helper/test_command_generate.py
+++ tests/copilot_helper/test_command_generate.py
@@ -3,7 +3,9 @@ from unittest.mock import patch
 
 from click.testing import CliRunner
 
+from dbt_copilot_helper.commands.copilot import make_addons
 from dbt_copilot_helper.commands.generate import generate as copilot_helper_generate
+from dbt_copilot_helper.commands.pipeline import generate as pipeline_generate
 
 
 @patch("dbt_copilot_helper.commands.generate.make_addons", return_value=None)
@@ -28,10 +30,48 @@ def test_copilot_helper_generate_shows_a_warning_when_version_is_different_than_
     get_file_app_versions, secho
 ):
     get_file_app_versions.return_value = (1, 0, 1), (1, 0, 0)
-    
+
     CliRunner().invoke(copilot_helper_generate)
 
     secho.assert_called_once_with(
         f"WARNING: You are running copilot-helper v1.0.1 against v1.0.0 specified by .copilot-helper-version.",
         fg="red",
+    )
+
+
+@patch("click.secho")
+@patch("dbt_copilot_helper.utils.versioning.get_file_app_versions")
+@patch(
+    "dbt_copilot_helper.utils.versioning.running_as_installed_package", new=Mock(return_value=True)
+)
+@patch("dbt_copilot_helper.commands.copilot.make_addons", new=Mock(return_value=None))
+def test_make_addons_shows_a_warning_when_version_is_different_than_on_file(
+    get_file_app_versions, secho
+):
+    get_file_app_versions.return_value = (1, 0, 1), (1, 0, 0)
+
+    CliRunner().invoke(make_addons)
+
+    assert (
+        secho.call_args_list[0][0][0]
+        == f"WARNING: You are running copilot-helper v1.0.1 against v1.0.0 specified by .copilot-helper-version."
+    )
+
+
+@patch("click.secho")
+@patch("dbt_copilot_helper.utils.versioning.get_file_app_versions")
+@patch(
+    "dbt_copilot_helper.utils.versioning.running_as_installed_package", new=Mock(return_value=True)
+)
+@patch("dbt_copilot_helper.commands.pipeline.generate", new=Mock(return_value=None))
+def test_pipeline_generate_shows_a_warning_when_version_is_different_than_on_file(
+    get_file_app_versions, secho
+):
+    get_file_app_versions.return_value = (1, 0, 1), (1, 0, 0)
+
+    CliRunner().invoke(pipeline_generate)
+
+    assert (
+        secho.call_args_list[0][0][0]
+        == f"WARNING: You are running copilot-helper v1.0.1 against v1.0.0 specified by .copilot-helper-version."
     )
