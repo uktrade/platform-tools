9b1287c5bbf59266db48533783ff1a28e10b0209
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-03-13 15:30:35 +0000

    Properly rename the package from dbt-copilot-tools to dbt-copilot-helper

--- dbt_platform_helper/templates/pipelines/codebase/overrides/stack.ts
+++ dbt_platform_helper/templates/pipelines/codebase/overrides/stack.ts
@@ -418,7 +418,7 @@ export class TransformedStack extends cdk.Stack {
             path.join(pipelineRoot, 'manifest.yml'),
         ).toString('utf-8')) as PipelineManifest;
 
-        // Load dbt-platform-tools pipelines configurations
+        // Load dbt-platform-helper pipelines configurations
         this.pipelinesFile = parse(readFileSync(
             path.join(deployRepoRoot, 'pipelines.yml'),
         ).toString('utf-8')) as PipelinesConfiguration;
