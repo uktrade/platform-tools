72c3cd032fc7a4900e6838f2917219a1338e3ec6
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-04-22 10:47:34 +0100

    add Mock object as return type for call to click.secho method

--- tests/platform_helper/test_command_generate.py
+++ tests/platform_helper/test_command_generate.py
@@ -17,11 +17,14 @@ def test_platform_helper_generate_calls_invoke(
 
 
 def test_platform_helper_generate_creates_the_pipeline_configuration_and_addons():
-    with patch(
-        "dbt_platform_helper.commands.generate.make_addons", new=Mock(return_value=None)
-    ) as mock_make_addons, patch(
-        "dbt_platform_helper.commands.generate.pipeline_generate", new=Mock(return_value=None)
-    ) as mock_generate:
+    with (
+        patch(
+            "dbt_platform_helper.commands.generate.make_addons", new=Mock(return_value=None)
+        ) as mock_make_addons,
+        patch(
+            "dbt_platform_helper.commands.generate.pipeline_generate", new=Mock(return_value=None)
+        ) as mock_generate,
+    ):
         CliRunner().invoke(platform_helper_generate)
 
         assert mock_generate.call_count == 1
@@ -29,16 +32,21 @@ def test_platform_helper_generate_creates_the_pipeline_configuration_and_addons(
 
 
 def test_platform_helper_generate_shows_a_warning_when_version_is_different_than_on_file():
-    with patch("click.secho") as secho, patch(
-        "dbt_platform_helper.utils.versioning.get_file_app_versions"
-    ) as get_file_app_versions, patch(
-        "dbt_platform_helper.utils.versioning.running_as_installed_package",
-        new=Mock(return_value=True),
-    ), patch(
-        "dbt_platform_helper.commands.generate.make_addons", new=Mock(return_value=None)
-    ), patch(
-        "dbt_platform_helper.commands.generate.pipeline_generate", new=Mock(return_value=None)
+    with (
+        patch("click.secho", new=Mock(return_value=None)) as secho,
+        patch(
+            "dbt_platform_helper.utils.versioning.get_file_app_versions"
+        ) as get_file_app_versions,
+        patch(
+            "dbt_platform_helper.utils.versioning.running_as_installed_package",
+            new=Mock(return_value=True),
+        ),
+        patch("dbt_platform_helper.commands.generate.make_addons", new=Mock(return_value=None)),
+        patch(
+            "dbt_platform_helper.commands.generate.pipeline_generate", new=Mock(return_value=None)
+        ),
     ):
+
         get_file_app_versions.return_value = (1, 0, 1), (1, 0, 0)
 
         CliRunner().invoke(platform_helper_generate)
