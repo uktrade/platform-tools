b64c6aa020a8cf283aa5b89e039c268d8fd18577
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-04-22 12:48:40 +0100

    change tests to patch echo

--- tests/platform_helper/test_command_generate.py
+++ tests/platform_helper/test_command_generate.py
@@ -27,23 +27,22 @@ def test_platform_helper_generate_calls_invoke(
 #     assert mock_make_addons.called
 #
 #
-@patch("click.secho")
+@patch("click.echo")
 @patch("dbt_platform_helper.utils.versioning.get_file_app_versions")
 @patch(
     "dbt_platform_helper.utils.versioning.running_as_installed_package", new=Mock(return_value=True)
 )
-@patch("dbt_platform_helper.commands.generate.make_addons", return_value=None)
-@patch("dbt_platform_helper.commands.generate.pipeline_generate", return_value=None)
+@patch("dbt_platform_helper.commands.generate.make_addons", new=Mock(return_value=True))
+@patch("dbt_platform_helper.commands.generate.pipeline_generate", new=Mock(return_value=True))
 def test_platform_helper_generate_shows_a_warning_when_version_is_different_than_on_file(
-    fake_make_addons, fake_generate, get_file_app_versions, secho
+    get_file_app_versions, echo
 ):
     get_file_app_versions.return_value = (1, 0, 1), (1, 0, 0)
 
     res = CliRunner().invoke(platform_helper_generate)
     print(res.output)
-    secho.assert_called_once_with(
-        f"WARNING: You are running platform-helper v1.0.1 against v1.0.0 specified by .platform-helper-version.",
-        fg="red",
+    echo.assert_called_with(
+        f"WARNING: You are running platform-helper v1.0.1 against v1.0.0 specified by .platform-helper-version."
     )
 
 
