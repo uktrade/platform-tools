2caba51dde0d6772a2212ab1bd6ecf9c08155897
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-04-25 13:52:57 +0100

    pyfakefs working and simplified to singlehappy path test

--- tests/utils/test_create_command_docs.py
+++ tests/utils/test_create_command_docs.py
@@ -1,9 +1,6 @@
 from pathlib import Path
-from unittest import TestCase
 
 from click.testing import CliRunner
-
-# from pyfakefs.fake_filesystem_unittest import TestCase
 from pyfakefs.fake_filesystem_unittest import TestCase
 from pyfakefs.fake_filesystem_unittest import patchfs
 
@@ -11,32 +8,12 @@ from tests.platform_helper.conftest import BASE_DIR
 from tests.platform_helper.conftest import DOCS_DIR
 from utils.create_command_docs import docs
 
-# @pytest.fixture
-# def mock_create_docs(monkeypatch):
-#     def mock_create_docs_func(base_command, output):
-#         output = f"{DOCS_DIR}/example.md"
-#         create_docs(base_command, output)
-#
-#     monkeypatch.setattr("utils.create_command_docs.create_docs", mock_create_docs_func)
-
 
 class TestCreateCommandDocsCli(TestCase):
-    def setUp(self) -> None:
+    @patchfs
+    def setUp(self, fs) -> None:
         self.setUpPyfakefs()
         self.runner = CliRunner()
-
-    @classmethod
-    def tearDownClass(cls):
-        Path(f"{DOCS_DIR}/test-docs.md").unlink(missing_ok=True)
-        Path(f"{DOCS_DIR}/example.md").unlink(missing_ok=True)
-
-    # def teardown(self) -> None:
-    #     # self._cleanup()
-    #     pass
-    #
-    # def _cleanup(self):
-    #     Path(f"{DOCS_DIR}/test-docs.md").unlink(missing_ok=True)
-    #     Path(f"{DOCS_DIR}/example.md").unlink(missing_ok=True)
 
     def test_check_required_module_option(self):
         result = self.runner.invoke(docs, ["--cmd", "bar", "--output", "baz"])
@@ -80,39 +57,18 @@ class TestCreateCommandDocsCli(TestCase):
         assert result.exit_code != 0
         assert "Error: Could not find command bar in platform_helper module" in output
 
-    def test_create_command_docs(self):
-        output_path = f"{DOCS_DIR}/test-docs.md"
-
+    @patchfs
+    def test_create_command_docs_template_output(self, fs):
+        fs.add_real_directory(BASE_DIR / "dbt_platform_helper/templates")
+        expected_output_path = "expected_output.md"
+        fs.add_real_file(
+            source_path=f"{DOCS_DIR}/expected_output.md", target_path=expected_output_path
+        )
+        expected_output = open(expected_output_path).read()
+        output_path = "actual_output.md"
         assert not Path(output_path).is_file()
 
         result = self.runner.invoke(
-            docs,
-            [
-                "--module",
-                "platform_helper",
-                "--cmd",
-                "platform_helper",
-                "--output",
-                output_path,
-            ],
-        )
-
-        output = result.output
-
-        assert result.exit_code == 0
-        assert "Markdown docs have been successfully saved to " + output_path in output
-
-    # @pytest.mark.usefixtures("mock_create_docs")
-    @patchfs
-    def test_create_command_docs_template_output(self, fs):
-        output_path = f"{DOCS_DIR}/example.md"
-        expected_output_path = f"{DOCS_DIR}/expected_output.md"
-
-        fs.add_real_directory(BASE_DIR / "dbt_platform_helper/templates", lazy_read=True)
-
-        assert not Path(output_path).is_file()
-
-        self.runner.invoke(
             docs,
             [
                 "--module",
@@ -123,8 +79,7 @@ class TestCreateCommandDocsCli(TestCase):
                 output_path,
             ],
         )
-        print(f"LOOK HERE: {open(output_path).read()}")
-        output = open(output_path).read()
-        expected_output = open(expected_output_path).read()
 
-        assert output == expected_output
+        assert result.exit_code == 0
+        assert "Markdown docs have been successfully saved to " + output_path in result.output
+        assert open(output_path).read() == expected_output
