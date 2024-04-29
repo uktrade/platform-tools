d8735917b7d7e38980b1c59011da9c49462ae2ec
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-04-29 16:15:50 +0100

    Refractor get_aws_session_or_abort method exceptions

--- tests/platform_helper/utils/test_aws.py
+++ tests/platform_helper/utils/test_aws.py
@@ -5,6 +5,7 @@ from unittest.mock import mock_open
 from unittest.mock import patch
 
 import boto3
+import botocore
 import pytest
 from moto import mock_aws
 
@@ -81,6 +82,45 @@ def test_get_ssm_secrets(mock_get_aws_session_or_abort):
     result = get_ssm_secrets("test-application", "development")
 
     assert result == [("/copilot/test-application/development/secrets/TEST_SECRET", "test value")]
+
+
+@patch("click.secho")
+def test_get_aws_session_or_abort_with_invalid_credentials(mock_secho):
+    aws_profile = "existing_profile"
+    expected_error_message = (
+        "The SSO session associated with this profile has expired or is otherwise invalid."
+        + "To refresh this SSO session run `aws sso login` with the corresponding profile"
+    )
+
+    with patch("boto3.session.Session") as mock_session:
+        with patch("dbt_platform_helper.utils.aws.get_account_details") as mock_get_account_details:
+            mock_get_account_details.side_effect = botocore.exceptions.SSOTokenLoadError(
+                error_msg=expected_error_message
+            )
+
+            with pytest.raises(SystemExit) as exc_info:
+                get_aws_session_or_abort(aws_profile=aws_profile)
+
+            assert exc_info.value.code == 1
+            assert mock_secho.call_count > 0
+            assert mock_secho.call_args[0][0] == expected_error_message
+
+
+@patch("click.secho")
+def test_get_aws_session_or_abort_with_misconfigured_profile(mock_secho):
+    misconfigured_profile = "nonexistent_profile"
+    expected_error_message = f"""AWS profile "{misconfigured_profile}" is not configured."""
+    with patch("boto3.session.Session") as mock_session:
+        mock_session.side_effect = botocore.exceptions.ProfileNotFound(
+            profile=misconfigured_profile
+        )
+
+        with pytest.raises(SystemExit) as exc_info:
+            get_aws_session_or_abort(aws_profile=misconfigured_profile)
+
+        assert exc_info.value.code == 1
+        assert mock_secho.call_count > 0
+        assert mock_secho.call_args[0][0] == expected_error_message
 
 
 @pytest.mark.parametrize(
