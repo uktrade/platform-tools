36d621a693dd29d469c8fa396a8502c78eb7d0d4
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-04-29 16:46:35 +0100

    Add basic failing test_writes_aws_config_file

--- tests/platform_helper/test_command_aws.py
+++ tests/platform_helper/test_command_aws.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from unittest.mock import patch
 
 from click.testing import CliRunner
@@ -9,24 +10,29 @@ class TestAWSConfigureCommand:
     @patch("dbt_platform_helper.commands.aws.get_aws_session_or_abort")
     @patch("boto3.session.Session")
     def test_get_aws_session_or_abort_is_called1(self, mock_session, mock_get_aws_session_or_abort):
-        result = CliRunner().invoke(configure)
-        assert result.exit_code == 0
+        CliRunner().invoke(configure)
+
         assert mock_get_aws_session_or_abort.call_count > 0
 
-    # test_writes_aws_config_file
-    # Should use fake filesystem
-    # Be mocked to have suitable response from sso_client.list_accounts etc.
-    # Write file like...
-    # [default]
-    # region=eu-west-2
-    # sso_start_url=https://uktrade.awsapps.com/start
-    # sso_region=eu-west-2
-    # sso_account_id = 123456789
-    # sso_role_name = AdministratorAccess
-    #
-    # [profile test-account]
-    # sso_start_url=https://uktrade.awsapps.com/start
-    # sso_region=eu-west-2
-    # sso_account_id = 123456789
-    # sso_role_name = AdministratorAccess
-    # region=eu-west-2
+    @patch("dbt_platform_helper.commands.aws.get_aws_session_or_abort")
+    @patch("boto3.session.Session")
+    def test_writes_aws_config_file(self, mock_session, mock_get_aws_session_or_abort, fakefs):
+        expected_output = """[default]
+region=eu-west-2
+sso_start_url=https://uktrade.awsapps.com/start
+sso_region=eu-west-2
+sso_account_id = 123456789
+sso_role_name = AdministratorAccess
+
+[profile test-account]
+sso_start_url=https://uktrade.awsapps.com/start
+sso_region=eu-west-2
+sso_account_id = 123456789
+sso_role_name = AdministratorAccess
+region=eu-west-2
+"""
+
+        result = CliRunner().invoke(configure)
+
+        assert result.exit_code == 0
+        assert expected_output in Path("output_file").read_text()
