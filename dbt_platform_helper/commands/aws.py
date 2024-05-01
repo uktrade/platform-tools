691cd703a20be349d35421b9adbc3982722fc326
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-05-01 15:12:33 +0100

    Add test to check file is written with correct contents

--- dbt_platform_helper/commands/aws.py
+++ dbt_platform_helper/commands/aws.py
@@ -1,4 +1,6 @@
-from pathlib import Path
+# from configparser import ConfigParser
+import configparser
+import io
 from typing import Dict
 from typing import List
 
@@ -11,11 +13,34 @@ from dbt_platform_helper.utils.versioning import (
 )
 
 
-def get_aws_accounts(client, creds) -> List[Dict[str, str]]:
-    aws_accounts_response = client.list_accounts(accessToken=creds.get("token"))
-    if len(aws_accounts_response.get("accountList", [])) == 0:
+def get_aws_accounts(client, token) -> List[Dict[str, str]]:
+    aws_accounts_response = client.list_accounts(accessToken=token)
+    account_list = aws_accounts_response.get("accountList", [])
+    if not account_list:
         raise RuntimeError("Unable to retrieve AWS SSO account list\n")
-    return aws_accounts_response.get("accountList")
+    return account_list
+
+
+def get_aws_credentials(client, token, account_id, role_name="AdministratorAccess"):
+    sts_creds = client.get_role_credentials(
+        accessToken=token,
+        roleName=role_name,
+        accountId=account_id,
+    )
+
+    if "roleCredentials" not in sts_creds:
+        raise RuntimeError("Unable to retrieve STS credentials")
+    credentials = sts_creds.get("roleCredentials")
+    if "accessKeyId" not in credentials:
+        raise RuntimeError("Unable to retrieve STS credentials")
+
+    return {
+        "aws_access_key_id": credentials["accessKeyId"],
+        "aws_secret_access_key": credentials["secretAccessKey"],
+        "aws_session_token": credentials["sessionToken"],
+        "expiration": credentials["expiration"],
+        "role": role_name,
+    }
 
 
 @click.group(chain=True, cls=ClickDocOptGroup)
@@ -25,28 +50,51 @@ def aws():
 
 
 @aws.command()
-def configure():
+def configure(directory="."):
     # Todo: doc comment
 
     session = get_aws_session_or_abort()
     sso_client = session.client("sso")
-    credentials = session.get_credentials()
+    session_credentials = session.get_credentials()
+    config = configparser.ConfigParser()
 
-    get_aws_accounts(sso_client, credentials)
+    accounts = get_aws_accounts(sso_client, session_credentials.get("token"))
+    for account in accounts:
+        account_name = account["accountName"]
+        account_id = account["accountId"]
+        role_name = "AdministratorAccess"
 
-    profile_configurations = """[default]
-region=eu-west-2
-sso_start_url=https://uktrade.awsapps.com/start
-sso_region=eu-west-2
-sso_account_id = 123456789
-sso_role_name = AdministratorAccess
+        profile_name = f"profile {account_name.lower().replace(" ", " - ")}"
+        config[profile_name] = {
+            "sso_start_url": "https://uktrade.awsapps.com/start",
+            "sso_region": "eu-west-2",
+            "sso_account_id": account_id,
+            "sso_role_name": role_name,
+            "region": "eu-west-2",
+        }
+    config_str = io.StringIO()
+    config.write(config_str)
+    config_string_value = config_str.getvalue()
 
-[profile test-account]
-sso_start_url=https://uktrade.awsapps.com/start
-sso_region=eu-west-2
-sso_account_id = 123456789
-sso_role_name = AdministratorAccess
-region=eu-west-2
-"""
+    # mkfile(Path(directory), "aws_test_config.ini", config)
+    with open("./aws_test_config.ini", "w") as configfile:
+        configfile.write(config_string_value)
 
-    Path("aws_config").write_text(profile_configurations)
+
+#     profile_configurations = """[profile test-account-1]
+# sso_start_url = https://uktrade.awsapps.com/start
+# sso_region = eu-west-2
+# sso_account_id = 123456789012
+# sso_role_name = AdministratorAccess
+# region = eu-west-2
+#
+# [profile test-account-2]
+# sso_start_url = https://uktrade.awsapps.com/start
+# sso_region = eu-west-2
+# sso_account_id = 234567890123
+# sso_role_name = AdministratorAccess
+# region = eu-west-2
+#
+# """
+#
+#     Path("aws_config").write_text(profile_configurations)
--- tests/platform_helper/test_command_aws.py
+++ tests/platform_helper/test_command_aws.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from unittest.mock import MagicMock
+from unittest.mock import mock_open
 from unittest.mock import patch
 
 import pytest
@@ -25,11 +26,11 @@ class TestAWSConfigureCommand:
         self, mock_session, mock_get_aws_session_or_abort, mock_get_aws_accounts, fakefs
     ):
         expected_output = """[default]
-region=eu-west-2
 sso_start_url=https://uktrade.awsapps.com/start
 sso_region=eu-west-2
 sso_account_id = 123456789
 sso_role_name = AdministratorAccess
+region=eu-west-2
 
 [profile test-account]
 sso_start_url=https://uktrade.awsapps.com/start
@@ -49,8 +50,8 @@ region=eu-west-2
         mock_sso_client = MagicMock()
         mock_sso_client.list_accounts.return_value = {
             "accountList": [
-                {"AccountId": "123456789012", "AccountName": "Test Account 1"},
-                {"AccountId": "234567890123", "AccountName": "Test Account 2"},
+                {"accountId": "123456789012", "accountName": "Test Account 1"},
+                {"accountId": "234567890123", "accountName": "Test Account 2"},
             ]
         }
 
@@ -59,12 +60,12 @@ region=eu-west-2
 
         # Check if the function returns the expected list of AWS accounts
         expected_accounts = [
-            {"AccountId": "123456789012", "AccountName": "Test Account 1"},
-            {"AccountId": "234567890123", "AccountName": "Test Account 2"},
+            {"accountId": "123456789012", "accountName": "Test Account 1"},
+            {"accountId": "234567890123", "accountName": "Test Account 2"},
         ]
         assert aws_accounts == expected_accounts
 
-    def test_retrieve_aws_accounts_empty_list(self):
+    def test_get_aws_accounts_empty_list(self):
         # Mock the response from the SSO client to return an empty list
         mock_sso_client = MagicMock()
         mock_sso_client.list_accounts.return_value = {"accountList": []}
@@ -75,3 +76,87 @@ region=eu-west-2
         # Assert that calling the function raises a RuntimeError
         with pytest.raises(RuntimeError):
             get_aws_accounts(mock_sso_client, creds)
+
+    @patch("dbt_platform_helper.commands.aws.get_aws_session_or_abort")
+    @patch("dbt_platform_helper.commands.aws.get_aws_accounts")
+    @patch("boto3.session.Session")
+    @patch("dbt_platform_helper.commands.aws.ConfigParser")
+    @patch("builtins.open", new_callable=mock_open)
+    def test_file_open_is_called(
+        self,
+        mock_open_call,
+        mock_config_parser,
+        mock_session,
+        mock_aws_accounts,
+        mock_get_aws_session_or_abort,
+    ):
+        mock_file = mock_open_call.return_value.__enter__.return_value
+        mock_file.write.return_value = None
+
+        mock_aws_accounts.return_value = [
+            {"accountId": "123456789012", "accountName": "Test Account 1"},
+            {"accountId": "234567890123", "accountName": "Test Account 2"},
+        ]
+
+        CliRunner().invoke(configure)
+
+        assert mock_open_call.call_count > 0
+
+    @patch("dbt_platform_helper.commands.aws.get_aws_session_or_abort")
+    @patch("dbt_platform_helper.commands.aws.get_aws_accounts")
+    @patch("boto3.session.Session")
+    @patch("builtins.open", new_callable=mock_open, create=True)
+    def test_file_creation_and_writing(
+        self, mock_open_call, mock_session, mock_aws_accounts, mock_get_aws_session_or_abort
+    ):
+        mock_aws_accounts.return_value = [
+            {"accountId": "123456789012", "accountName": "Test Account 1"},
+            {"accountId": "234567890123", "accountName": "Test Account 2"},
+        ]
+
+        expected_content = """[profile test-account-1]
+sso_start_url = https://uktrade.awsapps.com/start
+sso_region = eu-west-2
+sso_account_id = 123456789012
+sso_role_name = AdministratorAccess
+region = eu-west-2
+
+[profile test-account-2]
+sso_start_url = https://uktrade.awsapps.com/start
+sso_region = eu-west-2
+sso_account_id = 234567890123
+sso_role_name = AdministratorAccess
+region = eu-west-2
+
+"""
+        CliRunner().invoke(configure)
+        mock_open_call.assert_called_once_with("./aws_test_config.ini", "w")
+
+        file_content = mock_open_call().write.call_args[0][0]
+        assert file_content == expected_content
+
+    @patch("dbt_platform_helper.commands.aws.get_aws_session_or_abort")
+    @patch("dbt_platform_helper.commands.aws.get_aws_accounts")
+    @patch("boto3.session.Session")
+    @patch("dbt_platform_helper.commands.aws.config")
+    @patch("builtins.open", create=True)
+    def test_configure_happy_path(
+        self,
+        mock_open_file,
+        mock_config_parser,
+        mock_session,
+        mock_aws_accounts,
+        mock_get_aws_session_or_abort,
+    ):
+        # mock_get_aws_session_or_abort.return_value = MagicMock()
+        mock_aws_accounts.return_value = [
+            {"accountId": "123456789012", "accountName": "Test Account 1"},
+            {"accountId": "234567890123", "accountName": "Test Account 2"},
+        ]
+
+        result = CliRunner().invoke(configure)
+        print(f"RESULT IS: --- {result.output}")
+
+        mock_aws_accounts.assert_called_once()
+        mock_config_parser_instance = mock_config_parser.return_value
+        assert mock_config_parser_instance.write.call_count > 0
