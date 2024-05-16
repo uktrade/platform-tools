707f1d37eeb32981b38f5ac28d318d8c04e36511
Ben Noble <ben.noble@digital.trade.gov.uk>
2024-05-16 16:46:49 +0100

    Test nothing returned when no git repo provided

---
+++ tests/platform_helper/utils/test_git.py
@@ -0,0 +1,13 @@
+import dbt_platform_helper.utils.git
+
+
+def test_extract_repository_name_from_ssh_clone():
+    result = dbt_platform_helper.utils.git.extract_repository_name(
+        "git@github.com:uktrade/platform-tools.git"
+    )
+    assert result == "uktrade/platform-tools"
+
+
+def test_doesnt_fail_when_no_git_repo():
+    result = dbt_platform_helper.utils.git.extract_repository_name(None)
+    assert result == None
