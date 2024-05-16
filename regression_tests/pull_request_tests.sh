c44c9a240433c6b6bf81314f541c0e7afe5cb336
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-05-16 14:03:03 +0100

    Run platform-helper generate in the regression tests

--- regression_tests/pull_request_tests.sh
+++ regression_tests/pull_request_tests.sh
@@ -28,11 +28,8 @@ aws configure --profile "$AWS_PROFILE" set account_id "$AWS_ACCOUNT_ID"
 aws configure --profile "$AWS_PROFILE" set region "eu-west-2"
 aws configure --profile "$AWS_PROFILE" set output "json"
 
-echo -e "\nRun platform-helper copilot make-addons"
-PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper copilot make-addons
-
-# echo -e "\nRun platform-helper pipeline generate"
-# platform-helper pipeline generate
+echo -e "\nRun platform-helper generate"
+PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper generate
 
 # echo -e "\nRun copilot env init"
 # copilot env init --name toolspr --profile $AWS_PROFILE --default-config
