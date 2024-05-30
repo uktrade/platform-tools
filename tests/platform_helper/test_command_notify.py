72ca24666a093bb5640e39bdadd0d9e79b702bb8
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-05-30 16:50:11 +0100

    Comment tests added

--- tests/platform_helper/test_command_notify.py
+++ tests/platform_helper/test_command_notify.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 from click.testing import CliRunner
 
+from dbt_platform_helper.commands.notify import add_comment
 from dbt_platform_helper.commands.notify import environment_progress
 from dbt_platform_helper.commands.notify import get_build_url
 
@@ -188,3 +189,73 @@ class TestNotify(unittest.TestCase):
             f"*Revision*: <https://github.com/{repository}/commit/{commit}|{commit}>",
         )
         self.assertEqual(actual_elements[2].text, f"<{get_build_url(build_arn)}|Build Logs>")
+
+    def test_adding_comments_no_options_set(self, webclient):
+        CliRunner().invoke(
+            add_comment,
+            [
+                "my-slack-channel-id",
+                "my-slack-token",
+                "1234.56",
+                "The comment",
+            ],
+        )
+
+        calls = webclient().chat_postMessage.call_args_list
+        self.assertEqual(len(calls), 1)
+        call_args = calls[0].kwargs
+        self.assertEqual(call_args["channel"], "my-slack-channel-id")
+        self.assertEqual(call_args["text"], "The comment")
+        self.assertFalse(call_args["reply_broadcast"])
+        self.assertEqual(call_args["unfurl_links"], False)
+        self.assertEqual(call_args["unfurl_media"], False)
+        self.assertEqual(call_args["thread_ts"], "1234.56")
+        self.assertEqual(call_args["blocks"][0].text.text, "The comment")
+
+    def test_adding_comments_title_overrides_message_text(self, webclient):
+        CliRunner().invoke(
+            add_comment,
+            [
+                "my-slack-channel-id",
+                "my-slack-token",
+                "1234.56",
+                "The comment",
+                "--title",
+                "The Title",
+            ],
+        )
+
+        calls = webclient().chat_postMessage.call_args_list
+        self.assertEqual(len(calls), 1)
+        call_args = calls[0].kwargs
+        self.assertEqual(call_args["channel"], "my-slack-channel-id")
+        self.assertEqual(call_args["text"], "The Title")
+        self.assertFalse(call_args["reply_broadcast"])
+        self.assertEqual(call_args["unfurl_links"], False)
+        self.assertEqual(call_args["unfurl_media"], False)
+        self.assertEqual(call_args["thread_ts"], "1234.56")
+        self.assertEqual(call_args["blocks"][0].text.text, "The comment")
+
+    def test_adding_comments_send_to_main_channel(self, webclient):
+        CliRunner().invoke(
+            add_comment,
+            [
+                "my-slack-channel-id",
+                "my-slack-token",
+                "1234.56",
+                "The comment",
+                "--send-to-main-channel",
+                "true",
+            ],
+        )
+
+        calls = webclient().chat_postMessage.call_args_list
+        self.assertEqual(len(calls), 1)
+        call_args = calls[0].kwargs
+        self.assertEqual(call_args["channel"], "my-slack-channel-id")
+        self.assertEqual(call_args["text"], "The comment")
+        self.assertTrue(call_args["reply_broadcast"])
+        self.assertEqual(call_args["unfurl_links"], False)
+        self.assertEqual(call_args["unfurl_media"], False)
+        self.assertEqual(call_args["thread_ts"], "1234.56")
+        self.assertEqual(call_args["blocks"][0].text.text, "The comment")
