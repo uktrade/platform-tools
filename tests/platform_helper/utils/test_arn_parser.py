ed50b69b7eabdc63a594028c6171a41c640fa876
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-05-31 11:32:34 +0100

    Remove unittest in favour of plain assert

--- tests/platform_helper/utils/test_arn_parser.py
+++ tests/platform_helper/utils/test_arn_parser.py
@@ -1,5 +1,3 @@
-import unittest
-
 import pytest
 from parameterized import parameterized
 
@@ -7,27 +5,25 @@ from dbt_platform_helper.exceptions import ValidationException
 from dbt_platform_helper.utils.arn_parser import ARN
 
 
-class TestArnParser(unittest.TestCase):
-    def setUp(self, *args, **kwargs):
-        self.source_arn = "arn:partition:service:region:account-id:resource-type:resource-id"
+def test_arn_parser_properties():
+    source_arn = "arn:partition:service:region:account-id:resource-type:resource-id"
+    arn = ARN(source_arn)
 
-    def test_arn_parser_properties(self):
-        arn = ARN(self.source_arn)
+    assert arn.source == source_arn
+    assert arn.partition == "partition"
+    assert arn.service == "service"
+    assert arn.region == "region"
+    assert arn.account_id == "account-id"
+    assert arn.project == "resource-type"
+    assert arn.build_id == "resource-id"
 
-        self.assertEqual(arn.source, self.source_arn)
-        self.assertEqual(arn.partition, "partition")
-        self.assertEqual(arn.service, "service")
-        self.assertEqual(arn.region, "region")
-        self.assertEqual(arn.account_id, "account-id")
-        self.assertEqual(arn.project, "resource-type")
-        self.assertEqual(arn.build_id, "resource-id")
 
-    @parameterized.expand(
-        [
-            "",
-            "arn:partition:service:region:account-id",
-        ]
-    )
-    def test_arn_parser_raises_error_if_arn_not_valid(self, arn):
-        with pytest.raises(ValidationException, match=f"Invalid ARN: {arn}"):
-            ARN(arn)
+@parameterized.expand(
+    [
+        "",
+        "arn:partition:service:region:account-id",
+    ]
+)
+def test_arn_parser_raises_error_if_arn_not_valid(arn):
+    with pytest.raises(ValidationException, match=f"Invalid ARN: {arn}"):
+        ARN(arn)
