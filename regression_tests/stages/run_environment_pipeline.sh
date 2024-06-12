6deea31eab2c80d5741e251b9b0159109139c442
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-06-12 11:57:48 +0100

    test: DBTP-681 Run deploy environment pipeline in regression tests (#452)
    Co-authored-by: tony griffin <54268925+tony-griffin@users.noreply.github.com>

---
+++ regression_tests/stages/run_environment_pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+echo -e "\nTrigger environment pipeline"
+pipeline_execution_id=$(aws codepipeline start-pipeline-execution --name demodjango-environment-pipeline-TOOLSPR --profile platform-sandbox | jq -r .pipelineExecutionId)
+echo "Environment pipeline started with pipeline execution ID: $pipeline_execution_id"
+
+start=$( date +%s )
+timeout=600
+
+echo -e "\nWait for environment pipeline to complete"
+while [ "$pipeline_status" != "Succeeded" ];
+do
+  sleep 10
+  now=$( date +%s )
+  elapsed=$(( now-start ))
+  pipeline_status=$(aws codepipeline get-pipeline-execution --pipeline-name demodjango-environment-pipeline-TOOLSPR --pipeline-execution-id "$pipeline_execution_id" --profile platform-sandbox | jq -r .pipelineExecution.status)
+  echo "Environment pipeline status after $elapsed seconds: $pipeline_status"
+
+  if [[ elapsed -gt timeout ]]; then
+    echo "Error: Environment pipeline not completed in $timeout seconds"
+    exit 1
+  fi
+done
