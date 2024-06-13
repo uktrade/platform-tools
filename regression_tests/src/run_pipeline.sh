bbc51f7a8ce5712f76a2fc7bf3dc021c34bceac8
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-06-13 12:20:15 +0100

    Add exit for not Successful status

--- regression_tests/src/run_pipeline.sh
+++ regression_tests/src/run_pipeline.sh
@@ -3,6 +3,7 @@
 function run_pipeline() {
     pipeline_type="$1"
     pipeline_name="$2"
+    timeout="$3"
 
     pipeline_type_lowercase=$(echo "$pipeline_type" | tr '[:upper:]' '[:lower:]')
 
@@ -11,10 +12,9 @@ function run_pipeline() {
     echo "$pipeline_type pipeline started with pipeline execution ID: $pipeline_execution_id"
 
     start=$( date +%s )
-    timeout=600
 
     echo -e "\nWait for $pipeline_type_lowercase pipeline to complete"
-    while [ "$pipeline_status" != "Succeeded" ];
+    while [ "$pipeline_status" == "InProgress" ];
     do
       sleep 10
       now=$( date +%s )
@@ -22,11 +22,14 @@ function run_pipeline() {
       pipeline_status=$(aws codepipeline get-pipeline-execution --pipeline-name "$pipeline_name" --pipeline-execution-id "$pipeline_execution_id" --profile platform-sandbox | jq -r .pipelineExecution.status)
       echo "$pipeline_type pipeline status after $elapsed seconds: $pipeline_status"
 
-      # Todo: Exit early if it's failed
-
       if [[ elapsed -gt timeout ]]; then
         echo "Error: $pipeline_type pipeline not completed in $timeout seconds"
         exit 1
       fi
     done
+    
+    if [ "$pipeline_status" != "Succeeded" ]; then 
+      echo "Error: $pipeline_type pipeline did not succeed. Pipeline status is $pipeline_status"
+      exit 1
+    fi
 }
