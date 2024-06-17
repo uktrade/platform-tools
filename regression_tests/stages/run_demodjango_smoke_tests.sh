4fb7e520b95baa67975e9d5953524995622334aa
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-06-17 11:08:38 +0100

    Point regression test branch to point to test branch

--- regression_tests/stages/run_demodjango_smoke_tests.sh
+++ regression_tests/stages/run_demodjango_smoke_tests.sh
@@ -6,6 +6,7 @@ echo -e "\nClone demodjango"
 git clone "${GIT_CLONE_BASE_URL}demodjango.git"
 
 cd "${CODEBUILD_SRC_DIR}/demodjango" 
+git checkout "dbt-681-regression-tests"
 echo "Current demodjango branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
 
 echo -e "\nInstall dependencies"
