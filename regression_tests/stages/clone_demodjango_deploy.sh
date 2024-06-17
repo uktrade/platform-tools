9a24e2b5f3557478d9f8bd51a6d65d2bf82018ff
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-06-17 09:22:11 +0100

    Add explicit cd into correct directories

--- regression_tests/stages/clone_demodjango_deploy.sh
+++ regression_tests/stages/clone_demodjango_deploy.sh
@@ -13,6 +13,6 @@ git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git clone "https://codestar-connections.eu-west-2.amazonaws.com/git-http/$aws_account/eu-west-2/$codestar_connection_id/uktrade/demodjango-deploy.git"
 
-cd demodjango-deploy
+cd "${CODEBUILD_SRC_DIR}/demodjango-deploy"
 echo "Current demodjango-deploy branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
-cd ..
+
--- regression_tests/stages/run_codebase_pipeline.sh
+++ regression_tests/stages/run_codebase_pipeline.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cd "${CODEBUILD_SRC_DIR}"
+
 source ./regression_tests/src/run_pipeline.sh
 
 run_pipeline "Codebase" "pipeline-demodjango-application-toolspr" 1800
