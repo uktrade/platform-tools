1e0c3d61205520ec6dff9d770745b4a0073a4845
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-06-17 10:57:18 +0100

    Add GIT_CLONE_BASE_URL for codestar

--- regression_tests/stages/clone_demodjango_deploy.sh
+++ regression_tests/stages/clone_demodjango_deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 echo -e "\nClone demodjango_deploy"
 
-git clone "https://codestar-connections.eu-west-2.amazonaws.com/git-http/$aws_account/eu-west-2/$codestar_connection_id/uktrade/demodjango-deploy.git"
+git clone "${GIT_CLONE_BASE_URL}demodjango-deploy.git"
 
 cd "${CODEBUILD_SRC_DIR}/demodjango-deploy"
 echo "Current demodjango-deploy branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
--- regression_tests/stages/run_demodjango_smoke_tests.sh
+++ regression_tests/stages/run_demodjango_smoke_tests.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo -e "\nClone demodjango"
-git clone "https://codestar-connections.eu-west-2.amazonaws.com/git-http/$aws_account/eu-west-2/$codestar_connection_id/uktrade/demodjango.git"
+git clone "${GIT_CLONE_BASE_URL}demodjango.git"
 
 cd "${CODEBUILD_SRC_DIR}/demodjango" 
 echo "Current demodjango branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
