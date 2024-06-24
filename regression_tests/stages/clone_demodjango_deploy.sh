a5b1ee665e160719a5af4dbf68de40ecec1f44b0
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-06-24 08:44:50 +0100

    git checkout DBTP-1096-environment-vpc-default-bug

--- regression_tests/stages/clone_demodjango_deploy.sh
+++ regression_tests/stages/clone_demodjango_deploy.sh
@@ -6,5 +6,7 @@ echo -e "\nClone demodjango_deploy"
 
 git clone "${GIT_CLONE_BASE_URL}/demodjango-deploy.git"
 
+git checkout DBTP-1096-environment-vpc-default-bug
+
 cd "${CODEBUILD_SRC_DIR}/demodjango-deploy"
 echo "Current demodjango-deploy branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
