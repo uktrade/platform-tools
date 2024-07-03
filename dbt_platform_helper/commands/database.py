67b047213cc36c969e4154b4a7f1918d3d23f39a
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-07-03 17:47:54 +0100

    Fix parameter names; Pass env vars to ecs task

--- dbt_platform_helper/commands/database.py
+++ dbt_platform_helper/commands/database.py
@@ -1,11 +1,12 @@
+import json
 import subprocess
 
 import click
 from boto3 import Session
 
 from dbt_platform_helper.commands.conduit import addon_client_is_running
-from dbt_platform_helper.commands.conduit import connect_to_addon_client_task
 from dbt_platform_helper.commands.conduit import get_cluster_arn
+from dbt_platform_helper.commands.conduit import normalise_secret_name
 from dbt_platform_helper.commands.conduit import update_parameter_with_secret
 from dbt_platform_helper.utils.application import load_application
 from dbt_platform_helper.utils.aws import get_aws_session_or_abort
@@ -67,7 +68,7 @@ def copy(source_db: str, target_db: str):
         if tag["Key"] == "copilot-environment":
             target_env = tag["Value"]
 
-    if not app or source_env or target_env:
+    if not app and source_env and target_env:
         click.secho(f"""Required tags not found.""", fg="red")
         exit(1)
 
@@ -92,14 +93,14 @@ def copy(source_db: str, target_db: str):
 
     application = load_application(app)
     cluster_arn = get_cluster_arn(application, source_env)
-    task_name = f"database-copy-{app.name}-{source_db}-{target_db}"
+    task_name = f"database-copy-{app}-{source_env}-{app}-{target_env}"
 
     if not addon_client_is_running(application, source_env, cluster_arn, task_name):
         subprocess.call(
             f"copilot task run --app {app} --env {source_env} " f"--task-group-name {task_name} "
             # f"--image public.ecr.aws/uktrade/tunnel:database-copy "
             f"--dockerfile images/tools/database-copy/Dockerfile "
-            f"--env-vars SOURCE_DB_CONNECTION_SECRET='{source_db_connection}', TARGET_DB_CONNECTION_SECRET='{target_db_connection}'"
+            f'--env-vars SOURCE_DB_CONNECTION="{json.dumps(source_db_connection)}",TARGET_DB_CONNECTION="{json.dumps(target_db_connection)}" '
             "--platform-os linux "
             "--platform-arch arm64",
             shell=True,
@@ -111,15 +112,13 @@ def copy(source_db: str, target_db: str):
         #     application, env, addon_type, addon_name, task_name, parameter_name
         # )
 
-    connect_to_addon_client_task(application, source_env, cluster_arn, task_name)
+    # connect_to_addon_client_task(application, source_env, cluster_arn, task_name)
 
 
 def _get_connection_string(session: Session, app: str, env: str, db_identifier: str) -> str:
-    addon_name = db_identifier.split(f"{app}-{env}-", 1)[1]
-    connection_string_parameter = (
-        f"/copilot/{app}/{env}/secrets/{addon_name.upper()}_READ_ONLY_USER"
-    )
-    master_secret_name = f"/copilot/{app}/{env}/secrets/{addon_name.upper()}_RDS_MASTER_ARN"
+    addon_name = normalise_secret_name(db_identifier.split(f"{app}-{env}-", 1)[1])
+    connection_string_parameter = f"/copilot/{app}/{env}/secrets/{addon_name}_READ_ONLY_USER"
+    master_secret_name = f"/copilot/{app}/{env}/secrets/{addon_name}_RDS_MASTER_ARN"
     master_secret_arn = session.client("ssm").get_parameter(
         Name=master_secret_name, WithDecryption=True
     )["Parameter"]["Value"]
