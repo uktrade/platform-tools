befab4a4ea7e74d5a7e9435309b0a336abd6a381
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-15 17:27:03 +0100

    comment out platform helper assume role script

--- regression_tests/stages/run_platform_helper_environment_generate.sh
+++ regression_tests/stages/run_platform_helper_environment_generate.sh
@@ -6,4 +6,4 @@ cd "${CODEBUILD_SRC_DIR}/demodjango-deploy"
 
 echo -e "\nRun platform-helper environment generate"
 # The command is run elsewhere in pipelines, but this gives us faster, more granular feedback
-AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment generate --name toolspr
+AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment generate --name ${TARGET_ENVIRONMENT}
