ec786888f16f1476234ac804d3259d1618ea928b
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-16 15:19:11 +0100

    export vars from assume role script

--- regression_tests/pull_request_tests.sh
+++ regression_tests/pull_request_tests.sh
@@ -11,7 +11,7 @@ source ./regression_tests/stages/set_up_git_config.sh
 
 ./regression_tests/stages/assume_platform_sandbox_role.sh
 
-./regression_tests/stages/set_up_aws_config.sh
+source ./regression_tests/stages/set_up_aws_config.sh
 
 ./regression_tests/stages/build_platform_helper.sh
 
