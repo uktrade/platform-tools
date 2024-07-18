738d4d1f55eb40f0d7ad3e1931407b6df7e790f2
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-18 17:37:10 +0100

    add set -e

--- regression_tests/stages/ensure_not_under_maintenance.sh
+++ regression_tests/stages/ensure_not_under_maintenance.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+set -e
+
 echo "y" | AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment online --app demodjango --env ${TARGET_ENVIRONMENT} || true
