7961a39dcba1567cd3adfe64d089d51c4878aabb
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-18 18:31:25 +0100

    add change directory to regression test script

--- regression_tests/stages/ensure_not_under_maintenance.sh
+++ regression_tests/stages/ensure_not_under_maintenance.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+cd "${CODEBUILD_SRC_DIR}/demodjango"
+
 echo "y" | AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment online --app demodjango --env ${TARGET_ENVIRONMENT} || true
