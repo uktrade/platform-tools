8224938ac2cf3d10cb904fc53f1088c0a95e1f6b
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-07-24 12:04:22 +0100

    add checkout to regression tests

--- regression_tests/stages/clone_demodjango.sh
+++ regression_tests/stages/clone_demodjango.sh
@@ -8,3 +8,5 @@ git clone "${GIT_CLONE_BASE_URL}/demodjango.git"
 
 cd "${CODEBUILD_SRC_DIR}/demodjango"
 echo -e "\nCurrent demodjango branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
+
+git checkout add-private-submodule
--- regression_tests/stages/clone_demodjango_deploy.sh
+++ regression_tests/stages/clone_demodjango_deploy.sh
@@ -8,3 +8,5 @@ git clone "${GIT_CLONE_BASE_URL}/demodjango-deploy.git"
 
 cd "${CODEBUILD_SRC_DIR}/demodjango-deploy"
 echo -e "\nCurrent demodjango-deploy branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
+
+git checkout add-gabe-pipeline
