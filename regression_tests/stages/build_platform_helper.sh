d66ef93d140466acaec6bdff219ec468bd783e37
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-08-05 16:15:03 +0100

    Install platform helper to build tools directory

--- regression_tests/stages/build_platform_helper.sh
+++ regression_tests/stages/build_platform_helper.sh
@@ -6,5 +6,7 @@ cd "${CODEBUILD_SRC_DIR}"
 
 echo -e "\nBuild and install platform-helper"
 poetry build --no-interaction --format sdist --no-ansi
-pip install "dist/$(ls -t1 dist | head -1)"
+mkdir ./build-tools
+cd ./build-tools
+pip install --target . "dist/$(ls -t1 dist | head -1)"
 platform-helper --version
