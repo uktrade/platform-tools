0f6da975ee431fb34e6e3f608f406d5f4f32342f
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-08-05 17:18:24 +0100

    remove version check for now

--- regression_tests/stages/build_platform_helper.sh
+++ regression_tests/stages/build_platform_helper.sh
@@ -8,5 +8,5 @@ echo -e "\nBuild and install platform-helper"
 poetry build --no-interaction --format sdist --no-ansi
 mkdir ./build-tools
 pip install --target ./build-tools --quiet "dist/$(ls -t1 dist | head -1)"
-export "PATH=./build-tools/bin:$PATH"
-platform-helper --version
+#export "PATH=./build-tools/bin:$PATH"
+#platform-helper --version
