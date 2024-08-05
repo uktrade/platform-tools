777a40ec7124452d1b6cef742b473b80967f8121
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-08-05 17:05:59 +0100

    Removed platform-tools install from buildspec; Add build-tools to path

--- regression_tests/stages/build_platform_helper.sh
+++ regression_tests/stages/build_platform_helper.sh
@@ -7,6 +7,6 @@ cd "${CODEBUILD_SRC_DIR}"
 echo -e "\nBuild and install platform-helper"
 poetry build --no-interaction --format sdist --no-ansi
 mkdir ./build-tools
-echo "dist/$(ls -t1 dist | head -1)"
 pip install --target ./build-tools --quiet "dist/$(ls -t1 dist | head -1)"
+export "PATH=./build-tools/bin:$PATH"
 platform-helper --version
