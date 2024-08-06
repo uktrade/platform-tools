c83a7d2728c5237be83c8ec54a381e2bddc2c54e
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-08-06 10:24:42 +0100

    add platform-helper to path

--- regression_tests/stages/build_platform_helper.sh
+++ regression_tests/stages/build_platform_helper.sh
@@ -8,5 +8,6 @@ echo -e "\nBuild and install platform-helper"
 poetry build --no-interaction --format sdist --no-ansi
 mkdir ./build-tools
 pip install --target ./build-tools --quiet "dist/$(ls -t1 dist | head -1)"
-#export "PATH=./build-tools/bin:$PATH"
-#platform-helper --version
+export PATH="${CODEBUILD_SRC_DIR}/build-tools/bin:$PATH"
+export PYTHONPATH="${CODEBUILD_SRC_DIR}/build-tools"
+platform-helper --version
