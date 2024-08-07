4465609ef06774a035cc4dcb732907bf6053ba09
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-08-07 09:16:01 +0100

    list contents of build-tools

--- regression_tests/stages/build_platform_helper.sh
+++ regression_tests/stages/build_platform_helper.sh
@@ -6,10 +6,16 @@ cd "${CODEBUILD_SRC_DIR}"
 
 echo -e "\nBuild and install platform-helper"
 mkdir ./build-tools
+export PATH="./build-tools/bin:$PATH"
+export PYTHONPATH="./build-tools"
+
 pip install --target ./build-tools poetry
-export PATH="${CODEBUILD_SRC_DIR}/build-tools/bin:$PATH"
-export PYTHONPATH="${CODEBUILD_SRC_DIR}/build-tools"
+
 poetry install
 poetry build --no-interaction --format sdist --no-ansi
+
 pip install --target ./build-tools "dist/$(ls -t1 dist | head -1)"
+
+ls -LR ./build-tools
+
 platform-helper --version
