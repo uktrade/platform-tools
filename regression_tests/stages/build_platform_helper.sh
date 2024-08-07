635e5db494e943611da4673cad5d3c9cf29207bd
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-08-07 09:26:34 +0100

    Revert to previous platform-helper install stage

--- regression_tests/stages/build_platform_helper.sh
+++ regression_tests/stages/build_platform_helper.sh
@@ -5,17 +5,9 @@ set -e
 cd "${CODEBUILD_SRC_DIR}"
 
 echo -e "\nBuild and install platform-helper"
-mkdir ./build-tools
-export PATH="./build-tools/bin:$PATH"
-export PYTHONPATH="./build-tools"
-
-pip install --target ./build-tools poetry
-
-poetry install
 poetry build --no-interaction --format sdist --no-ansi
-
+mkdir ./build-tools
 pip install --target ./build-tools "dist/$(ls -t1 dist | head -1)"
-
-ls -LR ./build-tools
-
+export PATH="${CODEBUILD_SRC_DIR}/build-tools/bin:$PATH"
+export PYTHONPATH="${CODEBUILD_SRC_DIR}/build-tools"
 platform-helper --version
