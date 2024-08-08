646b6a07a4b2df6473ea284c5d5e62e582fa3bd0
Kate Sugden <kate.sugden@madetech.com>
2024-08-08 16:51:54 +0100

    Add test for listing images in chronological order

--- tests/platform_helper/test_command_codebase.py
+++ tests/platform_helper/test_command_codebase.py
@@ -605,6 +605,46 @@ class TestCodebaseList:
             in result.output
         )
 
+    @patch("dbt_platform_helper.commands.codebase.get_aws_session_or_abort")
+    def test_lists_codebases_with_disordered_images_in_chronological_order(self, get_aws_session_or_abort):
+        client = mock_aws_client(get_aws_session_or_abort)
+        client.get_parameters_by_path.return_value = {
+            "Parameters": [
+                {"Value": json.dumps({"name": "application", "repository": "uktrade/example"})}
+            ],
+        }
+        client.get_paginator.return_value.paginate.return_value = [
+            {
+                "imageDetails": [
+                    {
+                        "imageTags": ["latest", "tag-latest", "tag-1.0", "commit-4"],
+                        "imagePushedAt": datetime(2023, 11, 4, 00, 00, 00),
+                    },
+                    {
+                        "imageTags": ["branch-main", "commit-2"],
+                        "imagePushedAt": datetime(2023, 11, 2, 00, 00, 00),
+                    },
+                ]
+            },
+            {
+                "imageDetails": [
+                    {
+                        "imageTags": ["commit-1"],
+                        "imagePushedAt": datetime(2023, 11, 1, 00, 00, 00),
+                    },
+                    {
+                        "imageTags": ["commit-3"],
+                        "imagePushedAt": datetime(2023, 11, 3, 00, 00, 00),
+                    },
+                ]
+            }
+        ]
+        from dbt_platform_helper.commands.codebase import list
+
+        result = CliRunner().invoke(list, ["--app", "test-application", "--with-images"])
+        
+        assert result.output.index("commit/4") < result.output.index("commit/3") < result.output.index("commit/2") < result.output.index("commit/1")
+
     @patch(
         "dbt_platform_helper.commands.codebase.load_application",
         side_effect=ApplicationNotFoundError,
