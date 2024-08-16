25a22eb6e8c68d89bbf26415f2d182521cd19533
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-08-16 12:20:00 +0100

    add ssm parameter logic and temporarily comment out other regression tests

--- regression_tests/stages/run_demodjango_smoke_tests.sh
+++ regression_tests/stages/run_demodjango_smoke_tests.sh
@@ -7,5 +7,12 @@ cd "${CODEBUILD_SRC_DIR}/demodjango"
 echo -e "\nInstall dependencies"
 poetry install
 
+echo -e "\nFetch parameters from AWS Parameter Store"
+USERNAME=$(aws ssm get-parameter --name "/copilot/demodjango/${TARGET_ENVIRONMENT}/secrets/BASIC_AUTH_USERNAME" --with-decryption --query "Parameter.Value" --output text)
+PASSWORD=$(aws ssm get-parameter --name "/copilot/demodjango/${TARGET_ENVIRONMENT}/secrets/BASIC_AUTH_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
+
+export BASIC_AUTH_USERNAME="$USERNAME"
+export BASIC_AUTH_PASSWORD="$PASSWORD"
+
 echo -e "\nRun smoke tests"
 ./tests/browser/run.sh ${TARGET_ENVIRONMENT} smoke
