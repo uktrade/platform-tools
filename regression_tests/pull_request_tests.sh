2280070d3f0e5378dc2faeff6df9ada2e6a4b26a
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-08-16 13:29:38 +0100

    uncomment regression tests

--- regression_tests/pull_request_tests.sh
+++ regression_tests/pull_request_tests.sh
@@ -11,29 +11,29 @@ source ./regression_tests/stages/set_up_git_config.sh
 
 ./regression_tests/stages/set_up_aws_config.sh
 
-# ./regression_tests/stages/build_platform_helper.sh
+./regression_tests/stages/build_platform_helper.sh
 
-# ./regression_tests/stages/clone_demodjango_deploy.sh
+./regression_tests/stages/clone_demodjango_deploy.sh
 
 ./regression_tests/stages/clone_demodjango.sh
 
-# ./regression_tests/stages/run_platform_helper_environment_generate.sh
+./regression_tests/stages/run_platform_helper_environment_generate.sh
 
-# ./regression_tests/stages/run_platform_helper_generate.sh
+./regression_tests/stages/run_platform_helper_generate.sh
 
 # Todo: DBTP-1073 Include deploying environment pipelines in regression tests
 
 # Todo: DBTP-1074 Include deploying codebase pipelines in regression tests
 
-# ./regression_tests/stages/run_environment_pipeline.sh
+./regression_tests/stages/run_environment_pipeline.sh
 
-# ./regression_tests/stages/run_codebase_pipeline.sh
+./regression_tests/stages/run_codebase_pipeline.sh
 
-# ./regression_tests/stages/ensure_not_under_maintenance.sh
+./regression_tests/stages/ensure_not_under_maintenance.sh
 
 ./regression_tests/stages/run_demodjango_smoke_tests.sh
 
-# ./regression_tests/stages/run_maintenance_page_tests.sh
+./regression_tests/stages/run_maintenance_page_tests.sh
 
 # Todo: DBTP-1076 Ensure regression tests builds run one at a time
 
