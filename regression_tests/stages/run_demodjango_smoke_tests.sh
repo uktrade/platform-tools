4aa04dbb0e414e89928c309f9935cf42a5c4049e
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-08-16 16:55:21 +0100

    make vars available to different regression tests

--- regression_tests/stages/run_demodjango_smoke_tests.sh
+++ regression_tests/stages/run_demodjango_smoke_tests.sh
@@ -7,18 +7,6 @@ cd "${CODEBUILD_SRC_DIR}/demodjango"
 echo -e "\nInstall dependencies"
 poetry install
 
-echo -e "\nAssume platform-sandbox role to trigger environment pipeline"
-assumed_role=$(aws sts assume-role \
-    --role-arn "arn:aws:iam::$PLATFORM_SANDBOX_AWS_ACCOUNT_ID:role/regression-tests-assume-role-for-platform-tools" \
-    --role-session-name "pull-request-regression-tests-$(date +%s)")
-PLATFORM_SANDBOX_AWS_ACCESS_KEY_ID=$(echo $assumed_role | jq -r .Credentials.AccessKeyId)
-PLATFORM_SANDBOX_AWS_SECRET_ACCESS_KEY=$(echo $assumed_role | jq -r .Credentials.SecretAccessKey)
-PLATFORM_SANDBOX_AWS_SESSION_TOKEN=$(echo $assumed_role | jq -r .Credentials.SessionToken)
-
-export AWS_ACCESS_KEY_ID=$PLATFORM_SANDBOX_AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$PLATFORM_SANDBOX_AWS_SECRET_ACCESS_KEY
-export AWS_SESSION_TOKEN=$PLATFORM_SANDBOX_AWS_SESSION_TOKEN
-
 echo -e "\nFetch parameters from AWS Parameter Store"
 USERNAME=$(aws ssm get-parameter --name "/copilot/demodjango/${TARGET_ENVIRONMENT}/secrets/BASIC_AUTH_USERNAME" --with-decryption --query "Parameter.Value" --output text)
 PASSWORD=$(aws ssm get-parameter --name "/copilot/demodjango/${TARGET_ENVIRONMENT}/secrets/BASIC_AUTH_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
