6e186ca74fc7459c780c712cdf885ca3940b426c
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-09-04 17:58:50 +0100

    Put the

--- regression_tests/stages/ensure_not_under_maintenance.sh
+++ regression_tests/stages/ensure_not_under_maintenance.sh
@@ -6,5 +6,4 @@ echo -e "\n\nEnsure environment not under maintenance\n"
 
 cd "${CODEBUILD_SRC_DIR}/demodjango"
 
-echo "y" | AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment online --app demodjango --env ${TARGET_ENVIRONMENT}
-# || true
+echo "y" | AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment online --app demodjango --env ${TARGET_ENVIRONMENT} || true
