d69c527dc11261e503292009a6a87f83f7167aa2
Kate Sugden <kate.sugden@madetech.com>
2024-09-05 10:07:16 +0100

    formatting

--- dbt_platform_helper/utils/validation.py
+++ dbt_platform_helper/utils/validation.py
@@ -274,9 +274,16 @@ LIFECYCLE_RULE = {
 }
 
 DATA_IMPORT = {
-    Optional("source_kms_key_arn"): Regex(r"^arn:aws:kms:.*:\d{12}:(key|alias).*", error = "source_kms_key_arn must be a valid arn for a kms key"),
-    "source_bucket_arn": Regex(r"^arn:aws:s3::.*", error = "source_bucket_arn must be a valid arn for an s3 bucket"),
-    "worker_role_arn": Regex(r"^arn:aws:iam::\d{12}:role/.*", error = "worker_role_arn must be a valid arn for an iam role"),
+    Optional("source_kms_key_arn"): Regex(
+        r"^arn:aws:kms:.*:\d{12}:(key|alias).*",
+        error="source_kms_key_arn must be a valid arn for a kms key",
+    ),
+    "source_bucket_arn": Regex(
+        r"^arn:aws:s3::.*", error="source_bucket_arn must be a valid arn for an s3 bucket"
+    ),
+    "worker_role_arn": Regex(
+        r"^arn:aws:iam::\d{12}:role/.*", error="worker_role_arn must be a valid arn for an iam role"
+    ),
 }
 
 DATA_MIGRATION = {
