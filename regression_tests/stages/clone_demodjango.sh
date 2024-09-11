c9cd97bc4b79a795cfba519b0ea0e5ca71dd26db
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2024-09-11 10:27:58 +0100

    fix demodjango branch check

--- regression_tests/stages/clone_demodjango.sh
+++ regression_tests/stages/clone_demodjango.sh
@@ -5,9 +5,11 @@ set -e
 echo -e "\n\nClone demodjango\n"
 
 git clone "${GIT_CLONE_BASE_URL}/demodjango.git"
-if [[ -z "${DEMODJANGO_BRANCH}" ]]; then
+
+cd "${CODEBUILD_SRC_DIR}/demodjango"
+if [[ -n "${DEMODJANGO_BRANCH}" ]]; then
+    echo -e "\nChecking out branch ${DEMODJANGO_BRANCH}\n"
     git checkout "${DEMODJANGO_BRANCH}"
 fi
 
-cd "${CODEBUILD_SRC_DIR}/demodjango"
 echo -e "\nCurrent demodjango branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
