fe0e476edf4298c23336f89ebd9a4686d3d7c75a
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2024-09-11 09:38:05 +0100

    add branch option to demodjango

--- regression_tests/stages/clone_demodjango.sh
+++ regression_tests/stages/clone_demodjango.sh
@@ -5,6 +5,9 @@ set -e
 echo -e "\n\nClone demodjango\n"
 
 git clone "${GIT_CLONE_BASE_URL}/demodjango.git"
+if [[ -z "${DEMODJANGO_BRANCH}" ]]; then
+    git checkout "${DEMODJANGO_BRANCH}"
+fi
 
 cd "${CODEBUILD_SRC_DIR}/demodjango"
 echo -e "\nCurrent demodjango branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
--- regression_tests/stages/run_maintenance_page_tests.sh
+++ regression_tests/stages/run_maintenance_page_tests.sh
@@ -31,3 +31,33 @@ echo "y" | AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true p
 echo -e "\nCheck we can view the page again (running smoke tests)"
 cd "${CODEBUILD_SRC_DIR}/demodjango"
 ./smoke_tests.sh ${TARGET_ENVIRONMENT} smoke
+
+################################################################ CDN tests section
+CODEBUILD_SRC_DIR="/Users/jayesh/WebOps/"
+TARGET_ENVIRONMENT="jayesh"
+export IS_CDN=True
+
+echo -e "\nNow testing CDN endpoint"
+
+URL="${TARGET_ENVIRONMENT}.demodjango.uktrade.digital/"
+
+echo -e "\nCheck we can view the page (running smoke tests)"
+cd "${CODEBUILD_SRC_DIR}/demodjango"
+./tests/browser/run.sh ${URL} smoke/test_landing_page.py
+
+echo -e "\nRunning offline command"
+OUTPUT=$(echo "y" | AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment offline --app demodjango --env "${TARGET_ENVIRONMENT}" --svc web-cdn --vpc platform-sandbox-dev)
+
+echo "$OUTPUT"
+MAINTENANCE_PAGE_BYPASS_VALUE=$(echo "$OUTPUT" | grep -oP 'Bypass-Key` header with value \K[^\s]+')
+
+echo -e "\nCheck maintenance page is working and that we can view the site with Bypass-Key header"
+cd "${CODEBUILD_SRC_DIR}/demodjango"
+./tests/browser/run.sh ${URL} maintenance_pages ${MAINTENANCE_PAGE_BYPASS_VALUE}
+
+echo -e "\nRunning online command"
+echo "y" | AWS_PROFILE=platform-sandbox PLATFORM_TOOLS_SKIP_VERSION_CHECK=true platform-helper environment online --app demodjango --env ${TARGET_ENVIRONMENT}
+
+echo -e "\nCheck we can view the page again (running smoke tests)"
+cd "${CODEBUILD_SRC_DIR}/demodjango"
+./tests/browser/run.sh ${URL} smoke/test_landing_page.py
