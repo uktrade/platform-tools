e5468787e3389a50bdee5fdf29f8aa3763235470
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-09-11 10:05:49 +0100

    Revert Run pipeline-demodjango-application-dbtp-1291-regression-tests-temp

--- regression_tests/stages/run_codebase_pipeline.sh
+++ regression_tests/stages/run_codebase_pipeline.sh
@@ -8,6 +8,4 @@ cd "${CODEBUILD_SRC_DIR}"
 
 source ./regression_tests/src/run_pipeline.sh
 
-# Todo: Come back to this...
-# run_pipeline "Codebase" "pipeline-demodjango-application-${TARGET_ENVIRONMENT}" 1800
-run_pipeline "Codebase" "pipeline-demodjango-application-dbtp-1291-regression-tests-temp" 1800
+run_pipeline "Codebase" "pipeline-demodjango-application-${TARGET_ENVIRONMENT}" 1800
