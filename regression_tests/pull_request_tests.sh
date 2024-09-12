d83d89093327ca08c1706e8870c75a475329f26c
chiaramapellimt <chiara.mapelli@madetech.com>
2024-09-12 13:09:45 +0100

    Refactoring repo directories

--- regression_tests/pull_request_tests.sh
+++ regression_tests/pull_request_tests.sh
@@ -9,17 +9,17 @@ export TARGET_ENVIRONMENT=${TARGET_ENVIRONMENT:-toolspr}
 
 echo -e "\nCurrent platform-tools branch/commit: $(git rev-parse --abbrev-ref HEAD)/$(git rev-parse HEAD)"
 
-source ./regression_tests/stages/set_up_git_config.sh
+source ./regression_tests/stages/build/set_up_git_config.sh
 
-./regression_tests/stages/set_up_aws_config.sh
+./regression_tests/stages/deploy/set_up_aws_config.sh
 
-./regression_tests/stages/build_platform_helper.sh
+./regression_tests/stages/build/build_platform_helper.sh
 
-./regression_tests/stages/clone_demodjango_deploy.sh
+./regression_tests/stages/build/clone_demodjango_deploy.sh
 
-./regression_tests/stages/clone_demodjango.sh
+./regression_tests/stages/build/clone_demodjango.sh
 
-./regression_tests/stages/run_platform_helper_environment_generate.sh
+./regression_tests/stages/deploy/run_platform_helper_environment_generate.sh
 
 ./regression_tests/stages/run_platform_helper_generate.sh
 
@@ -27,15 +27,15 @@ source ./regression_tests/stages/set_up_git_config.sh
 
 # Todo: DBTP-1074 Include deploying codebase pipelines in regression tests
 
-./regression_tests/stages/run_environment_pipeline.sh
+./regression_tests/stages/deploy/un_environment_pipeline.sh
 
-./regression_tests/stages/run_codebase_pipeline.sh
+./regression_tests/stages/deploy/run_codebase_pipeline.sh
 
-./regression_tests/stages/ensure_not_under_maintenance.sh
+./regression_tests/stages/test/ensure_not_under_maintenance.sh
 
-source ./regression_tests/stages/run_demodjango_smoke_tests.sh
+source ./regression_tests/stages/test/run_demodjango_smoke_tests.sh
 
-./regression_tests/stages/run_maintenance_page_tests.sh
+./regression_tests/stages/test/run_maintenance_page_tests.sh
 
 # Todo: DBTP-1076 Ensure regression tests builds run one at a time
 
