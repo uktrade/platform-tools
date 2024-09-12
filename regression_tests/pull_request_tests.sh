07eca677e0770385d892619b0ec1384ffcfdda21
Kate Sugden <kate.sugden@madetech.com>
2024-09-12 12:43:40 +0100

    WIP: re-add stub regression test code

--- regression_tests/pull_request_tests.sh
+++ regression_tests/pull_request_tests.sh
@@ -35,6 +35,9 @@ source ./regression_tests/stages/run_demodjango_smoke_tests.sh
 
 ./regression_tests/stages/run_maintenance_page_tests.sh
 
+./regression_tests/stages/run_s3_data_migration_tests.sh || echo "S3 data migration test failed, but continuing build..."
+
+
 # Todo: DBTP-1076 Ensure regression tests builds run one at a time
 
 # Todo: DBTP-1075 Trigger regression tests off all four main DBT Platform Codebases
--- tests/platform_helper/utils/test_platform_config.py
+++ tests/platform_helper/utils/test_platform_config.py
@@ -5,7 +5,7 @@ import yaml
 
 from dbt_platform_helper.constants import PLATFORM_CONFIG_FILE
 from dbt_platform_helper.utils.platform_config import get_environment_pipeline_names
-from dbt_platform_helper.utils.platform_config import is_terraform_project
+from dbt_platform_helper.utils.platform_config import is_terraform_project, is_s3_bucket_data_migration_enabled
 
 
 def test_get_environment_pipeline_names(fakefs, valid_platform_config):
@@ -36,3 +36,14 @@ def test_is_terraform_project(fakefs, platform_config_content, expected_result):
     fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=platform_config_content)
 
     assert is_terraform_project() == expected_result
+    
+def test_is_s3_bucket_data_migration_enabled(fakefs, valid_platform_config):
+    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=yaml.dump(valid_platform_config))
+
+    assert is_s3_bucket_data_migration_enabled("s3-data-migration")
+
+
+def test_is_s3_bucket_data_migration_not_enabled(fakefs, valid_platform_config):
+    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=yaml.dump(valid_platform_config))
+
+    assert not is_s3_bucket_data_migration_enabled("test-app-s3-bucket")
