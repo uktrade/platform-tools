967a035928177e82a5ed59ae85d602ec17cfc7ff
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-09-13 12:34:58 +0100

    move demodjango poetry install into the build stage

--- regression_tests/stages/run_demodjango_smoke_tests.sh
+++ regression_tests/stages/run_demodjango_smoke_tests.sh
@@ -6,8 +6,6 @@ echo -e "\n\nInstall dependencies\n"
 
 cd "${CODEBUILD_SRC_DIR}/demodjango"
 
-poetry install
-
 echo -e "\nAssume platform-sandbox role to access basic auth secrets"
 assumed_role=$(aws sts assume-role \
     --role-arn "arn:aws:iam::$PLATFORM_SANDBOX_AWS_ACCOUNT_ID:role/regression-tests-assume-role-for-platform-tools" \
