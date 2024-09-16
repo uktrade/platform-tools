116a9a07e7a481a1cf788bd364d7dc63a8b09b47
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-09-16 16:18:08 +0100

    Tidy up commands in the entrypoint

--- images/tools/database-copy2/entrypoint.sh
+++ images/tools/database-copy2/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "${DATA_COPY_OPERATION:-DUMP}" != "RESTORE" ]
 then
   echo "Starting data dump"
-  pg_dump --format tar $DB_CONNECTION_STRING | gzip > data_dump.tgz
+  pg_dump --format tar "${DB_CONNECTION_STRING}" | gzip > data_dump.tgz
   aws s3 cp data_dump.tgz s3://data-copy-poc-ant/
   echo "Stopping data dump"
 else
@@ -11,6 +11,6 @@ else
   aws s3 cp s3://data-copy-poc-ant/data_dump.tgz data_dump.tgz
   ls -al
   gunzip data_dump.tgz
-  pg_restore --dbname $DB_CONNECTION_STRING --format tar data_dump.tar
+  pg_restore --dbname "${DB_CONNECTION_STRING}" --format tar data_dump.tar
   echo "Stopping data restore"
 fi
