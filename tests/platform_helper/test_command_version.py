026dfe2e85fdfee565f654439d186996d1816d34
Kate Sugden <kate.sugden@madetech.com>
2024-09-20 15:05:43 +0100

    Move test back into same file and add fixture functions to initialise helper command

--- tests/platform_helper/test_command_version.py
+++ tests/platform_helper/test_command_version.py
@@ -1,22 +1,42 @@
+import pytest
 import re
+import yaml
+
 from pathlib import Path
 from unittest.mock import patch
-
-import yaml
 from click.testing import CliRunner
 
-from dbt_platform_helper.commands.version import get_platform_helper_for_project
 from dbt_platform_helper.constants import PLATFORM_CONFIG_FILE
+
+
+@pytest.fixture
+def get_platform_helper_for_project_with_valid_config(fakefs, valid_platform_config):
+    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=yaml.dump(valid_platform_config))
+    from dbt_platform_helper.commands.version import get_platform_helper_for_project
+
+    return get_platform_helper_for_project
+
+
+@pytest.fixture
+def get_platform_helper_for_project_with_invalid_config(
+    fakefs, invalid_platform_config_with_platform_version_overrides
+):
+    fakefs.create_file(
+        Path(PLATFORM_CONFIG_FILE),
+        contents=yaml.dump(invalid_platform_config_with_platform_version_overrides),
+    )
+    from dbt_platform_helper.commands.version import get_platform_helper_for_project
+
+    return get_platform_helper_for_project
 
 
 @patch("dbt_platform_helper.commands.version.get_required_platform_helper_version")
 def test_calls_versioning_function_and_prints_returned_version(
     mock_get_required_platform_helper_version,
-    fakefs,
-    valid_platform_config,
 ):
-    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=yaml.dump(valid_platform_config))
     mock_get_required_platform_helper_version.return_value = "1.2.3"
+
+    from dbt_platform_helper.commands.version import get_platform_helper_for_project
 
     result = CliRunner().invoke(get_platform_helper_for_project, [])
 
@@ -29,11 +49,10 @@ def test_calls_versioning_function_and_prints_returned_version(
 @patch("dbt_platform_helper.commands.version.get_required_platform_helper_version")
 def test_calls_versioning_function_and_prints_returned_version_with_pipeline_override(
     mock_get_required_platform_helper_version,
-    fakefs,
-    valid_platform_config,
 ):
-    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=yaml.dump(valid_platform_config))
     mock_get_required_platform_helper_version.return_value = "1.2.3"
+
+    from dbt_platform_helper.commands.version import get_platform_helper_for_project
 
     result = CliRunner().invoke(get_platform_helper_for_project, ["--pipeline", "main"])
 
@@ -43,29 +62,57 @@ def test_calls_versioning_function_and_prints_returned_version_with_pipeline_ove
     assert re.match(r"\s*1\.2\.3\s*", result.output)
 
 
-def test_works_with_with_incompatible_config_version(
-    fakefs,
-    invalid_platform_config_with_platform_version_overrides,
+def test_works_with_invalid_config_with_pipeline_override(
+    get_platform_helper_for_project_with_invalid_config,
 ):
-    fakefs.create_file(
-        Path(PLATFORM_CONFIG_FILE),
-        contents=yaml.dump(invalid_platform_config_with_platform_version_overrides),
+    result = CliRunner().invoke(
+        get_platform_helper_for_project_with_invalid_config, ["--pipeline", "prod-main"]
     )
 
-    result = CliRunner().invoke(get_platform_helper_for_project, [])
+    assert result.exit_code == 0
+    assert result.output == "9.0.9\n"
+
+
+def test_works_with_with_incompatible_config_version(
+    get_platform_helper_for_project_with_invalid_config,
+):
+
+    result = CliRunner().invoke(get_platform_helper_for_project_with_invalid_config, [])
 
     assert result.exit_code == 0
     assert result.output == "1.2.3\n"
 
 
 def test_fail_if_pipeline_option_is_not_a_pipeline(
-    fakefs,
-    valid_platform_config,
+    get_platform_helper_for_project_with_valid_config,
 ):
-    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=yaml.dump(valid_platform_config))
-
-    result = CliRunner().invoke(get_platform_helper_for_project, ["--pipeline", "bogus"])
+    result = CliRunner().invoke(
+        get_platform_helper_for_project_with_valid_config, ["--pipeline", "bogus"]
+    )
 
     assert result.exit_code != 0
     assert "'bogus' is not one of" in result.output
     assert "'main'" in result.output
+
+
+def test_still_fails_if_pipeline_option_is_not_a_pipeline_with_invalid_config(
+    get_platform_helper_for_project_with_invalid_config,
+):
+    result = CliRunner().invoke(
+        get_platform_helper_for_project_with_invalid_config, ["--pipeline", "bogus"]
+    )
+
+    assert result.exit_code != 0
+    assert "'bogus' is not " in result.output
+    assert "'prod-main'" in result.output
+
+
+def test_pipeline_override_with_invalid_config(
+    get_platform_helper_for_project_with_invalid_config,
+):
+    result = CliRunner().invoke(
+        get_platform_helper_for_project_with_invalid_config, ["--pipeline", "prod-main"]
+    )
+
+    assert result.exit_code == 0
+    assert result.output == "9.0.9\n"
