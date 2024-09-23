cd54e62f8b0df12fb08f85648a27d5db487b68a0
Kate Sugden <kate.sugden@madetech.com>
2024-09-23 11:08:48 +0100

    use internal method

--- dbt_platform_helper/commands/version.py
+++ dbt_platform_helper/commands/version.py
@@ -62,16 +62,4 @@ def _get_platform_helper_for_project(pipeline):
     help="Take into account platform-tools version overrides in the specified pipeline",
 )
 def get_platform_helper_for_project(pipeline):
-    """
-    Version precedence is in this order:
-        - if the --pipeline option is supplied, the version in 'platform-config.yml' in:
-            environment_pipelines:
-                <pipeline>:
-                    ...
-                    versions:
-                        platform-helper
-        - The version from default_versions/platform-helper in 'platform-config.yml'
-        - Fall back on the version in the deprecated '.platform-helper-version' file
-    """
-    required_version = get_required_platform_helper_version(pipeline)
-    click.secho(required_version)
+    _get_platform_helper_for_project(pipeline)
