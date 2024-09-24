f57da661181ce0dbcb166beaeac8e566af5561af
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-09-24 13:55:12 +0100

    replace_static_paths_in_shebangs_etc

--- regression_tests/temp.sh
+++ regression_tests/temp.sh
@@ -17,13 +17,23 @@ run_checks() {
     run_command "pip list"
 }
 
-replace_static_path() {
+replace_static_path_in_venv_activate() {
     static_path="\"$(pwd)/venv_temp1/venv\""
     dynamic_path='"$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" \&\& pwd)"'
     sedcmd=(sed -i '' "s|${static_path}|${dynamic_path}|g" venv_temp1/venv/bin/activate)
     "${sedcmd[@]}"
-    sedcmd=(sed -i '' '1s/.*/#!\/usr\/bin\/env python/' venv_temp1/venv/bin/pip*)
-    "${sedcmd[@]}"
+}
+
+replace_static_paths_in_shebangs_etc() {
+    # static_path="/codebuild/output/src[^/]+/src/codestar-connections.eu-west-2.amazonaws.com/git-http/763451185160/eu-west-2/[^/]+/uktrade/platform-tools/venv"
+    # todo: lose the hardcoded venv_temp1 in old path
+    old_path="/Users/willgibson/Dev/DBT/uktrade/platform-tools/regression_tests/venv_temp1/venv"
+    new_path="/Users/willgibson/Dev/DBT/uktrade/platform-tools/regression_tests/venv_temp2/venv"
+    files=$(grep -RiIl "$old_path" venv_temp2/venv)
+    while IFS= read -r file; do
+        sedcmd=(sed -i '' "s|$old_path|$new_path|g" "$file")
+        "${sedcmd[@]}"
+    done <<< "$files"
 }
 
 run_command "cd regression_tests"
@@ -32,7 +42,7 @@ run_command "rm -rf venv_temp*"
 
 echo -e "\n\nDo venv1"
 run_command "python -m venv --copies venv_temp1/venv"
-run_command "replace_static_path"
+run_command "replace_static_path_in_venv_activate"
 run_command "source venv_temp1/venv/bin/activate"
 run_checks
 
@@ -42,9 +52,7 @@ run_command "which python"
 run_command "which pip"
 
 echo -e "\n\nDo venv2"
-#run_command "python -m venv --copies venv_temp2/venv"
-#run_command "rm -rf venv_temp2/venv/lib"
-run_command "cp -R venv_temp1 venv_temp2"
-#run_command "rm -rf venv_temp1"
+run_command "mv venv_temp1 venv_temp2"
+run_command "replace_static_paths_in_shebangs_etc"
 run_command "source venv_temp2/venv/bin/activate"
 run_checks
