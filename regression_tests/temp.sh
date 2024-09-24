19ce48fe43a3256f2f0c80569f7eb718d015319c
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-09-24 14:51:19 +0100

    Using old path from file

--- regression_tests/temp.sh
+++ regression_tests/temp.sh
@@ -17,19 +17,31 @@ run_checks() {
     run_command "pip list"
 }
 
+write_static_path_to_file() {
+    static_path="$(pwd)/venv_temp1/venv"
+    echo "$static_path" > "${static_path}/.venv_path"
+}
+
+get_old_path() {
+    cat venv_temp1/venv/.venv_path
+}
+
+get_old_path_2() {
+    cat venv_temp2/venv/.venv_path
+}
+
 replace_static_path_in_venv_activate() {
-    static_path="\"$(pwd)/venv_temp1/venv\""
+    static_path="\"$(get_old_path)\""
     dynamic_path='"$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" \&\& pwd)"'
     sedcmd=(sed -i '' "s|${static_path}|${dynamic_path}|g" venv_temp1/venv/bin/activate)
     "${sedcmd[@]}"
 }
 
 replace_static_paths_in_shebangs_etc() {
-    # static_path="/codebuild/output/src[^/]+/src/codestar-connections.eu-west-2.amazonaws.com/git-http/763451185160/eu-west-2/[^/]+/uktrade/platform-tools/venv"
-    # todo: lose the hardcoded venv_temp1 in old path, perhaps we can drop this into a file in the artifacts
-    old_path="/Users/willgibson/Dev/DBT/uktrade/platform-tools/regression_tests/venv_temp1/venv"
+    old_path="$(get_old_path_2)"
+    # new_path="/codebuild/output/src[^/]+/src/codestar-connections.eu-west-2.amazonaws.com/git-http/763451185160/eu-west-2/[^/]+/uktrade/platform-tools/venv"
     new_path="/Users/willgibson/Dev/DBT/uktrade/platform-tools/regression_tests/venv_temp2/venv"
-    files=$(grep -RiIl "$old_path" venv_temp2/venv)
+    files=$(grep -RiIl "$old_path" "$new_path")
     while IFS= read -r file; do
         sedcmd=(sed -i '' "s|$old_path|$new_path|g" "$file")
         "${sedcmd[@]}"
@@ -42,6 +54,7 @@ run_command "rm -rf venv_temp*"
 
 echo -e "\n\nDo venv1"
 run_command "python -m venv --copies venv_temp1/venv"
+run_command "write_static_path_to_file"
 run_command "replace_static_path_in_venv_activate"
 run_command "source venv_temp1/venv/bin/activate"
 run_checks
