24c3d6ae12243f60cc5019b6208d59e0a1b8b45d
Kate Sugden <kate.sugden@madetech.com>
2024-09-25 06:41:08 +0100

    move tests into classes

--- tests/platform_helper/test_command_version.py
+++ tests/platform_helper/test_command_version.py
@@ -10,83 +10,67 @@ from dbt_platform_helper.commands.version import VersionCommand
 from dbt_platform_helper.constants import PLATFORM_CONFIG_FILE
 
 
-@patch("dbt_platform_helper.commands.version.get_required_platform_helper_version")
-def test_calls_versioning_function_and_prints_returned_version(
-    mock_get_required_platform_helper_version,
-):
-    mock_get_required_platform_helper_version.return_value = "1.2.3"
+@pytest.mark.usefixtures("create_invalid_platform_config_file")
+class TestVersionCommandWithInvalidConfig:
+    def test_works_with_invalid_config(self):
+        command = VersionCommand().command
+        result = CliRunner().invoke(command, [])
 
-    command = VersionCommand().command
-    result = CliRunner().invoke(command, [])
+        assert result.exit_code == 0
+        assert result.output == "1.2.3\n"
 
-    assert len(mock_get_required_platform_helper_version.mock_calls) == 1
-    assert mock_get_required_platform_helper_version.mock_calls[0].args == (None,)
-    assert result.exit_code == 0
-    assert re.match(r"\s*1\.2\.3\s*", result.output)
+    def test_pipeline_override_with_invalid_config(self):
+        command = VersionCommand().command
+        result = CliRunner().invoke(command, ["--pipeline", "prod-main"])
 
+        assert result.exit_code == 0
+        assert result.output == "9.0.9\n"
 
-@patch("dbt_platform_helper.commands.version.get_required_platform_helper_version")
-def test_calls_versioning_function_and_prints_returned_version_with_pipeline_override(
-    mock_get_required_platform_helper_version,
-):
-    mock_get_required_platform_helper_version.return_value = "1.2.3"
+    def test_fails_if_pipeline_option_is_not_a_pipeline_with_invalid_config(self):
+        command = VersionCommand().command
+        result = CliRunner().invoke(command, ["--pipeline", "bogus"])
 
-    command = VersionCommand().command
-    result = CliRunner().invoke(command, ["--pipeline", "main"])
-
-    assert len(mock_get_required_platform_helper_version.mock_calls) == 1
-    assert mock_get_required_platform_helper_version.mock_calls[0].args == ("main",)
-    assert result.exit_code == 0
-    assert re.match(r"\s*1\.2\.3\s*", result.output)
+        assert result.exit_code != 0
+        assert "'bogus' is not " in result.output
+        assert "'prod-main'" in result.output
 
 
-def test_works_with_invalid_config_with_pipeline_override(
-    create_invalid_platform_config_file,
-):
-    command = VersionCommand().command
-    result = CliRunner().invoke(command, ["--pipeline", "prod-main"])
+@pytest.mark.usefixtures("create_valid_platform_config_file")
+class TestVersionCommandWithValidConfig:
+    @patch("dbt_platform_helper.commands.version.get_required_platform_helper_version")
+    def test_calls_versioning_function_and_prints_returned_version(
+        self,
+        mock_get_required_platform_helper_version,
+    ):
+        mock_get_required_platform_helper_version.return_value = "1.2.3"
 
-    assert result.exit_code == 0
-    assert result.output == "9.0.9\n"
+        command = VersionCommand().command
+        result = CliRunner().invoke(command, [])
 
+        assert len(mock_get_required_platform_helper_version.mock_calls) == 1
+        assert mock_get_required_platform_helper_version.mock_calls[0].args == (None,)
+        assert result.exit_code == 0
+        assert re.match(r"\s*1\.2\.3\s*", result.output)
 
-def test_works_with_with_incompatible_config_version(
-    create_invalid_platform_config_file,
-):
-    command = VersionCommand().command
-    result = CliRunner().invoke(command, [])
+    @patch("dbt_platform_helper.commands.version.get_required_platform_helper_version")
+    def test_calls_versioning_function_and_prints_returned_version_with_pipeline_override(
+        self,
+        mock_get_required_platform_helper_version,
+    ):
+        mock_get_required_platform_helper_version.return_value = "1.2.3"
 
-    assert result.exit_code == 0
-    assert result.output == "1.2.3\n"
+        command = VersionCommand().command
+        result = CliRunner().invoke(command, ["--pipeline", "main"])
 
+        assert len(mock_get_required_platform_helper_version.mock_calls) == 1
+        assert mock_get_required_platform_helper_version.mock_calls[0].args == ("main",)
+        assert result.exit_code == 0
+        assert re.match(r"\s*1\.2\.3\s*", result.output)
 
-def test_fail_if_pipeline_option_is_not_a_pipeline(
-    create_valid_platform_config_file,
-):
-    command = VersionCommand().command
-    result = CliRunner().invoke(command, ["--pipeline", "bogus"])
+    def test_fail_if_pipeline_option_is_not_a_pipeline(self):
+        command = VersionCommand().command
+        result = CliRunner().invoke(command, ["--pipeline", "bogus"])
 
-    assert result.exit_code != 0
-    assert "'bogus' is not one of" in result.output
-    assert "'main'" in result.output
-
-
-def test_still_fails_if_pipeline_option_is_not_a_pipeline_with_invalid_config(
-    create_invalid_platform_config_file,
-):
-    command = VersionCommand().command
-    result = CliRunner().invoke(command, ["--pipeline", "bogus"])
-
-    assert result.exit_code != 0
-    assert "'bogus' is not " in result.output
-    assert "'prod-main'" in result.output
-
-
-def test_pipeline_override_with_invalid_config(
-    create_invalid_platform_config_file,
-):
-    command = VersionCommand().command
-    result = CliRunner().invoke(command, ["--pipeline", "prod-main"])
-
-    assert result.exit_code == 0
-    assert result.output == "9.0.9\n"
+        assert result.exit_code != 0
+        assert "'bogus' is not one of" in result.output
+        assert "'main'" in result.output
