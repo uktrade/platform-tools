0d48024f573a1fc85d256bb0c58348172221d006
Kate Sugden <kate.sugden@madetech.com>
2024-09-25 09:28:28 +0100

    Refactor: extract function get_latest_release

--- dbt_platform_helper/utils/versioning.py
+++ dbt_platform_helper/utils/versioning.py
@@ -105,17 +105,21 @@ def get_github_released_version(repository: str, tags: bool = False) -> Tuple[in
     return parse_version(package_info["tag_name"])
 
 
+def get_latest_release():
+    package_info = requests.get("https://pypi.org/pypi/dbt-platform-helper/json").json()
+    released_versions = package_info["releases"].keys()
+    parsed_released_versions = [parse_version(v) for v in released_versions]
+    parsed_released_versions.sort(reverse=True)
+    return parsed_released_versions[0]
+
+
 def get_platform_helper_versions(include_project_versions=True) -> PlatformHelperVersions:
     try:
         locally_installed_version = parse_version(version("dbt-platform-helper"))
     except PackageNotFoundError:
         locally_installed_version = None
 
-    package_info = requests.get("https://pypi.org/pypi/dbt-platform-helper/json").json()
-    released_versions = package_info["releases"].keys()
-    parsed_released_versions = [parse_version(v) for v in released_versions]
-    parsed_released_versions.sort(reverse=True)
-    latest_release = parsed_released_versions[0]
+    latest_release = get_latest_release()
 
     if not include_project_versions:
         return PlatformHelperVersions(
