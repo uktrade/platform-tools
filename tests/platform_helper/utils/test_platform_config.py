b3477b165054110344eb754d2ed90dce3646c9cb
Kate Sugden <kate.sugden@madetech.com>
2024-09-25 16:50:59 +0100

    align with main version to reduce diff

--- tests/platform_helper/utils/test_platform_config.py
+++ tests/platform_helper/utils/test_platform_config.py
@@ -1,15 +1,43 @@
 from pathlib import Path
-from unittest.mock import patch
 
 import pytest
 import yaml
 
 from dbt_platform_helper.constants import PLATFORM_CONFIG_FILE
-from dbt_platform_helper.utils.platform_config import (
-    get_environment_pipeline_names,
-    load_config_file,
-    is_terraform_project,
+from dbt_platform_helper.utils.platform_config import get_environment_pipeline_names
+from dbt_platform_helper.utils.platform_config import is_terraform_project
+from dbt_platform_helper.utils.platform_config import load_config_file
+
+
+def test_get_environment_pipeline_names(fakefs, valid_platform_config):
+    fakefs.create_file(PLATFORM_CONFIG_FILE, contents=yaml.dump(valid_platform_config))
+    names = get_environment_pipeline_names()
+
+    assert {"main", "test", "prod-main"} == names
+
+
+def test_get_environment_pipeline_names_defaults_to_empty_list_when_theres_no_platform_config_file(
+    fakefs,
+):
+    names = get_environment_pipeline_names()
+
+    assert {} == names
+
+
+@pytest.mark.parametrize(
+    "platform_config_content, expected_result",
+    [
+        ("application: my-app\nlegacy_project: True", False),
+        ("application: my-app\nlegacy_project: False", True),
+        ("application: my-app", True),
+    ],
 )
+
+
+def test_is_terraform_project(fakefs, platform_config_content, expected_result):
+    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=platform_config_content)
+
+    assert is_terraform_project() == expected_result
 
 
 def test_load_config_file_returns_a_dict_given_valid_yaml(fakefs):
@@ -32,13 +60,6 @@ def test_load_config_file_returns_none_given_invalid_yaml(fakefs):
     assert config == None
 
 
-def test_get_environment_pipeline_names(fakefs, valid_platform_config):
-    fakefs.create_file(PLATFORM_CONFIG_FILE, contents=yaml.dump(valid_platform_config))
-    names = get_environment_pipeline_names()
-
-    assert {"main", "test", "prod-main"} == names
-
-
 def test_get_environment_pipeline_names_returns_empty_dict_given_invalid_yaml(fakefs):
     fakefs.create_file(PLATFORM_CONFIG_FILE, contents="{")
     names = get_environment_pipeline_names()
@@ -49,25 +70,3 @@ def test_get_environment_pipeline_names_given_invalid_config(create_invalid_plat
     names = get_environment_pipeline_names()
 
     assert {"prod-main"} == names
-
-
-def test_get_environment_pipeline_names_defaults_to_empty_list_when_theres_no_platform_config_file(
-    fakefs,
-):
-    names = get_environment_pipeline_names()
-
-    assert {} == names
-
-
-@pytest.mark.parametrize(
-    "platform_config_content, expected_result",
-    [
-        ("application: my-app\nlegacy_project: True", False),
-        ("application: my-app\nlegacy_project: False", True),
-        ("application: my-app", True),
-    ],
-)
-def test_is_terraform_project(fakefs, platform_config_content, expected_result):
-    fakefs.create_file(Path(PLATFORM_CONFIG_FILE), contents=platform_config_content)
-
-    assert is_terraform_project() == expected_result
