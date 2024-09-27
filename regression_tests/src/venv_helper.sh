8837c007ff1b1d6bac9ca0e17a9eddcb5c30786d
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-09-27 07:24:34 +0100

    re-enable the tests

--- regression_tests/src/venv_helper.sh
+++ regression_tests/src/venv_helper.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 function create_and_activate_venv() {
+    echo -e "\nCreating virtual environment"
     rm -rf venv
     python -m venv --copies venv
+    echo -e "\nUpdating paths"
     write_static_path_to_file
     replace_static_path_in_venv_activate
+    echo -e "\nActivating virtual environment"
     source venv/bin/activate
     run_checks
 }
 
 function fix_paths_and_activate_venv() {
+    echo -e "\nUpdating paths"
     replace_static_paths_in_shebangs_etc
+    echo -e "\nActivating virtual environment"
     source venv/bin/activate
     run_checks
     echo -e "\nWhich platform-helper: $(which platform-helper)"
@@ -18,7 +23,7 @@ function fix_paths_and_activate_venv() {
 }
 
 run_checks() {
-    echo "Running virtual environment checks"
+    echo -e "\nRunning virtual environment checks"
     echo -e "\nVIRTUAL_ENV: $VIRTUAL_ENV"
     echo -e "\nPython version: $(python --version)"
     echo -e "\nWhich Python: $(which python)"
