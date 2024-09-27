71f717c93968c8f27a08a289e48ed1d94446732b
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-09-27 12:09:50 +0100

    Increase timeouts on the environment pipelines

--- regression_tests/actions/run_environment_pipeline.sh
+++ regression_tests/actions/run_environment_pipeline.sh
@@ -8,4 +8,4 @@ cd "${CODEBUILD_SRC_DIR}"
 
 source ./regression_tests/src/run_pipeline.sh
 
-run_pipeline "Environment" "demodjango-${TARGET_ENVIRONMENT}-environment-pipeline" 600
+run_pipeline "Environment" "demodjango-${TARGET_ENVIRONMENT}-environment-pipeline" 900
