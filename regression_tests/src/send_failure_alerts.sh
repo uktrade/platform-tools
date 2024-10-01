c06bedc4e9c32531a0419bf16d780fa023c93bf9
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-10-01 17:07:33 +0100

    Try to make it send alert to test channel

--- regression_tests/src/send_failure_alerts.sh
+++ regression_tests/src/send_failure_alerts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-env
+echo "TARGET_ENVIRONMENT: ${TARGET_ENVIRONMENT}"
 
-if [ "${CODEBUILD_BUILD_SUCCEEDING}" != "1" ] && git branch --contains $CODEBUILD_RESOLVED_SOURCE_VERSION | grep -q "main" && [ "${TARGET_ENVIRONMENT:-toolspr}" == "toolspr" ]; then
+if [ "${CODEBUILD_BUILD_SUCCEEDING}" != "1" ] && git branch --contains $CODEBUILD_RESOLVED_SOURCE_VERSION | grep -q "1291-sort-out-alerts" && [ "${TARGET_ENVIRONMENT:-toolspr}" == "tony" ]; then
     echo -e "\nAction failed sending alert"
     pip install dbt-platform-helper
     MESSAGE=":alert: @here DBT Platform regression tests have failed in <https://eu-west-2.console.aws.amazon.com/codesuite/codebuild/763451185160/projects/platform-tools-test/build/${CODEBUILD_BUILD_ID}/?region=eu-west-2|build ${CODEBUILD_BUILD_NUMBER}> :sob:"
