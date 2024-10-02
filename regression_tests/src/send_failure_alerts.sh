8ceffd30f3c7c123c9735caee414fa4e26e4e87f
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-10-02 11:44:12 +0100

    Add check for repos being on main branch

--- regression_tests/src/send_failure_alerts.sh
+++ regression_tests/src/send_failure_alerts.sh
@@ -2,7 +2,29 @@
 
 echo "TARGET_ENVIRONMENT: ${TARGET_ENVIRONMENT}"
 
-if [ "${CODEBUILD_BUILD_SUCCEEDING}" != "1" ] && git branch --contains $CODEBUILD_RESOLVED_SOURCE_VERSION | grep -q "1291-sort-out-alerts" && [ "${TARGET_ENVIRONMENT:-toolspr}" == "tony" ]; then
+function is_platform_tools_branch_main() {
+    git branch --contains $CODEBUILD_RESOLVED_SOURCE_VERSION | grep -q "1291-sort-out-alerts"
+}
+
+function is_demodjango_deploy_branch_main() {
+    cd demodjango-deploy
+    git branch --contains $CODEBUILD_RESOLVED_SOURCE_VERSION | grep -q "main"
+    cd "$CODEBUILD_SRC_DIR"
+}
+
+function is_demodjango_branch_main() {
+    cd demodjango
+    git branch --contains $CODEBUILD_RESOLVED_SOURCE_VERSION | grep -q "main"
+    cd "$CODEBUILD_SRC_DIR"
+}
+
+#todo: Add terraform-platform-tools when we are targeting branches
+if [ "${CODEBUILD_BUILD_SUCCEEDING}" != "1" ] && \
+    is_platform_tools_branch_main && \
+    is_demodjango_deploy_branch_main && \
+    is_demodjango_branch_main && \
+    [ "${TARGET_ENVIRONMENT:-toolspr}" == "tony" ]; then
+
     echo -e "\nAction failed sending alert"
     pip install dbt-platform-helper
     MESSAGE=":alert: @here DBT Platform regression tests have failed in <https://eu-west-2.console.aws.amazon.com/codesuite/codebuild/763451185160/projects/platform-tools-test/build/${CODEBUILD_BUILD_ID}/?region=eu-west-2|build ${CODEBUILD_BUILD_NUMBER}> :sob:"
