c217a44031dddfaf57b1a4a4b3e67181fde42557
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-10-14 11:11:04 +0100

    Updated entrypoint to use a text format for the dump file rather than a tar file

--- images/tools/database-copy2/entrypoint.sh
+++ images/tools/database-copy2/entrypoint.sh
@@ -3,14 +3,12 @@
 if [ "${DATA_COPY_OPERATION:-DUMP}" != "LOAD" ]
 then
   echo "Starting data dump"
-  pg_dump --format tar "${DB_CONNECTION_STRING}" | gzip > data_dump.tgz
-  aws s3 cp data_dump.tgz s3://${S3_BUCKET_NAME}/
+  pg_dump --format c "${DB_CONNECTION_STRING}" > data_dump.sql
+  aws s3 cp data_dump.sql s3://${S3_BUCKET_NAME}/
   echo "Stopping data dump"
 else
   echo "Starting data restore"
-  aws s3 cp s3://${S3_BUCKET_NAME}/data_dump.tgz data_dump.tgz
-  ls -al
-  gunzip data_dump.tgz
-  pg_restore --dbname "${DB_CONNECTION_STRING}" --format tar data_dump.tar
+  aws s3 cp s3://${S3_BUCKET_NAME}/data_dump.sql data_dump.sql
+  pg_restore --format c --dbname "${DB_CONNECTION_STRING}" data_dump.sql
   echo "Stopping data restore"
 fi
