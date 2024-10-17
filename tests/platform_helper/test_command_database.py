8bf400eb7eaef89c8fea8692741ad20e92873dd2
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-10-17 14:03:17 +0100

    Refactoring of helper methods

--- tests/platform_helper/test_command_database.py
+++ tests/platform_helper/test_command_database.py
@@ -29,10 +29,8 @@ def test_command_dump_success(mock_database_copy_object):
     )
 
     assert result.exit_code == 0
-    mock_database_copy_object.assert_called_once_with(
-        "12345", "my_app", "my_env", None, "my_postgres", "my_vpc"
-    )
-    mock_database_copy_instance.dump.assert_called_once_with()
+    mock_database_copy_object.assert_called_once_with("12345", "my_app", "my_postgres", "my_vpc")
+    mock_database_copy_instance.dump.assert_called_once_with("my_env")
 
 
 @patch("dbt_platform_helper.commands.database.DatabaseCopy")
@@ -56,10 +54,8 @@ def test_command_load_success(mock_database_copy_object):
     )
 
     assert result.exit_code == 0
-    mock_database_copy_object.assert_called_once_with(
-        "12345", "my_app", None, "my_env", "my_postgres", "my_vpc"
-    )
-    mock_database_copy_instance.load.assert_called_once_with()
+    mock_database_copy_object.assert_called_once_with("12345", "my_app", "my_postgres", "my_vpc")
+    mock_database_copy_instance.load.assert_called_once_with("my_env")
 
 
 @patch("dbt_platform_helper.commands.database.DatabaseCopy")
@@ -85,8 +81,6 @@ def test_command_copy_success(mock_database_copy_object):
     )
 
     assert result.exit_code == 0
-    mock_database_copy_object.assert_called_once_with(
-        "12345", "my_app", "my_prod_env", "my_hotfix_env", "my_postgres", "my_vpc"
-    )
-    mock_database_copy_instance.dump.assert_called_once_with()
-    mock_database_copy_instance.load.assert_called_once_with()
+    mock_database_copy_object.assert_called_once_with("12345", "my_app", "my_postgres", "my_vpc")
+    mock_database_copy_instance.dump.assert_called_once_with("my_prod_env")
+    mock_database_copy_instance.load.assert_called_once_with("my_hotfix_env")
