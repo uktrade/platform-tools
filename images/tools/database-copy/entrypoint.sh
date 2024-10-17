b2e3576b3448e251d787852c60bdd176bc4bbc68
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-10-17 11:29:57 +0100

    feat: Added dump file deletion from S3 at the end of the data load process

--- images/tools/database-copy/entrypoint.sh
+++ images/tools/database-copy/entrypoint.sh
@@ -7,8 +7,12 @@ then
   aws s3 cp data_dump.sql s3://${S3_BUCKET_NAME}/
   echo "Stopping data dump"
 else
-  echo "Starting data restore"
+  echo "Starting data load"
   aws s3 cp s3://${S3_BUCKET_NAME}/data_dump.sql data_dump.sql
   pg_restore --format c --dbname "${DB_CONNECTION_STRING}" data_dump.sql
-  echo "Stopping data restore"
+  echo "Cleaning up dump file"
+  rm data_dump.sql
+  echo "Removing dump file from S3"
+  aws s3 rm s3://${S3_BUCKET_NAME}/data_dump.sql
+  echo "Stopping data load"
 fi
