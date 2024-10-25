ec1f09544b83efad5f107391a0d4d5258ef312ca
Kate Sugden <kate.sugden@madetech.com>
2024-10-25 16:17:25 +0100

    Handle errors and clean up

--- images/tools/database-copy/entrypoint.sh
+++ images/tools/database-copy/entrypoint.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+clean_up(){
+  echo "Cleaning up dump file"
+  rm data_dump.sql
+  echo "Removing dump file from S3"
+  aws s3 rm s3://${S3_BUCKET_NAME}/data_dump.sql
+  if [ ${exit_code} -ne 0 ]
+  then
+    echo "Aborting data load: Clean up failed"
+    exit $exit_code    
+  fi
+}
+
+handle_errors(){
+  exit_code=$1
+  message=$2
+  if [ ${exit_code} -ne 0 ]
+  then
+    clean_up
+    echo "Aborting data load: {$message}"
+    exit $exit_code    
+  fi
+}
+
 if [ "${DATA_COPY_OPERATION:-DUMP}" != "LOAD" ]
 then
   echo "Starting data dump"
@@ -27,61 +50,51 @@ else
 
   echo "Copying data dump from S3"
   aws s3 cp s3://${S3_BUCKET_NAME}/data_dump.sql data_dump.sql
-  exit_code=$?
-
-  if [ ${exit_code} -ne 0 ]
-  then
-    echo "Aborting data load"
-    exit $exit_code
-  fi
+  
+  handle_errors $? "Copy failed"
 
   echo "Scaling down services"
   SERVICES=$(aws ecs list-services --cluster "${ECS_CLUSTER}" | jq -r '.serviceArns[]')
+  
+  handle_errors $? "Failed to list services"
+
   for service in ${SERVICES}
   do
     COUNT=$(aws ecs describe-services --cluster "${ECS_CLUSTER}" --services "${service}" | jq '.services[0].desiredCount')
-    CONFIG_FILE="$(basename "${service}").desired_count"
+    
+    handle_errors $? "Failed to describe service"
+    
+    SERVICE_NAME=$(basename "${service}")
+    CONFIG_FILE="${SERVICE_NAME}.desired_count"
     echo "${COUNT}" > "${CONFIG_FILE}"
 
-    echo "$(basename ${service})"
+    echo ${SERVICE_NAME}
     aws ecs update-service --cluster "${ECS_CLUSTER}" --service "${service}" --desired-count 0 | jq -r '"  Desired Count: \(.service.desiredCount)\n  Running Count: \(.service.runningCount)"'
+  
+    handle_errors $? "Failed to update service ${SERVICE_NAME}"
   done
 
   echo "Clearing down the database prior to loading new data"
   psql "${DB_CONNECTION_STRING}" -f /clear_db.sql
 
-  exit_code=$?
-
-  if [ ${exit_code} -ne 0 ]
-  then
-    echo "Aborting data load"
-    exit $exit_code
-  fi
-
+  handle_errors $? "Clear down failed"
+  
   echo "loading new data from S3"
   pg_restore --format c --dbname "${DB_CONNECTION_STRING}" data_dump.sql
-  exit_code=$?
-
-  echo "Cleaning up dump file"
-  rm data_dump.sql
-  echo "Removing dump file from S3"
-  aws s3 rm s3://${S3_BUCKET_NAME}/data_dump.sql
-
+  
+  handle_errors $? "Restore failed"
   for service in ${SERVICES}
   do
     CONFIG_FILE="$(basename "${service}").desired_count"
     COUNT=$(cat "${CONFIG_FILE}")
-
-  echo "Scaling up services"
-    echo "$(basename ${service})"
+    SERVICE_NAME=$(basename "${service}")
+    echo "Scaling up services"
+    echo ${SERVICE_NAME}
     aws ecs update-service --cluster "${ECS_CLUSTER}" --service "${service}" --desired-count "${COUNT}" | jq -r '"  Desired Count: \(.service.desiredCount)\n  Running Count: \(.service.runningCount)"'
+    handle_errors $? "Failed to update service ${SERVICE_NAME}"
   done
 
-  if [ ${exit_code} -ne 0 ]
-  then
-    echo "Aborting data load"
-    exit $exit_code
-  fi
+  clean_up
 
   echo "Stopping data load"
 fi
