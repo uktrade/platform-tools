2421b48f7fcefde62374096a6d7592ea7368373f
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2024-11-08 16:46:19 +0000

    patch version check at command conduit level

--- tests/platform_helper/test_command_conduit.py
+++ tests/platform_helper/test_command_conduit.py
@@ -16,18 +16,16 @@ from dbt_platform_helper.commands.conduit import conduit
         ("custom-name-opensearch"),
     ],
 )
-@patch(
-    "dbt_platform_helper.utils.versioning.running_as_installed_package", new=Mock(return_value=True)
-)
 @patch("dbt_platform_helper.commands.conduit.Conduit")
-@patch("dbt_platform_helper.utils.versioning.check_platform_helper_version_needs_update")
-def test_start_conduit(
-    mock_conduit_object, check_platform_helper_version_needs_update, addon_name, mock_application
-):
+@patch(
+    "dbt_platform_helper.utils.versioning.running_as_installed_package",
+    new=Mock(return_value=True),
+)
+def test_start_conduit(mock_conduit_object, addon_name, mock_application, validate_version):
     """Test that given an app, env and addon name strings, the conduit command
     calls start_conduit with app, env, addon type and addon name."""
+
     mock_conduit_instance = mock_conduit_object.return_value
-    validate_version = check_platform_helper_version_needs_update.return_value
 
     result = CliRunner().invoke(
         conduit,
@@ -41,7 +39,7 @@ def test_start_conduit(
     )
 
     validate_version.assert_called_once()
-    mock_conduit_instance.assert_called_once_with(
+    mock_conduit_instance.start.assert_called_once_with(
         mock_application, "development", addon_name, "read"
     )
 
