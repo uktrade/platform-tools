c78d74420e1007042f44cd6cb771ec6a8175164a
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-11-20 08:47:15 +0000

    Drop change in conftest.py

--- tests/platform_helper/conftest.py
+++ tests/platform_helper/conftest.py
@@ -39,7 +39,6 @@ class NoSuchEntityException(ClientError):
 @pytest.fixture
 def fakefs(fs):
     """Mock file system fixture with the templates and schemas dirs retained."""
-    print(f"BASE_DIR: {BASE_DIR}")
     fs.add_real_directory(BASE_DIR / "dbt_platform_helper/templates", lazy_read=True)
     fs.add_real_directory(FIXTURES_DIR, lazy_read=True)
     fs.add_real_file(BASE_DIR / "dbt_platform_helper/addon-plans.yml")
