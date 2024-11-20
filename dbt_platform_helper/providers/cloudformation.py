f505908c2836ef4bf75f8f7f0baeac81e75d5578
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-11-20 17:04:37 +0000

    Example UPDATE_ROLLBACK_COMPLETE

--- dbt_platform_helper/providers/cloudformation.py
+++ dbt_platform_helper/providers/cloudformation.py
@@ -98,7 +98,7 @@ def update_conduit_stack_resources(
     return conduit_stack_name
 
 
-# TODO opportunity to add error handling if cloudformation stack goes into rollback
+# TODO opportunity to add error handling if cloudformation stack goes into rollback e.g. botocore.exceptions.WaiterError: Waiter StackUpdateComplete failed: Waiter encountered a terminal failure state: For expression "Stacks[].StackStatus" we matched expected path: "UPDATE_ROLLBACK_COMPLETE" at least once
 def wait_for_cloudformation_to_reach_status(cloudformation_client, stack_status, stack_name):
 
     waiter = cloudformation_client.get_waiter(stack_status)
