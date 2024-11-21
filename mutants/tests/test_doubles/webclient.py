7a8d1699c5964dd5b40b04ac41145e9be3f17734
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-11-21 12:39:20 +0000

    remove mutants directory

--- mutants/tests/test_doubles/webclient.py
+++
@@ -1,7 +0,0 @@
-from unittest.mock import Mock
-
-
-class WebClient:
-    def __init__(self, token: str):
-        self.token = token
-        self.chat_postMessage = Mock(return_value={"ts": "first-message"})
