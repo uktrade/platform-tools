1abf7ad99254b95eea1cab515be06c1cfe7f5366
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-12-06 15:19:53 +0000

    More reorganising

--- dbt_platform_helper/utils/platform_config_schema.py
+++ dbt_platform_helper/utils/platform_config_schema.py
@@ -11,6 +11,7 @@ from schema import SchemaError
 def _string_matching_regex(regex_pattern: str):
     def validator(string):
         if not re.match(regex_pattern, string):
+            # Todo: Raise suitable PlatformException?
             raise SchemaError(
                 f"String '{string}' does not match the required pattern '{regex_pattern}'. For more details on valid string patterns see: https://aws.github.io/copilot-cli/docs/manifest/lb-web-service/"
             )
@@ -23,6 +24,7 @@ def _integer_between(lower, upper):
     def _is_between(value):
         if isinstance(value, int) and lower <= value <= upper:
             return True
+        # Todo: Raise suitable PlatformException?
         raise SchemaError(f"should be an integer between {lower} and {upper}")
 
     return _is_between
@@ -140,7 +142,52 @@ _monitoring_schema = {
     },
 }
 
+
 # Opensearch...
+class ConditionalOpensSearchSchema(Schema):
+    def validate(self, data, _is_conditional_schema=True):
+        data = super(ConditionalOpensSearchSchema, self).validate(
+            data, _is_conditional_schema=False
+        )
+        if _is_conditional_schema:
+            default_plan = None
+            default_volume_size = None
+
+            default_environment_config = data["environments"].get(
+                "*", data["environments"].get("default", None)
+            )
+            if default_environment_config:
+                default_plan = default_environment_config.get("plan", None)
+                default_volume_size = default_environment_config.get("volume_size", None)
+
+            for env in data["environments"]:
+                volume_size = data["environments"][env].get("volume_size", default_volume_size)
+                plan = data["environments"][env].get("plan", default_plan)
+
+                if volume_size:
+                    if not plan:
+                        # Todo: Raise suitable PlatformException?
+                        raise SchemaError(f"Missing key: 'plan'")
+
+                    if volume_size < _valid_opensearch_min_volume_size:
+                        # Todo: Raise suitable PlatformException?
+                        raise SchemaError(
+                            f"Key 'environments' error: Key '{env}' error: Key 'volume_size' error: should be an integer greater than {_valid_opensearch_min_volume_size}"
+                        )
+
+                    for key in _valid_opensearch_max_volume_size:
+                        if (
+                            plan == key
+                            and not volume_size <= _valid_opensearch_max_volume_size[key]
+                        ):
+                            # Todo: Raise suitable PlatformException?
+                            raise SchemaError(
+                                f"Key 'environments' error: Key '{env}' error: Key 'volume_size' error: should be an integer between {_valid_opensearch_min_volume_size} and {_valid_opensearch_max_volume_size[key]} for plan {plan}"
+                            )
+
+        return data
+
+
 # Todo: Move to OpenSearch provider?
 _valid_opensearch_plans = Or(
     "tiny", "small", "small-ha", "medium", "medium-ha", "large", "large-ha", "x-large", "x-large-ha"
@@ -312,7 +359,7 @@ def _valid_s3_bucket_name(name: str):
             errors.append(f"Names cannot be suffixed '{suffix}'.")
 
     if errors:
-        # Todo: Raise suitable PlatformException
+        # Todo: Raise suitable PlatformException?
         raise SchemaError(
             "Bucket name '{}' is invalid:\n{}".format(name, "\n".join(f"  {e}" for e in errors))
         )
@@ -492,45 +539,6 @@ _environment_pipelines_schema = {
 }
 
 
-class ConditionalSchema(Schema):
-    def validate(self, data, _is_conditional_schema=True):
-        data = super(ConditionalSchema, self).validate(data, _is_conditional_schema=False)
-        if _is_conditional_schema:
-            default_plan = None
-            default_volume_size = None
-
-            default_environment_config = data["environments"].get(
-                "*", data["environments"].get("default", None)
-            )
-            if default_environment_config:
-                default_plan = default_environment_config.get("plan", None)
-                default_volume_size = default_environment_config.get("volume_size", None)
-
-            for env in data["environments"]:
-                volume_size = data["environments"][env].get("volume_size", default_volume_size)
-                plan = data["environments"][env].get("plan", default_plan)
-
-                if volume_size:
-                    if not plan:
-                        raise SchemaError(f"Missing key: 'plan'")
-
-                    if volume_size < _valid_opensearch_min_volume_size:
-                        raise SchemaError(
-                            f"Key 'environments' error: Key '{env}' error: Key 'volume_size' error: should be an integer greater than {_valid_opensearch_min_volume_size}"
-                        )
-
-                    for key in _valid_opensearch_max_volume_size:
-                        if (
-                            plan == key
-                            and not volume_size <= _valid_opensearch_max_volume_size[key]
-                        ):
-                            raise SchemaError(
-                                f"Key 'environments' error: Key '{env}' error: Key 'volume_size' error: should be an integer between {_valid_opensearch_min_volume_size} and {_valid_opensearch_max_volume_size[key]} for plan {plan}"
-                            )
-
-        return data
-
-
 # Used outside this file by validate_platform_config()
 PLATFORM_CONFIG_SCHEMA = Schema(
     {
@@ -561,7 +569,7 @@ PLATFORM_CONFIG_SCHEMA = Schema(
 EXTENSION_SCHEMAS = {
     "alb": Schema(_alb_schema),
     "appconfig-ipfilter": _no_configuration_required_schema("appconfig-ipfilter"),
-    "opensearch": ConditionalSchema(_opensearch_schema),
+    "opensearch": ConditionalOpensSearchSchema(_opensearch_schema),
     "postgres": Schema(_postgres_schema),
     "prometheus-policy": Schema(_prometheus_policy_schema),
     "redis": Schema(_redis_schema),
