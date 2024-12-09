b94b8efe5f0d00ae2913b54b26b160a040ecc2c9
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-12-09 09:43:11 +0000

    Remove copilot waffle from error message

--- dbt_platform_helper/utils/platform_config_schema.py
+++ dbt_platform_helper/utils/platform_config_schema.py
@@ -13,7 +13,7 @@ def _string_matching_regex(regex_pattern: str):
         if not re.match(regex_pattern, string):
             # Todo: Raise suitable PlatformException?
             raise SchemaError(
-                f"String '{string}' does not match the required pattern '{regex_pattern}'. For more details on valid string patterns see: https://aws.github.io/copilot-cli/docs/manifest/lb-web-service/"
+                f"String '{string}' does not match the required pattern '{regex_pattern}'."
             )
         return string
 
