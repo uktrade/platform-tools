c70bce4c93c8521088e0771996ef368047268e1d
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-12-12 12:19:10 +0000

    refactor: DBTP-1596 Convert to PlatformConfigSchema class (#688)
    Co-authored-by: chiaramapellimt <chiara.mapelli@madetech.com>

--- tests/platform_helper/utils/test_validation.py
+++ tests/platform_helper/utils/test_validation.py
@@ -8,9 +8,7 @@ from schema import SchemaError
 
 from dbt_platform_helper.constants import PLATFORM_CONFIG_FILE
 from dbt_platform_helper.constants import PLATFORM_HELPER_VERSION_FILE
-from dbt_platform_helper.providers.platform_config_schema import _is_integer_between
-from dbt_platform_helper.providers.platform_config_schema import _string_matching_regex
-from dbt_platform_helper.providers.platform_config_schema import _valid_s3_bucket_name
+from dbt_platform_helper.providers.platform_config_schema import PlatformConfigSchema
 from dbt_platform_helper.utils.validation import _validate_extension_supported_versions
 from dbt_platform_helper.utils.validation import config_file_check
 from dbt_platform_helper.utils.validation import float_between_with_halfstep
@@ -42,7 +40,7 @@ def load_addons(addons_file):
     ],
 )
 def test_validate_string(regex_pattern, valid_strings, invalid_strings):
-    validator = _string_matching_regex(regex_pattern)
+    validator = PlatformConfigSchema.string_matching_regex(regex_pattern)
 
     for valid_string in valid_strings:
         assert validator(valid_string) == valid_string
@@ -306,13 +304,13 @@ def test_validate_addons_missing_type():
 
 @pytest.mark.parametrize("value", [5, 1, 9])
 def test_between_success(value):
-    assert _is_integer_between(1, 9)(value)
+    assert PlatformConfigSchema.is_integer_between(1, 9)(value)
 
 
 @pytest.mark.parametrize("value", [-1, 10])
 def test_between_raises_error(value):
     try:
-        _is_integer_between(1, 9)(value)
+        PlatformConfigSchema.is_integer_between(1, 9)(value)
         assert False, f"testing that {value} is between 1 and 9 failed to raise an error."
     except SchemaError as ex:
         assert ex.code == "should be an integer between 1 and 9"
@@ -336,7 +334,7 @@ def test_between_with_step_raises_error(value):
 
 @pytest.mark.parametrize("bucket_name", ["abc", "a" * 63, "abc-123.xyz", "123", "257.2.2.2"])
 def test_validate_s3_bucket_name_success_cases(bucket_name):
-    assert _valid_s3_bucket_name(bucket_name)
+    assert PlatformConfigSchema.valid_s3_bucket_name(bucket_name)
 
 
 @pytest.mark.parametrize(
@@ -361,7 +359,7 @@ def test_validate_s3_bucket_name_success_cases(bucket_name):
 def test_validate_s3_bucket_name_failure_cases(bucket_name, error_message):
     exp_error = f"Bucket name '{bucket_name}' is invalid:\n  {error_message}"
     with pytest.raises(SchemaError) as ex:
-        _valid_s3_bucket_name(bucket_name)
+        PlatformConfigSchema.valid_s3_bucket_name(bucket_name)
 
     assert exp_error in str(ex.value)
 
@@ -369,7 +367,7 @@ def test_validate_s3_bucket_name_failure_cases(bucket_name, error_message):
 def test_validate_s3_bucket_name_multiple_failures():
     bucket_name = "xn--one-two..THREE" + "z" * 50 + "--ol-s3"
     with pytest.raises(SchemaError) as ex:
-        _valid_s3_bucket_name(bucket_name)
+        PlatformConfigSchema.valid_s3_bucket_name(bucket_name)
 
     exp_errors = [
         "Length must be between 3 and 63 characters inclusive.",
