d170b8abebd57be3441061f8d245ef0c860040db
Kate Sugden <kate.sugden@madetech.com>
2024-12-20 14:41:46 +0000

    inject file provider

--- dbt_platform_helper/providers/config.py
+++ dbt_platform_helper/providers/config.py
@@ -2,8 +2,8 @@ from copy import deepcopy
 from pathlib import Path
 
 import click
-
 from schema import SchemaError
+
 from dbt_platform_helper.constants import PLATFORM_CONFIG_FILE
 from dbt_platform_helper.providers.platform_config_schema import PlatformConfigSchema
 from dbt_platform_helper.providers.yaml_file import YamlFileProvider
@@ -12,11 +12,11 @@ from dbt_platform_helper.utils.messages import abort_with_error
 
 
 class ConfigProvider:
-    def __init__(self, config_validator, echo=click.secho):
+    def __init__(self, config_validator, file_provider=None, echo=click.secho):
         self.config = {}
         self.validator = config_validator
         self.echo = echo
-        self.yaml_file_provider = YamlFileProvider
+        self.file_provider = file_provider or YamlFileProvider
 
     def validate_platform_config(self):
         PlatformConfigSchema.schema().validate(self.config)
@@ -27,7 +27,7 @@ class ConfigProvider:
 
     def load_and_validate_platform_config(self, path=PLATFORM_CONFIG_FILE):
         try:
-            self.config = self.yaml_file_provider.load(path)
+            self.config = self.file_provider.load(path)
         except YamlFileProviderException as e:
             abort_with_error(f"Error loading configuration from {path}: {e}")
 
