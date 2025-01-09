078b7929e8f830abd0ea7046ace3373d046e41d3
Kate Sugden <kate.sugden@madetech.com>
2025-01-09 15:41:38 +0000

    Move private methods inside the class

--- dbt_platform_helper/domain/terraform_environment.py
+++ dbt_platform_helper/domain/terraform_environment.py
@@ -5,39 +5,6 @@ from dbt_platform_helper.providers.files import FileProvider
 from dbt_platform_helper.utils.template import setup_templates
 
 
-def _generate_terraform_environment_manifests(
-    application, environment_name, env_config, cli_terraform_platform_modules_version
-):
-    env_template = setup_templates().get_template("environments/main.tf")
-
-    terraform_platform_modules_version = _determine_terraform_platform_modules_version(
-        env_config, cli_terraform_platform_modules_version
-    )
-
-    return env_template.render(
-        {
-            "application": application,
-            "environment": environment_name,
-            "config": env_config,
-            "terraform_platform_modules_version": terraform_platform_modules_version,
-        }
-    )
-
-
-def _determine_terraform_platform_modules_version(
-    env_conf, terraform_platform_modules_version_override
-):
-    env_conf_terraform_platform_modules_version = env_conf.get("versions", {}).get(
-        "terraform-platform-modules"
-    )
-    version_preference_order = [
-        terraform_platform_modules_version_override,
-        env_conf_terraform_platform_modules_version,
-        DEFAULT_TERRAFORM_PLATFORM_MODULES_VERSION,
-    ]
-    return [version for version in version_preference_order if version][0]
-
-
 class TerraformEnvironment:
     def __init__(self, config_provider):
         self.config_provider = config_provider
@@ -47,7 +14,7 @@ class TerraformEnvironment:
         enriched_config = self.config_provider.apply_environment_defaults(config)
 
         env_config = enriched_config["environments"][environment_name]
-        contents = _generate_terraform_environment_manifests(
+        contents = self._generate_terraform_environment_manifests(
             config["application"],
             environment_name,
             env_config,
@@ -58,3 +25,34 @@ class TerraformEnvironment:
                 ".", f"terraform/environments/{environment_name}/main.tf", contents, overwrite=True
             )
         )
+
+    def _generate_terraform_environment_manifests(
+        self, application, environment_name, env_config, cli_terraform_platform_modules_version
+    ):
+        env_template = setup_templates().get_template("environments/main.tf")
+
+        terraform_platform_modules_version = self._determine_terraform_platform_modules_version(
+            env_config, cli_terraform_platform_modules_version
+        )
+
+        return env_template.render(
+            {
+                "application": application,
+                "environment": environment_name,
+                "config": env_config,
+                "terraform_platform_modules_version": terraform_platform_modules_version,
+            }
+        )
+
+    def _determine_terraform_platform_modules_version(
+        self, env_conf, terraform_platform_modules_version_override
+    ):
+        env_conf_terraform_platform_modules_version = env_conf.get("versions", {}).get(
+            "terraform-platform-modules"
+        )
+        version_preference_order = [
+            terraform_platform_modules_version_override,
+            env_conf_terraform_platform_modules_version,
+            DEFAULT_TERRAFORM_PLATFORM_MODULES_VERSION,
+        ]
+        return [version for version in version_preference_order if version][0]
