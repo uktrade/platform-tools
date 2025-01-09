fd3578009964d0744b37dd61041fa70871cb78cb
Kate Sugden <kate.sugden@madetech.com>
2025-01-09 12:22:03 +0000

    Add command level test for without the version flag

--- tests/platform_helper/test_command_environment.py
+++ tests/platform_helper/test_command_environment.py
@@ -168,6 +168,23 @@ class TestGenerateTerraform:
         mock_terraform_environment_instance.generate.assert_called_with("test", "123")
 
     @patch("dbt_platform_helper.commands.environment.TerraformEnvironment")
+    def test_generate_terraform_without_version_flag_success(self, terraform_environment_mock):
+        """Test that given name and terraform-platform-modules-version, the
+        generate terraform command calls TerraformEnvironment generate with app,
+        env, addon type and addon name."""
+
+        mock_terraform_environment_instance = terraform_environment_mock.return_value
+
+        result = CliRunner().invoke(
+            generate_terraform,
+            ["--name", "test"],
+        )
+
+        assert result.exit_code == 0
+
+        mock_terraform_environment_instance.generate.assert_called_with("test", None)
+
+    @patch("dbt_platform_helper.commands.environment.TerraformEnvironment")
     @patch("click.secho")
     def test_generate_terraform_catches_platform_exception_and_exits(
         self, mock_click, terraform_environment_mock
