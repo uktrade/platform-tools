77d02cd0342be1d20c8496e4089f10bbdea5ee3f
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2025-01-09 14:54:19 +0000

    store get_reference in local variable

--- dbt_platform_helper/domain/versions.py
+++ dbt_platform_helper/domain/versions.py
@@ -1,10 +1,14 @@
 from dbt_platform_helper.providers.aws import get_reference
 from dbt_platform_helper.providers.aws import get_supported_versions
+from dbt_platform_helper.providers.aws.interfaces import GetReferenceProtocol
+from dbt_platform_helper.providers.aws.interfaces import GetVersionsProtocol
 from dbt_platform_helper.providers.cache import CacheProvider
 
 
 def get_supported_aws_versions(
-    client_provider,
+    client_provider: (
+        GetReferenceProtocol | GetVersionsProtocol
+    ),  # TODO how to interface required functions of object
     cache_provider=CacheProvider(),
 ) -> list[str]:
     """
@@ -14,12 +18,11 @@ def get_supported_aws_versions(
     The cache value is retrieved if it exists.
     """
     supported_versions = []
-    if cache_provider.cache_refresh_required(get_reference(client_provider)):
+    aws_reference = get_reference(client_provider)
+    if cache_provider.cache_refresh_required(aws_reference):
         supported_versions = get_supported_versions(client_provider)
-        cache_provider.update_cache(get_reference(client_provider), supported_versions)
+        cache_provider.update_cache(aws_reference, supported_versions)
     else:
-        supported_versions = cache_provider.read_supported_versions_from_cache(
-            get_reference(client_provider)
-        )
+        supported_versions = cache_provider.read_supported_versions_from_cache(aws_reference)
 
     return supported_versions
