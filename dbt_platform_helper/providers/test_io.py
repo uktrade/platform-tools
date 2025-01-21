69edaf649020be6cda42b29052742c69c6681d2e
Kate Sugden <kate.sugden@madetech.com>
2025-01-21 13:02:23 +0000

    Add test for confirm with user input

--- dbt_platform_helper/providers/test_io.py
+++ dbt_platform_helper/providers/test_io.py
@@ -1,4 +1,7 @@
+from io import StringIO
 from unittest.mock import patch
+
+import pytest
 
 from dbt_platform_helper.providers.io import ClickIOProvider
 
@@ -7,14 +10,27 @@ class TestClickIOProvider:
     def test_warn_stdout(self, capsys):
         io = ClickIOProvider()
         io.warn("Warning!")
-        captured = str(capsys.readouterr())
-        assert "Warning!" in captured
+        assert "Warning!" in str(capsys.readouterr())
 
     def test_error_stdout(self, capsys):
         io = ClickIOProvider()
         io.error("Error!")
-        captured = str(capsys.readouterr())
-        assert "Error!" in captured
+        assert "Error!" in str(capsys.readouterr())
+
+    @pytest.mark.parametrize(
+        "input, expected",
+        [
+            ("y", True),
+            ("n", False),
+            ("yes", True),
+            ("no", False),
+        ],
+    )
+    def test_confirm_with_various_user_input(self, input, expected):
+        mock_input = StringIO(input)
+        with patch("sys.stdin", mock_input):
+            result = ClickIOProvider.confirm("Is that really your name?")
+            assert result == expected
 
     @patch("click.secho")
     def test_error_calls_secho_with_correct_formatting(self, mock_echo):
