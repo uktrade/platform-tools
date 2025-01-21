872058a9ab3c72be3c4eb037fe10f7a762854bf4
Kate Sugden <kate.sugden@madetech.com>
2025-01-21 12:13:33 +0000

    Implement Click IO Provider

--- dbt_platform_helper/providers/io.py
+++ dbt_platform_helper/providers/io.py
@@ -1,10 +1,29 @@
-from typing import Callable
+import click
 
 
 class IOProvider:
-    def __init__(
-        self, warn: Callable[[str], str], error: Callable[[str], str], prompt: Callable[[str], bool]
-    ):
-        self.warn = warn
-        self.error = error
-        self.prompt = prompt
+    @staticmethod
+    def warn(message: str):
+        pass
+
+    @staticmethod
+    def error(message: str):
+        pass
+
+    @staticmethod
+    def confirm(message: str) -> bool:
+        pass
+
+
+class ClickIOProvider(IOProvider):
+    @staticmethod
+    def warn(message: str):
+        click.secho(message, fg="yellow")
+
+    @staticmethod
+    def error(message: str):
+        click.secho(message, fg="red")
+
+    @staticmethod
+    def confirm(message: str) -> bool:
+        return click.confirm(message)
--- dbt_platform_helper/providers/test_io.py
+++ dbt_platform_helper/providers/test_io.py
@@ -1,11 +1,36 @@
-import click
+from unittest.mock import patch
 
-from dbt_platform_helper.providers.io import IOProvider
+from dbt_platform_helper.providers.io import ClickIOProvider
 
 
-class TestIOProvider:
-    def test_warning(self, capsys):
-        io_provider = IOProvider(click.secho, click.secho, click.confirm)
-        io_provider.warn("Beware!")
+class TestClickIOProvider:
+    def test_warn_stdout(self, capsys):
+        io_provider = ClickIOProvider()
+        io_provider.warn("Warning!")
         captured = str(capsys.readouterr())
-        assert "Beware!" in captured
+        assert "Warning!" in captured
+
+    def test_error_stdout(self, capsys):
+        io_provider = ClickIOProvider()
+        io_provider.error("Error!")
+        captured = str(capsys.readouterr())
+        assert "Error!" in captured
+
+    @patch("click.secho")
+    def test_error_calls_secho_with_correct_formatting(self, mock_echo):
+        io_provider = ClickIOProvider()
+        io_provider.error("Error!")
+        mock_echo.assert_called_once_with("Error!", fg="red")
+
+    @patch("click.secho")
+    def test_warn_calls_secho_with_correct_formatting(self, mock_echo):
+        io_provider = ClickIOProvider()
+        io_provider.warn("Warning!")
+        mock_echo.assert_called_once_with("Warning!", fg="yellow")
+
+    # def test_confirm(self, mock_input):
+    #     mock_input.return_value="y"
+    #     io_provider = IOProvider(click.secho, click.secho, click.confirm)
+    #     result = io_provider.confirm("Are you sure?")
+    #     assert result == True
+    # assert "Are you sure?" in captured
