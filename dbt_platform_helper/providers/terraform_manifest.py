0f2dde1862e5b45be655721f68cc1c7589411f03
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2025-02-05 16:18:42 +0000

    Refactored to move to a json formatted env terraform manifest

--- dbt_platform_helper/providers/terraform_manifest.py
+++ dbt_platform_helper/providers/terraform_manifest.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from dbt_platform_helper.constants import SUPPORTED_AWS_PROVIDER_VERSION
 from dbt_platform_helper.constants import SUPPORTED_TERRAFORM_VERSION
+from dbt_platform_helper.providers.config import ConfigProvider
 from dbt_platform_helper.providers.files import FileProvider
 from dbt_platform_helper.providers.io import ClickIOProvider
 
@@ -29,17 +30,53 @@ class TerraformManifestProvider:
             .get("deploy", {})
             .get("name")
         )
+
+        state_key_suffix = f"{platform_config['application']}-codebase-pipelines"
+
         terraform = {}
         self._add_header(terraform)
-        self._add_locals(terraform)
+        self._add_codebase_pipeline_locals(terraform)
         self._add_provider(terraform, default_account)
-        self._add_backend(terraform, platform_config, default_account)
+        self._add_backend(terraform, platform_config, default_account, state_key_suffix)
         self._add_codebase_pipeline_module(terraform, terraform_platform_modules_version)
         self._add_imports(terraform, ecr_imports)
 
         message = self.file_provider.mkfile(
             str(Path(".").absolute()),
             "terraform/codebase-pipelines/main.tf.json",
+            json.dumps(terraform, indent=2),
+            True,
+        )
+        self.io.info(message)
+
+    def generate_environment_config(
+        self,
+        platform_config: dict,
+        env: str,
+        terraform_platform_modules_version: str,
+    ):
+        platform_config = ConfigProvider.apply_environment_defaults(platform_config)
+        account = (
+            platform_config.get("environments", {})
+            .get(env, {})
+            .get("accounts", {})
+            .get("deploy", {})
+            .get("name")
+        )
+
+        state_key_suffix = f"{platform_config['application']}-{env}"
+
+        terraform = {}
+        self._add_header(terraform)
+        application_name = platform_config["application"]
+        self._add_environment_locals(terraform, application_name)
+        self._add_backend(terraform, platform_config, account, state_key_suffix)
+        self._add_extensions_module(terraform, terraform_platform_modules_version, env)
+        self._add_moved(terraform)
+
+        message = self.file_provider.mkfile(
+            str(Path(".").absolute()),
+            f"terraform/environments/{env}/main.tf.json",
             json.dumps(terraform, indent=2),
             True,
         )
@@ -54,7 +91,7 @@ class TerraformManifestProvider:
         terraform["//"] = f"{version_header} {warning}"
 
     @staticmethod
-    def _add_locals(terraform: dict):
+    def _add_codebase_pipeline_locals(terraform: dict):
         terraform["locals"] = {
             "platform_config": '${yamldecode(file("../../platform-config.yml"))}',
             "application": '${local.platform_config["application"]}',
@@ -71,17 +108,17 @@ class TerraformManifestProvider:
         terraform["provider"]["aws"]["shared_credentials_files"] = ["~/.aws/config"]
 
     @staticmethod
-    def _add_backend(terraform: dict, platform_config: dict, default_account: str):
+    def _add_backend(terraform: dict, platform_config: dict, account: str, state_key_suffix: str):
         terraform["terraform"] = {
             "required_version": SUPPORTED_TERRAFORM_VERSION,
             "backend": {
                 "s3": {
-                    "bucket": f"terraform-platform-state-{default_account}",
-                    "key": f"tfstate/application/{platform_config['application']}-codebase-pipelines.tfstate",
+                    "bucket": f"terraform-platform-state-{account}",
+                    "key": f"tfstate/application/{state_key_suffix}.tfstate",
                     "region": "eu-west-2",
                     "encrypt": True,
-                    "kms_key_id": f"alias/terraform-platform-state-s3-key-{default_account}",
-                    "dynamodb_table": f"terraform-platform-lockdb-{default_account}",
+                    "kms_key_id": f"alias/terraform-platform-state-s3-key-{account}",
+                    "dynamodb_table": f"terraform-platform-lockdb-{account}",
                 }
             },
             "required_providers": {
@@ -109,6 +146,13 @@ class TerraformManifestProvider:
         }
 
     @staticmethod
+    def _add_extensions_module(terraform: dict, terraform_platform_modules_version: str, env: str):
+        source = f"git::https://github.com/uktrade/terraform-platform-modules.git//extensions?depth=1&ref={terraform_platform_modules_version}"
+        terraform["module"] = {
+            "extensions": {"source": source, "args": "${local.args}", "environment": env}
+        }
+
+    @staticmethod
     def _add_imports(terraform: dict, ecr_imports: dict[str, str]):
         if ecr_imports:
             terraform["import"] = {
@@ -116,3 +160,26 @@ class TerraformManifestProvider:
                 "id": "${each.value}",
                 "to": "module.codebase-pipelines[each.key].aws_ecr_repository.this",
             }
+
+    @staticmethod
+    def _add_environment_locals(terraform: dict, app: str):
+        terraform["locals"] = {
+            "config": '${yamldecode(file("../../../platform-config.yml"))}',
+            "environments": '${local.config["environments"]}',
+            "env_config": '${{for name, config in local.environments: name => merge(lookup(local.environments, "*", {}), config)}}',
+            "args": {
+                "application": app,
+                "services": '${local.config["extensions"]}',
+                "env_config": "${local.env_config}",
+            },
+        }
+
+    @staticmethod
+    def _add_moved(terraform):
+        terraform["moved"] = [
+            {
+                "//": "Moved extensions-tf to just extensions - this block tells terraform this. Can be removed once all services have moved to the new naming.",
+                "from": "module.extensions-tf",
+                "to": "module.extensions",
+            }
+        ]
--- tests/platform_helper/domain/test_terraform_environment.py
+++ tests/platform_helper/domain/test_terraform_environment.py
@@ -2,9 +2,6 @@ from unittest.mock import Mock
 
 import pytest
 
-from dbt_platform_helper.domain.terraform_environment import (
-    PlatformTerraformManifestGenerator,
-)
 from dbt_platform_helper.domain.terraform_environment import TerraformEnvironment
 from dbt_platform_helper.platform_exception import PlatformException
 from dbt_platform_helper.providers.config import ConfigProvider
@@ -32,7 +29,7 @@ class TestGenerateTerraform:
 
         terraform_environment = TerraformEnvironment(
             config_provider=mock_config_provider,
-            manifest_generator=Mock(),
+            manifest_provider=Mock(),
             io=Mock(),
         )
         with pytest.raises(
@@ -40,36 +37,3 @@ class TestGenerateTerraform:
             match="cannot generate terraform for environment not-an-environment.  It does not exist in your configuration",
         ):
             terraform_environment.generate("not-an-environment")
-
-    # TODO test can be made more complete by using a file fixture for the expected content on the manifest
-    # (See copilot tests)
-    def test_terraform_environment_generate_writes_the_expected_manifest_to_file(
-        self,
-    ):
-        mock_config_provider = Mock(spec=ConfigProvider)
-        mock_config_provider.get_enriched_config.return_value = self.VALID_ENRICHED_CONFIG
-
-        mock_generator = Mock(spec=PlatformTerraformManifestGenerator)
-        mock_generator.generate_manifest.return_value = "I am a junk manifest for testing!"
-        mock_generator.write_manifest.return_value = "Hello, World!"
-
-        mock_io = Mock()
-
-        terraform_environment = TerraformEnvironment(
-            config_provider=mock_config_provider,
-            manifest_generator=mock_generator,
-            io=mock_io,
-        )
-
-        terraform_environment.generate("test")
-
-        mock_generator.generate_manifest.assert_called_with(
-            environment_name="test",
-            application_name="test-app",
-            environment_config=self.VALID_ENV_CONFIG,
-            terraform_platform_modules_version_override=None,
-        )
-        mock_generator.write_manifest.assert_called_with(
-            environment_name="test", manifest_content="I am a junk manifest for testing!"
-        )
-        mock_io.info.assert_called_with("Hello, World!")
