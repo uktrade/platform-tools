18f8b29a054c3dd7420c7194f75929556aa4f9e7
Kate Sugden <kate.sugden@madetech.com>
2025-02-06 14:39:17 +0000

    Add version compatibility check to SemanticVersion class

--- dbt_platform_helper/providers/semantic_version.py
+++ dbt_platform_helper/providers/semantic_version.py
@@ -1,3 +1,7 @@
+from dbt_platform_helper.providers.validation import IncompatibleMajorVersionException
+from dbt_platform_helper.providers.validation import IncompatibleMinorVersionException
+
+
 class SemanticVersion:
     def __init__(self, major, minor, patch):
         self.major = major
@@ -14,6 +18,27 @@ class SemanticVersion:
 
     def __eq__(self, other) -> bool:
         return (self.major, self.minor, self.patch) == (other.major, other.minor, other.patch)
+
+    def validate_version_compatibility(self, other):
+        app_major, app_minor, app_patch = self.major, self.minor, self.patch
+        check_major, check_minor, check_patch = (
+            other.major,
+            other.minor,
+            other.patch,
+        )
+        app_version_as_string = str(self)
+        check_version_as_string = str(other)
+
+        if (app_major == 0 and check_major == 0) and (
+            app_minor != check_minor or app_patch != check_patch
+        ):
+            raise IncompatibleMajorVersionException(app_version_as_string, check_version_as_string)
+
+        if app_major != check_major:
+            raise IncompatibleMajorVersionException(app_version_as_string, check_version_as_string)
+
+        if app_minor != check_minor:
+            raise IncompatibleMinorVersionException(app_version_as_string, check_version_as_string)
 
 
 class VersionStatus:
--- tests/platform_helper/providers/test_semantic_version.py
+++ tests/platform_helper/providers/test_semantic_version.py
@@ -1,6 +1,8 @@
 import pytest
 
 from dbt_platform_helper.providers.semantic_version import SemanticVersion
+from dbt_platform_helper.providers.validation import IncompatibleMajorVersionException
+from dbt_platform_helper.providers.validation import IncompatibleMinorVersionException
 
 
 class TestSemanticVersion:
@@ -22,3 +24,34 @@ class TestSemanticVersion:
 
     def test_different_semantic_versions_are_not_equal(self):
         assert SemanticVersion(1, 1, 1) != SemanticVersion(1, 2, 3)
+
+    @pytest.mark.parametrize(
+        "version_check",
+        [
+            (
+                SemanticVersion(1, 40, 0),
+                SemanticVersion(1, 30, 0),
+                IncompatibleMinorVersionException,
+            ),
+            (
+                SemanticVersion(1, 40, 0),
+                SemanticVersion(2, 1, 0),
+                IncompatibleMajorVersionException,
+            ),
+            (
+                SemanticVersion(0, 2, 40),
+                SemanticVersion(0, 1, 30),
+                IncompatibleMajorVersionException,
+            ),
+            (
+                SemanticVersion(0, 1, 40),
+                SemanticVersion(0, 1, 30),
+                IncompatibleMajorVersionException,
+            ),
+        ],
+    )
+    def test_validate_version_compatability(self, version_check):
+        app_version, check_version, raises = version_check
+
+        with pytest.raises(raises):
+            app_version.validate_version_compatibility(check_version)
