fc631674b504a6ee9f8d981b12e7f6b10fb5df90
Kate Sugden <kate.sugden@madetech.com>
2025-02-06 11:40:39 +0000

    Add version command group back in

--- dbt_platform_helper/commands/version.py
+++ dbt_platform_helper/commands/version.py
@@ -2,7 +2,14 @@ import click
 
 from dbt_platform_helper.platform_exception import PlatformException
 from dbt_platform_helper.providers.io import ClickIOProvider
+from dbt_platform_helper.utils.click import ClickDocOptGroup
 from dbt_platform_helper.utils.versioning import RequiredVersion
+
+
+@click.group(chain=True, cls=ClickDocOptGroup)
+def version():
+    """Contains subcommands for getting version information about the current
+    project."""
 
 
 @click.command(help="Print the version of platform-tools required by the current project")
@@ -12,7 +19,7 @@ from dbt_platform_helper.utils.versioning import RequiredVersion
     type=str,
     help="Take into account platform-tools version overrides in the specified pipeline",
 )
-def version(pipeline):
+def get_platform_helper_for_project(pipeline):
     """
     Version precedence is in this order:
         - if the --pipeline option is supplied, the version in 'platform-config.yml' in:
--- tests/platform_helper/test_command_version.py
+++ tests/platform_helper/test_command_version.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 from click.testing import CliRunner
 
-from dbt_platform_helper.commands.version import version
+from dbt_platform_helper.commands.version import get_platform_helper_for_project
 from dbt_platform_helper.platform_exception import PlatformException
 
 
@@ -18,7 +18,7 @@ class TestVersionCommand:
     ):
         mock_required_version.return_value = "1.2.3"
 
-        result = CliRunner().invoke(version, [])
+        result = CliRunner().invoke(get_platform_helper_for_project, [])
 
         assert result.exit_code == 0
         mock_required_version.assert_called_with(None)
@@ -35,7 +35,7 @@ class TestVersionCommand:
     ):
         mock_required_version.return_value = "1.2.3"
 
-        result = CliRunner().invoke(version, ["--pipeline", "main"])
+        result = CliRunner().invoke(get_platform_helper_for_project, ["--pipeline", "main"])
 
         assert result.exit_code == 0
         mock_required_version.assert_called_with("main")
@@ -52,7 +52,7 @@ class TestVersionCommand:
     ):
         mock_required_version.side_effect = PlatformException("Something bad happened")
 
-        result = CliRunner().invoke(version, ["--pipeline", "main"])
+        result = CliRunner().invoke(get_platform_helper_for_project, ["--pipeline", "main"])
 
         assert result.exit_code == 1
         mock_click.assert_called_with("Error: Something bad happened", err=True, fg="red")
