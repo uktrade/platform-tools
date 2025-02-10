ef126c680a6d1812df672f39c61c508672f7eb53
Anthony Roy <81255001+antroy-madetech@users.noreply.github.com>
2025-02-10 16:30:34 +0000

    fix: DBTP-1784: Passing a full path as the first mkfile parameter causes a file not found error (#768)

--- dbt_platform_helper/providers/files.py
+++ dbt_platform_helper/providers/files.py
@@ -1,4 +1,3 @@
-from os import makedirs
 from pathlib import Path
 
 
@@ -8,26 +7,21 @@ class FileProvider:
         pass
 
     @staticmethod
-    def mkfile(base_path: str, file_path: str, contents: str, overwrite=False) -> str:
-        file_path = Path(file_path)
-        file = Path(base_path).joinpath(file_path)
-        file_exists = file.exists()
-
-        if not file_path.parent.exists():
-            makedirs(file_path.parent)
-
+    def mkfile(base_path: str, file_name: str, contents: str, overwrite=False) -> str:
+        file_path = Path(base_path).joinpath(file_name)
+        file_exists = file_path.exists()
         if file_exists and not overwrite:
             return f"File {file_path} exists; doing nothing"
 
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.write_text(contents)
+
         action = "overwritten" if file_exists and overwrite else "created"
-
-        file.write_text(contents)
-
-        return f"File {file_path} {action}"
+        return f"File {file_name} {action}"
 
     @staticmethod
-    def delete_file(base_path: str, file: str):
-        file_path = Path(base_path) / file
+    def delete_file(base_path: str, file_name: str):
+        file_path = Path(base_path) / file_name
         if file_path.exists():
             file_path.unlink()
             return f"{str(file_path)} has been deleted"
--- tests/platform_helper/providers/test_files.py
+++ tests/platform_helper/providers/test_files.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 
 from dbt_platform_helper.providers.files import FileProvider
@@ -37,23 +35,41 @@ def test_mkfile_does_nothing_if_file_already_exists_but_override_is_false(tmp_pa
         str(tmp_path), filename, contents="does not matter", overwrite=False
     )
 
-    assert message == f"File {filename} exists; doing nothing"
+    assert message == f"File {file_path} exists; doing nothing"
 
 
-@pytest.mark.parametrize(
-    "file_exists, expected_message",
-    [(True, "a_folder/some_file.txt has been deleted"), (False, None)],
-)
-def test_delete_file_deletes_the_file(fs, file_exists, expected_message):
-    filename = "some_file.txt"
+def test_mkfile_can_write_to_a_file_in_a_non_existent_directory(tmp_path):
+    path = tmp_path / "test_dir/test_subdir"
+    filename = "test_file.txt"
+
+    message = FileProvider().mkfile(str(path), filename, contents="does not matter")
+
+    file_path = path / filename
+    assert file_path.exists()
+    assert message == "File test_file.txt created"
+
+
+def test_delete_file_deletes_the_file(tmp_path):
     folder = "a_folder"
-    folder_path = Path(folder)
+    filename = "some_file.txt"
+    folder_path = tmp_path / folder
     file_path = folder_path / filename
-    if file_exists:
-        folder_path.mkdir(parents=True)
-        file_path.touch()
+    folder_path.mkdir(parents=True)
+    file_path.touch()
 
-    message = FileProvider().delete_file(folder, filename)
+    message = FileProvider().delete_file(str(folder_path), filename)
 
     assert not file_path.exists()
-    assert message == expected_message
+    assert message == f"{file_path} has been deleted"
+
+
+def test_delete_file_does_nothing_if_file_didnt_exist(tmp_path):
+    folder = "a_folder"
+    filename = "some_file.txt"
+    folder_path = tmp_path / folder
+    file_path = folder_path / filename
+
+    message = FileProvider().delete_file(str(folder_path), filename)
+
+    assert not file_path.exists()
+    assert message is None
