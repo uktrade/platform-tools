61f5ab12f048b7278ae317d276283de2f82cbdc9
David Williams <david.williams@digital.trade.gov.uk>
2025-02-18 17:15:15 +0000

    Refactor copilot command

--- dbt_platform_helper/commands/copilot.py
+++ dbt_platform_helper/commands/copilot.py
@@ -5,6 +5,7 @@ import click
 from dbt_platform_helper.domain.config_validator import ConfigValidator
 from dbt_platform_helper.domain.copilot import Copilot
 from dbt_platform_helper.providers.config import ConfigProvider
+from dbt_platform_helper.providers.files import FileProvider
 from dbt_platform_helper.providers.io import ClickIOProvider
 from dbt_platform_helper.utils.click import ClickDocOptGroup
 from dbt_platform_helper.utils.versioning import (
@@ -22,6 +23,6 @@ def make_addons():
     """Generate addons CloudFormation for each environment."""
     try:
         config_provider = ConfigProvider(ConfigValidator())
-        Copilot()._make_addons(config_provider)
+        Copilot(config_provider, FileProvider()).make_addons()
     except Exception as err:
         ClickIOProvider().abort_with_error(str(err))
