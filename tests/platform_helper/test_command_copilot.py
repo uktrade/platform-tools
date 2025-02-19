9b14ea022b3f8ea00305a0a9173932f6d2da62d2
Connor Hindle <Connor.Hindle@digital.trade.gov.uk>
2025-02-19 11:12:58 +0000

    add additonal assertions to copilot command unit tests

--- tests/platform_helper/test_command_copilot.py
+++ tests/platform_helper/test_command_copilot.py
@@ -1,3 +1,4 @@
+from unittest.mock import Mock
 from unittest.mock import patch
 
 from click.testing import CliRunner
@@ -6,30 +7,54 @@ from dbt_platform_helper.commands.copilot import make_addons
 
 
 class TestMakeAddonsCommand:
-    @patch("dbt_platform_helper.commands.copilot.Copilot.make_addons")
-    @patch("click.secho")
+    @patch("dbt_platform_helper.commands.copilot.Copilot")
+    @patch("dbt_platform_helper.commands.copilot.ConfigProvider")
+    @patch("dbt_platform_helper.commands.copilot.ConfigValidator")
+    @patch("dbt_platform_helper.commands.copilot.FileProvider")
     def test_calls_make_addons(
         self,
-        mock_click,
-        mock_make_addons,
+        mock_file_provider,
+        mock_config_validator,
+        mock_config_provider,
+        mock_copilot,
     ):
-        mock_make_addons.return_value = None
+        mock_copilot_instance = mock_copilot.return_value
+        mock_config_validator.return_value = Mock()
 
         result = CliRunner().invoke(make_addons, [])
 
         assert result.exit_code == 0
-        mock_make_addons.assert_called_with()
+        mock_config_validator.assert_called_once()
+        mock_config_provider.assert_called_once_with(mock_config_validator.return_value)
+        mock_copilot.assert_called_with(
+            mock_config_provider.return_value, mock_file_provider.return_value
+        )
+        mock_copilot_instance.make_addons.assert_called_once()
 
-    @patch("dbt_platform_helper.commands.copilot.Copilot.make_addons")
+    @patch("dbt_platform_helper.commands.copilot.Copilot")
+    @patch("dbt_platform_helper.commands.copilot.ConfigProvider")
+    @patch("dbt_platform_helper.commands.copilot.ConfigValidator")
+    @patch("dbt_platform_helper.commands.copilot.FileProvider")
     @patch("click.secho")
     def test_prints_error_message_if_exception_is_thrown_by_make_addons(
         self,
         mock_click,
-        mock_make_addons,
+        mock_file_provider,
+        mock_config_validator,
+        mock_config_provider,
+        mock_copilot,
     ):
-        mock_make_addons.side_effect = Exception("Something bad happened")
+        mock_copilot_instance = mock_copilot.return_value
+
+        mock_copilot_instance.make_addons.side_effect = Exception("Something bad happened")
 
         result = CliRunner().invoke(make_addons, [])
 
         assert result.exit_code == 1
+        mock_config_validator.assert_called_once()
+        mock_config_provider.assert_called_once_with(mock_config_validator.return_value)
+        mock_copilot.assert_called_with(
+            mock_config_provider.return_value, mock_file_provider.return_value
+        )
+        mock_copilot_instance.make_addons.assert_called_once()
         mock_click.assert_called_with("Error: Something bad happened", err=True, fg="red")
