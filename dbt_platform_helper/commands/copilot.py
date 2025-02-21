b90c3b6942ffbf6249b8747fa7f72e53e5b988a1
Kate Sugden <kate.sugden@madetech.com>
2025-02-21 10:35:52 +0000

    remove spurious import

--- dbt_platform_helper/commands/copilot.py
+++ dbt_platform_helper/commands/copilot.py
@@ -7,15 +7,11 @@ from dbt_platform_helper.domain.copilot_environment import CopilotTemplating
 from dbt_platform_helper.providers.config import ConfigProvider
 from dbt_platform_helper.providers.config_validator import ConfigValidator
 from dbt_platform_helper.providers.files import FileProvider
+from dbt_platform_helper.providers.io import ClickIOProvider
 from dbt_platform_helper.providers.platform_helper_versioning import (
     PlatformHelperVersioning,
 )
-from dbt_platform_helper.providers.io import ClickIOProvider
 from dbt_platform_helper.utils.click import ClickDocOptGroup
-from dbt_platform_helper.utils.versioning import (
-    check_platform_helper_version_needs_update,
-)
-
 
 # TODOs
 # Figure out a pattern for copilot templating and the new copilot domain - probably a lot of overlap here that really belongs in the copilottemplating domain instead (atleast whatever is concerned with "templating")
