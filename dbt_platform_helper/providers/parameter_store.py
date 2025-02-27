7e593c64c05aba17db6e94b1cd37f8336553105b
Connor Hindle <Connor.Hindle@digital.trade.gov.uk>
2025-02-27 17:11:24 +0000

    DBTP-1829 first pass at get_ssm_parameters_by_path method

--- dbt_platform_helper/providers/parameter_store.py
+++ dbt_platform_helper/providers/parameter_store.py
@@ -19,14 +19,25 @@ class ParameterStore:
             raise ParameterNotFoundForNameException()
 
     def get_ssm_parameters_by_path(self, path: str):
+        """Retrieves all SSM parameters for a given path from parameter
+        store."""
 
-        response = self.ssm_client.get_parameters_by_path(
-            Path=path,
-            Recursive=True,
-        )
+        parameters = []
+        paginator = self.ssm_client.get_paginator("get_parameters_by_path")
+        page_iterator = paginator.paginate(Path=path, Recursive=True)
 
-        return response["parameters"]
+        for page in page_iterator:
+            parameters.extend(page.get("Parameters", []))
+
+        if parameters:
+            return parameters
+        else:
+            raise ParameterNotFoundForPathException()
 
 
 class ParameterNotFoundForNameException(PlatformException):
     """Exception raised when a given parameter name cannot be found in SSM."""
+
+
+class ParameterNotFoundForPathException(PlatformException):
+    """Exception raised when no parameters are found for a given path."""
