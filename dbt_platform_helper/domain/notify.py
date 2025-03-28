f1bea26c140b78974b8ba0ccafba1bbee13eb926
Kate Sugden <kate.sugden@madetech.com>
2025-03-28 12:36:39 +0000

    rename and reorder args

--- dbt_platform_helper/domain/notify.py
+++ dbt_platform_helper/domain/notify.py
@@ -9,22 +9,20 @@ class SlackChannelNotifier:
         self.client = WebClient(slack_token)
         self.slack_channel_id = slack_channel_id
 
-    def post_update(self, message_ref, message, context_elements=None):
+    def post_update(self, message_ref, message, context=None):
         args = {
             "channel": self.slack_channel_id,
-            "blocks": self._build_message_blocks(context_elements, message),
+            "blocks": self._build_message_blocks(message, context),
             "text": message,
             "unfurl_links": False,
             "unfurl_media": False,
         }
         self.client.chat_update(ts=message_ref, **args)
 
-    def post_new(
-        self, message, context_elements=None, title=None, reply_broadcast=None, thread_ref=None
-    ):
+    def post_new(self, message, context=None, title=None, reply_broadcast=None, thread_ref=None):
         args = {
             "channel": self.slack_channel_id,
-            "blocks": self._build_message_blocks(context_elements, message),
+            "blocks": self._build_message_blocks(message, context),
             "text": title if title else message,
             "reply_broadcast": reply_broadcast,
             "unfurl_links": False,
@@ -33,20 +31,17 @@ class SlackChannelNotifier:
         }
         self.client.chat_postMessage(ts=None, **args)
 
-    def _build_message_blocks(self, context_elements, message):
+    def _build_message_blocks(self, message, context):
         message_blocks = [
             blocks.SectionBlock(
                 text=blocks.TextObject(type="mrkdwn", text=message),
             ),
         ]
 
-        if context_elements:
+        if context:
             message_blocks.append(
                 blocks.ContextBlock(
-                    elements=[
-                        blocks.TextObject(type="mrkdwn", text=element)
-                        for element in context_elements
-                    ]
+                    elements=[blocks.TextObject(type="mrkdwn", text=element) for element in context]
                 )
             )
         return message_blocks
@@ -91,7 +86,7 @@ class Notify:
         self.notifier.post_new(
             message=message,
             title=title,
-            context_elements=[],
+            context=[],
             reply_broadcast=send_to_main_channel,
             thread_ref=slack_ref,
         )
