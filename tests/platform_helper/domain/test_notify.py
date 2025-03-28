822dcca35b89811e50ec0202e7381e1beee1c377
Kate Sugden <kate.sugden@madetech.com>
2025-03-28 16:11:02 +0000

    shift slack client test coverage to provider

--- tests/platform_helper/domain/test_notify.py
+++ tests/platform_helper/domain/test_notify.py
@@ -1,3 +1,4 @@
+from unittest.mock import Mock
 from unittest.mock import patch
 
 import pytest
@@ -18,6 +19,125 @@ def test_getting_build_url():
     )
     exp_url = "https://region.console.aws.amazon.com/codesuite/codebuild/000000000000/projects/project/build/project%3Aexample-build-id"
     assert actual_url == exp_url
+
+
+class TestSlackChannelNotifier:
+    @pytest.mark.parametrize(
+        "slack_ref, context",
+        (
+            ("10000.10", None),
+            ("10000.10", ["A"]),
+            ("10000.10", ["A", "B"]),
+        ),
+    )
+    @patch("dbt_platform_helper.domain.notify.WebClient")
+    def test_post_update_calls_client_with_expected_arguments(
+        self,
+        mock_webclient,
+        slack_ref,
+        context,
+    ):
+        mock_slack_client_instance = Mock()
+        mock_webclient.return_value = mock_slack_client_instance
+
+        SlackChannelNotifier("my-slack-token", "my-slack-channel-id").post_update(
+            message="The very important thing everyone should know",
+            message_ref=slack_ref,
+            context=context,
+        )
+
+        mock_webclient.assert_called_with("my-slack-token")
+
+        post_calls = mock_slack_client_instance.chat_postMessage.call_args_list
+        update_calls = mock_slack_client_instance.chat_update.call_args_list
+
+        assert len(update_calls) == 1
+        assert len(post_calls) == 0
+
+        call_args = update_calls[0].kwargs
+
+        assert call_args["ts"] == slack_ref
+        assert call_args["channel"] == "my-slack-channel-id"
+        assert call_args["text"] == "The very important thing everyone should know"
+        assert not call_args["unfurl_links"]
+        assert not call_args["unfurl_media"]
+        assert call_args["blocks"][0].text.text == "The very important thing everyone should know"
+
+        if context:
+            assert len(call_args["blocks"]) == 2
+            actual_block_elements = [element.text for element in call_args["blocks"][1].elements]
+            assert actual_block_elements == context
+        else:
+            assert len(call_args["blocks"]) == 1
+
+    @pytest.mark.parametrize(
+        "slack_ref, context, title, reply_broadcast",
+        (
+            (None, None, "A Title", True),
+            (None, ["A"], "A Title", True),
+            (None, ["A", "B"], "A Title", True),
+        ),
+    )
+    @patch("dbt_platform_helper.domain.notify.WebClient")
+    def test_post_new_calls_client_with_expected_arguments(
+        self,
+        mock_webclient,
+        slack_ref,
+        context,
+        title,
+        reply_broadcast,
+    ):
+        mock_slack_client_instance = Mock()
+        mock_webclient.return_value = mock_slack_client_instance
+
+        SlackChannelNotifier("my-slack-token", "my-slack-channel-id").post_new(
+            message="The comment",
+            context=context,
+            title=title,
+            reply_broadcast=reply_broadcast,
+            thread_ref=slack_ref,
+        )
+
+        mock_webclient.assert_called_with("my-slack-token")
+
+        post_calls = mock_slack_client_instance.chat_postMessage.call_args_list
+        update_calls = mock_slack_client_instance.chat_update.call_args_list
+
+        assert len(post_calls) == 1
+        assert len(update_calls) == 0
+
+        call_args = post_calls[0].kwargs
+
+        assert call_args["thread_ts"] == slack_ref
+        assert call_args["channel"] == "my-slack-channel-id"
+        assert call_args["text"] == "A Title"
+        assert not call_args["unfurl_links"]
+        assert not call_args["unfurl_media"]
+        assert call_args["blocks"][0].text.text == "The comment"
+        assert call_args["reply_broadcast"] == reply_broadcast
+
+        if context:
+            assert len(call_args["blocks"]) == 2
+            actual_block_elements = [element.text for element in call_args["blocks"][1].elements]
+            assert actual_block_elements == context
+        else:
+            assert len(call_args["blocks"]) == 1
+
+    @patch("dbt_platform_helper.domain.notify.WebClient")
+    def test_post_new_text_defaults_to_message_if_no_title(
+        self,
+        mock_webclient,
+    ):
+        mock_slack_client_instance = Mock()
+        mock_webclient.return_value = mock_slack_client_instance
+
+        SlackChannelNotifier("my-slack-token", "my-slack-channel-id").post_new(
+            message="The comment",
+            title=None,
+        )
+
+        call_args = mock_slack_client_instance.chat_postMessage.call_args_list[0].kwargs
+        assert call_args["text"] == "The comment"
 
 
 class TestNotify:
@@ -161,6 +281,3 @@ class TestNotify:
         assert call_args["unfurl_media"] == False
         assert call_args["thread_ts"] == "1234.56"
         assert call_args["blocks"][0].text.text == "The comment"
-
-
-# TODO add a test for the slack client to ensure the webclient is correctly set, or maybe just remove the SlackClient wrapper
