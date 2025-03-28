d423ff210741b153e5c58188517f8d1b36391da4
Kate Sugden <kate.sugden@madetech.com>
2025-03-28 11:38:58 +0000

    Move notify tests to a class

--- tests/platform_helper/domain/test_notify.py
+++ tests/platform_helper/domain/test_notify.py
@@ -20,140 +20,147 @@ def test_getting_build_url():
     assert actual_url == exp_url
 
 
-@pytest.mark.parametrize(
-    "slack_ref, repository, sha, build_arn, expected_text, expect_update",
-    (
-        (None, None, None, None, [], False),
-        ("10000.10", None, None, None, [], True),
-        (None, "repo1", None, None, [EXP_REPO_TEXT % {"name": "repo1"}], False),
+class TestNotify:
+    @pytest.mark.parametrize(
+        "slack_ref, repository, sha, build_arn, expected_text, expect_update",
         (
-            "10000.10",
-            "repo1",
-            None,
-            None,
-            [EXP_REPO_TEXT % {"name": "repo1"}],
-            True,
+            (None, None, None, None, [], False),
+            ("10000.10", None, None, None, [], True),
+            (None, "repo1", None, None, [EXP_REPO_TEXT % {"name": "repo1"}], False),
+            (
+                "10000.10",
+                "repo1",
+                None,
+                None,
+                [EXP_REPO_TEXT % {"name": "repo1"}],
+                True,
+            ),
+            (
+                None,
+                "repo2",
+                "abc1234",
+                None,
+                [
+                    EXP_REPO_TEXT % {"name": "repo2"},
+                    EXP_SHA_TEXT % {"name": "repo2", "sha": "abc1234"},
+                ],
+                False,
+            ),
+            (
+                "10000.10",
+                "repo2",
+                "abc1234",
+                None,
+                [
+                    EXP_REPO_TEXT % {"name": "repo2"},
+                    EXP_SHA_TEXT % {"name": "repo2", "sha": "abc1234"},
+                ],
+                True,
+            ),
+            (None, None, None, BUILD_ARN, [BUILD_ARN_MESSAGE], False),
+            ("10000.10", None, None, BUILD_ARN, [BUILD_ARN_MESSAGE], True),
+            (
+                None,
+                "repo3",
+                "xyz1234",
+                BUILD_ARN,
+                [
+                    EXP_REPO_TEXT % {"name": "repo3"},
+                    EXP_SHA_TEXT % {"name": "repo3", "sha": "xyz1234"},
+                    BUILD_ARN_MESSAGE,
+                ],
+                False,
+            ),
+            (
+                "10000.10",
+                "repo3",
+                "xyz1234",
+                BUILD_ARN,
+                [
+                    EXP_REPO_TEXT % {"name": "repo3"},
+                    EXP_SHA_TEXT % {"name": "repo3", "sha": "xyz1234"},
+                    BUILD_ARN_MESSAGE,
+                ],
+                True,
+            ),
         ),
-        (
-            None,
-            "repo2",
-            "abc1234",
-            None,
-            [EXP_REPO_TEXT % {"name": "repo2"}, EXP_SHA_TEXT % {"name": "repo2", "sha": "abc1234"}],
-            False,
-        ),
-        (
-            "10000.10",
-            "repo2",
-            "abc1234",
-            None,
-            [EXP_REPO_TEXT % {"name": "repo2"}, EXP_SHA_TEXT % {"name": "repo2", "sha": "abc1234"}],
-            True,
-        ),
-        (None, None, None, BUILD_ARN, [BUILD_ARN_MESSAGE], False),
-        ("10000.10", None, None, BUILD_ARN, [BUILD_ARN_MESSAGE], True),
-        (
-            None,
-            "repo3",
-            "xyz1234",
-            BUILD_ARN,
-            [
-                EXP_REPO_TEXT % {"name": "repo3"},
-                EXP_SHA_TEXT % {"name": "repo3", "sha": "xyz1234"},
-                BUILD_ARN_MESSAGE,
-            ],
-            False,
-        ),
-        (
-            "10000.10",
-            "repo3",
-            "xyz1234",
-            BUILD_ARN,
-            [
-                EXP_REPO_TEXT % {"name": "repo3"},
-                EXP_SHA_TEXT % {"name": "repo3", "sha": "xyz1234"},
-                BUILD_ARN_MESSAGE,
-            ],
-            True,
-        ),
-    ),
-)
-@patch("dbt_platform_helper.domain.notify.WebClient")
-def test_environment_progress(
-    mock_slack_client,
-    slack_ref,
-    repository,
-    sha,
-    build_arn,
-    expected_text: list[str],
-    expect_update: bool,
-):
-    slack_notifier = SlackChannelNotifier("my-slack-token", "my-slack-channel-id")
-    slack_notifier.client = mock_slack_client
-
-    Notify(slack_notifier).environment_progress(
-        message="The very important thing everyone should know",
-        slack_ref=slack_ref,
-        repository=repository,
-        commit_sha=sha,
-        build_arn=build_arn,
     )
+    @patch("dbt_platform_helper.domain.notify.WebClient")
+    def test_environment_progress(
+        self,
+        mock_slack_client,
+        slack_ref,
+        repository,
+        sha,
+        build_arn,
+        expected_text: list[str],
+        expect_update: bool,
+    ):
+        slack_notifier = SlackChannelNotifier("my-slack-token", "my-slack-channel-id")
+        slack_notifier.client = mock_slack_client
 
-    post_calls = mock_slack_client.chat_postMessage.call_args_list
-    update_calls = mock_slack_client.chat_update.call_args_list
+        Notify(slack_notifier).environment_progress(
+            message="The very important thing everyone should know",
+            slack_ref=slack_ref,
+            repository=repository,
+            commit_sha=sha,
+            build_arn=build_arn,
+        )
 
-    if expect_update:
-        calls = update_calls
-        zero_calls = post_calls
-    else:
-        calls = post_calls
-        zero_calls = update_calls
+        post_calls = mock_slack_client.chat_postMessage.call_args_list
+        update_calls = mock_slack_client.chat_update.call_args_list
 
-    assert len(calls) == 1
-    assert len(zero_calls) == 0
+        if expect_update:
+            calls = update_calls
+            zero_calls = post_calls
+        else:
+            calls = post_calls
+            zero_calls = update_calls
 
-    call_args = calls[0].kwargs
-    assert call_args["channel"] == "my-slack-channel-id"
-    assert call_args["text"] == "The very important thing everyone should know"
-    assert not call_args["unfurl_links"]
-    assert not call_args["unfurl_media"]
-    assert call_args["blocks"][0].text.text == "The very important thing everyone should know"
+        assert len(calls) == 1
+        assert len(zero_calls) == 0
 
-    if expected_text:
-        actual_elements = call_args["blocks"][1].elements
-        assert len(actual_elements) == len(expected_text)
-        for element, exp_text in zip(actual_elements, expected_text):
-            assert element.text == exp_text
+        call_args = calls[0].kwargs
+        assert call_args["channel"] == "my-slack-channel-id"
+        assert call_args["text"] == "The very important thing everyone should know"
+        assert not call_args["unfurl_links"]
+        assert not call_args["unfurl_media"]
+        assert call_args["blocks"][0].text.text == "The very important thing everyone should know"
 
+        if expected_text:
+            actual_elements = call_args["blocks"][1].elements
+            assert len(actual_elements) == len(expected_text)
+            for element, exp_text in zip(actual_elements, expected_text):
+                assert element.text == exp_text
 
-@pytest.mark.parametrize(
-    "title, broadcast, expected_text",
-    (
-        (None, False, "The comment"),
-        (None, True, "The comment"),
-        ("My title", False, "My title"),
-        ("My title", True, "My title"),
-    ),
-)
-@patch("dbt_platform_helper.domain.notify.WebClient")
-def test_add_comment(mock_slack_client, title: str, broadcast: bool, expected_text: str):
-    slack_notifier = SlackChannelNotifier("token", "my-slack-channel-id")
-    slack_notifier.client = mock_slack_client
-
-    Notify(slack_notifier).add_comment(
-        "1234.56", message="The comment", title=title, send_to_main_channel=broadcast
+    @pytest.mark.parametrize(
+        "title, broadcast, expected_text",
+        (
+            (None, False, "The comment"),
+            (None, True, "The comment"),
+            ("My title", False, "My title"),
+            ("My title", True, "My title"),
+        ),
     )
+    @patch("dbt_platform_helper.domain.notify.WebClient")
+    def test_add_comment(self, mock_slack_client, title: str, broadcast: bool, expected_text: str):
+        slack_notifier = SlackChannelNotifier("token", "my-slack-channel-id")
+        slack_notifier.client = mock_slack_client
 
-    calls = mock_slack_client.chat_postMessage.call_args_list
-    assert len(calls) == 1
-    call_args = calls[0].kwargs
-    assert call_args["channel"] == "my-slack-channel-id"
-    assert call_args["text"] == expected_text
-    assert call_args["reply_broadcast"] == broadcast
-    assert call_args["unfurl_links"] == False
-    assert call_args["unfurl_media"] == False
-    assert call_args["thread_ts"] == "1234.56"
-    assert call_args["blocks"][0].text.text == "The comment"
+        Notify(slack_notifier).add_comment(
+            "1234.56", message="The comment", title=title, send_to_main_channel=broadcast
+        )
+
+        calls = mock_slack_client.chat_postMessage.call_args_list
+        assert len(calls) == 1
+        call_args = calls[0].kwargs
+        assert call_args["channel"] == "my-slack-channel-id"
+        assert call_args["text"] == expected_text
+        assert call_args["reply_broadcast"] == broadcast
+        assert call_args["unfurl_links"] == False
+        assert call_args["unfurl_media"] == False
+        assert call_args["thread_ts"] == "1234.56"
+        assert call_args["blocks"][0].text.text == "The comment"
 
 
 # TODO add a test for the slack client to ensure the webclient is correctly set, or maybe just remove the SlackClient wrapper
