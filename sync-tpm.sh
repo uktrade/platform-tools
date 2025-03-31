0ab7d61cba29c6ec9cfe3c2a86e0c72bbdeb0af3
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2025-03-31 17:24:31 +0100

    Updated tpm

--- sync-tpm.sh
+++ sync-tpm.sh
@@ -4,6 +4,12 @@ rsync -r --exclude ".terraform*" ../terraform-platform-modules/* terraform/
 
 for f in $(git status --porcelain | grep "^ M" | sed 's/^ M //')
 do
-  sed -i 's/source( *= *)"git::ssh://git@github.com/uktrade/platform-tools.git//terraform/extensions\?depth=1&ref="/source$1"git::ssh://git@github.com/uktrade/terraform-platform-modules.git//extensions\?depth=1&ref="/' "$f"
+  sed -i 's#source\( *= *\)"git::ssh://git@github.com/uktrade/platform-tools.git//terraform/extensions\?depth=1&ref="#source\1"git::ssh://git@github.com/uktrade/terraform-platform-modules.git//extensions\?depth=1&ref="#' "$f"
 done
 
+rm terraform/CHANGELOG.md
+rm terraform/codebase-pipelines/ssm.tf
+rm terraform/codecov.yml
+rm terraform/poetry.lock
+rm terraform/pyproject.toml
+rm terraform/release-config.json
