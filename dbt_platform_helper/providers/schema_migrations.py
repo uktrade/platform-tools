8f0348f90fbf2f19ffe45ddf1eda5115ed0d372b
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2025-04-01 18:22:06 +0100

    Remaining migrations

--- dbt_platform_helper/providers/schema_migrations.py
+++ dbt_platform_helper/providers/schema_migrations.py
@@ -20,14 +20,33 @@ class SchemaV1ToV2Migration:
         return 1
 
     def migrate(self, platform_config: dict) -> dict:
-        output = deepcopy(platform_config)
+        migrated_config = deepcopy(platform_config)
 
-        if "default_versions" in output:
-            default_versions = output["default_versions"]
+        self._remove_terraform_platform_modules_default_version(migrated_config)
+        self._remove_terraform_platform_modules_from_env_config(migrated_config)
+        self._remove_to_account_and_from_account_from_database_copy(migrated_config)
+
+        return migrated_config
+
+    def _remove_terraform_platform_modules_from_env_config(self, migrated_config: dict) -> None:
+        for env in migrated_config.get("environments", {}).values():
+            if env and "versions" in env:
+                del env["versions"]
+
+    def _remove_terraform_platform_modules_default_version(self, migrated_config: dict) -> None:
+        if "default_versions" in migrated_config:
+            default_versions = migrated_config["default_versions"]
             if "terraform-platform-modules" in default_versions:
                 del default_versions["terraform-platform-modules"]
 
-        return output
+    def _remove_to_account_and_from_account_from_database_copy(self, migrated_config: dict) -> None:
+        for extension in migrated_config.get("extensions", {}).values():
+            if extension.get("type") == "postgres" and "database_copy" in extension:
+                for database_copy_block in extension["database_copy"]:
+                    if "from_account" in database_copy_block:
+                        del database_copy_block["from_account"]
+                    if "to_account" in database_copy_block:
+                        del database_copy_block["to_account"]
 
 
 ALL_MIGRATIONS = [SchemaV1ToV2Migration()]
--- tests/platform_helper/providers/test_schema_migrations.py
+++ tests/platform_helper/providers/test_schema_migrations.py
@@ -25,6 +25,89 @@ class TestSchemaV1ToV2Migration:
         assert actual_config != config
         assert actual_config == {"default_versions": {"platform-helper": "14.0.0"}}
 
+    def test_removes_version_overrides_from_envs(self):
+        migration = SchemaV1ToV2Migration()
+        config = {
+            "environments": {
+                "*": {
+                    "requires_approval": False,
+                },
+                "dev": {"versions": {"terraform-platform-modules": "5.0.0"}},
+                "staging": None,
+            }
+        }
+
+        actual_config = migration.migrate(config)
+
+        expected_config = {
+            "environments": {
+                "*": {
+                    "requires_approval": False,
+                },
+                "dev": {},
+                "staging": None,
+            }
+        }
+        # Ensure migrate does not modify the original
+        assert actual_config != config
+        assert actual_config == expected_config
+
+    def test_removes_from_and_to_account_from_postgres_extensions(self):
+        migration = SchemaV1ToV2Migration()
+        config = {
+            "extensions": {
+                "first_postgres": {
+                    "type": "postgres",
+                    "database_copy": [
+                        {
+                            "from": "prod",
+                            "to": "hotfix",
+                            "from_account": "1234556",
+                            "to_account": "8888777",
+                            "pipeline": {"schedule": "0 0 2 * * *"},
+                        }
+                    ],
+                },
+                "second_postgres": {
+                    "type": "postgres",
+                    "database_copy": [
+                        {
+                            "from": "prod",
+                            "to": "hotfix",
+                            "from_account": "45563323",
+                            "to_account": "9998777",
+                        }
+                    ],
+                },
+            }
+        }
+
+        actual_config = migration.migrate(config)
+
+        exp_config = {
+            "extensions": {
+                "first_postgres": {
+                    "type": "postgres",
+                    "database_copy": [
+                        {"from": "prod", "to": "hotfix", "pipeline": {"schedule": "0 0 2 * * *"}}
+                    ],
+                },
+                "second_postgres": {
+                    "type": "postgres",
+                    "database_copy": [
+                        {
+                            "from": "prod",
+                            "to": "hotfix",
+                        }
+                    ],
+                },
+            }
+        }
+
+        # Ensure migrate does not modify the original
+        assert actual_config != config
+        assert actual_config == exp_config
+
 
 class MockMigration:
     def __init__(self, name, from_version, call_record_list):
