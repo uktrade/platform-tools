b0e26cbae5c0d4984d56a9d11332233356ec1be9
David Williams <david.williams@digital.trade.gov.uk>
2025-04-08 13:45:49 +0100

    Add ticket numbers to TODOs in constants.py

--- dbt_platform_helper/constants.py
+++ dbt_platform_helper/constants.py
@@ -1,8 +1,8 @@
 # Todo: Move to Config provider
 PLATFORM_CONFIG_FILE = "platform-config.yml"
-# Todo: Can we get rid of this yet?
+# Todo: DBTP-1950: Can we get rid of this yet?
 PLATFORM_HELPER_VERSION_FILE = ".platform-helper-version"
-# Todo: Move to ???
+# Todo: DBTP-1949: Move to ???
 DEFAULT_TERRAFORM_PLATFORM_MODULES_VERSION = "7"
 SUPPORTED_TERRAFORM_VERSION = "~> 1.8"
 SUPPORTED_AWS_PROVIDER_VERSION = "~> 5"
