480db280d69767b7c2ffac757e010aa38315698b
John Stainsby <john.stainsby@digital.trade.gov.uk>
2025-04-15 11:12:08 +0100

    feat: Add env var version override (DBTP-1578) (#849)

--- tests/platform_helper/domain/test_terraform_environment.py
+++ tests/platform_helper/domain/test_terraform_environment.py
@@ -7,60 +7,69 @@ from dbt_platform_helper.platform_exception import PlatformException
 from dbt_platform_helper.providers.config import ConfigProvider
 from dbt_platform_helper.providers.terraform_manifest import TerraformManifestProvider
 
+VALID_ENV_CONFIG = {
+    "vpc": "vpc3",
+    "accounts": {
+        "deploy": {"name": "non-prod-acc", "id": "1122334455"},
+        "dns": {"name": "non-prod-dns-acc", "id": "6677889900"},
+    },
+}
+
+VALID_ENRICHED_CONFIG = {
+    "application": "test-app",
+    "default_versions": {"platform-helper": "14.0.0"},
+    "environments": {"test": VALID_ENV_CONFIG},
+}
+
+INVALID_ENRICHED_CONFIG = {
+    "application": "test-app",
+    "environments": {"test": VALID_ENV_CONFIG},
+}
+
+
+class GenerateTerraformMocks:
+    def __init__(self):
+        self.mock_config_provider = Mock(spec=ConfigProvider)
+        self.mock_config_provider.get_enriched_config.return_value = VALID_ENRICHED_CONFIG
+        self.mock_manifest_provider = Mock(spec=TerraformManifestProvider)
+        self.mock_platform_helper_version_override = None
+
+    def params(self):
+        return {
+            "config_provider": self.mock_config_provider,
+            "manifest_provider": self.mock_manifest_provider,
+            "platform_helper_version_override": self.mock_platform_helper_version_override,
+        }
+
 
 class TestGenerateTerraform:
-
-    VALID_ENV_CONFIG = {
-        "vpc": "vpc3",
-        "accounts": {
-            "deploy": {"name": "non-prod-acc", "id": "1122334455"},
-            "dns": {"name": "non-prod-dns-acc", "id": "6677889900"},
-        },
-    }
-
-    VALID_ENRICHED_CONFIG = {
-        "application": "test-app",
-        "default_versions": {"platform-helper": "14.0.0"},
-        "environments": {"test": VALID_ENV_CONFIG},
-    }
-
-    INVALID_ENRICHED_CONFIG = {
-        "application": "test-app",
-        "environments": {"test": VALID_ENV_CONFIG},
-    }
-
     def test_raises_a_platform_exception_if_environment_does_not_exist_in_config(self):
-        mock_config_provider = Mock(spec=ConfigProvider)
-        mock_config_provider.get_enriched_config.return_value = self.VALID_ENRICHED_CONFIG
+        mocks = GenerateTerraformMocks()
+        terraform_environment = TerraformEnvironment(**mocks.params())
 
-        terraform_environment = TerraformEnvironment(
-            config_provider=mock_config_provider,
-            manifest_provider=Mock(),
-            io=Mock(),
-        )
         with pytest.raises(
             PlatformException,
             match="cannot generate terraform for environment not-an-environment.  It does not exist in your configuration",
         ):
             terraform_environment.generate("not-an-environment")
 
-    def test_generate_success(self):
+    @pytest.mark.parametrize(
+        "use_environment_variable_platform_helper_version, expected_platform_helper_version",
+        [(False, "14.0.0"), (True, "test-branch")],
+    )
+    def test_generate_success(
+        self, use_environment_variable_platform_helper_version, expected_platform_helper_version
+    ):
+        mocks = GenerateTerraformMocks()
         environment_name = "test"
-        platform_helper_version = "14.0.0"
 
-        mock_manifest_provider = Mock(spec=TerraformManifestProvider)
+        if use_environment_variable_platform_helper_version:
+            mocks.mock_platform_helper_version_override = "test-branch"
 
-        mock_config_provider = Mock(spec=ConfigProvider)
-        mock_config_provider.get_enriched_config.return_value = self.VALID_ENRICHED_CONFIG
-
-        terraform_environment = TerraformEnvironment(
-            config_provider=mock_config_provider,
-            manifest_provider=mock_manifest_provider,
-            io=Mock(),
-        )
+        terraform_environment = TerraformEnvironment(**mocks.params())
 
         terraform_environment.generate(environment_name)
 
-        mock_manifest_provider.generate_environment_config.assert_called_once_with(
-            self.VALID_ENRICHED_CONFIG, environment_name, platform_helper_version
+        mocks.mock_manifest_provider.generate_environment_config.assert_called_once_with(
+            VALID_ENRICHED_CONFIG, environment_name, expected_platform_helper_version
         )
--- tests/platform_helper/test_command_generate.py
+++ tests/platform_helper/test_command_generate.py
@@ -1,13 +1,8 @@
-from unittest.mock import Mock
-from unittest.mock import call
 from unittest.mock import patch
 
 from click.testing import CliRunner
 
 from dbt_platform_helper.commands.generate import generate as platform_helper_generate
-from dbt_platform_helper.constants import PLATFORM_HELPER_VERSION_FILE
-from dbt_platform_helper.providers.semantic_version import SemanticVersion
-from dbt_platform_helper.providers.version_status import PlatformHelperVersionStatus
 
 
 @patch("dbt_platform_helper.commands.copilot.get_aws_session_or_abort")
@@ -20,56 +15,3 @@ def test_platform_helper_generate_creates_the_pipeline_configuration_and_addons(
 
     mock_pipeline_domain_generate.assert_called_once()
     mock_copilot_domain_make_addons.assert_called_once()
-
-
-@patch("click.secho")
-@patch(
-    "dbt_platform_helper.domain.versioning.PlatformHelperVersioning._get_version_status",
-    new=Mock(
-        return_value=PlatformHelperVersionStatus(
-            installed=SemanticVersion(1, 0, 1),
-            deprecated_version_file=SemanticVersion(1, 0, 0),
-        )
-    ),
-)
-@patch("dbt_platform_helper.commands.copilot.get_aws_session_or_abort", new=Mock())
-@patch("dbt_platform_helper.domain.copilot.Copilot.make_addons", new=Mock())
-@patch("dbt_platform_helper.domain.pipelines.Pipelines.generate", new=Mock())
-def test_platform_helper_generate_shows_a_warning_when_version_is_different_than_on_file(
-    mock_secho,
-    tmp_path,
-    no_skipping_version_checks,
-):
-    CliRunner().invoke(platform_helper_generate)
-
-    mock_secho.assert_has_calls(
-        [
-            call(
-                "Please delete '.platform-helper-version' as it is now deprecated.\nCreate a section in the root of 'platform-config.yml':\n\ndefault_versions:\n  platform-helper: 1.0.0\n",
-                fg="magenta",
-            ),
-            call(
-                f"WARNING: You are running platform-helper v1.0.1 against v1.0.0 specified for the project.",
-                fg="magenta",
-            ),
-        ]
-    )
-
-
-@patch("dbt_platform_helper.commands.copilot.get_aws_session_or_abort", new=Mock())
-@patch("dbt_platform_helper.domain.copilot.Copilot.make_addons", new=Mock())
-@patch("dbt_platform_helper.domain.pipelines.Pipelines.generate", new=Mock())
-def test_platform_helper_generate_does_not_override_version_file_if_exists(
-    tmp_path,
-):
-    contents = "2.0.0"
-    version_file_path = tmp_path / PLATFORM_HELPER_VERSION_FILE
-    version_file_path.touch()
-    version_file_path.write_text(contents)
-
-    assert version_file_path.exists()
-
-    CliRunner().invoke(platform_helper_generate)
-
-    assert version_file_path.exists()
-    assert version_file_path.read_text() == contents
