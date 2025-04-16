ac7719d73cd40528dea8b521f684439ac9bd6c0f
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-04-16 15:25:15 +0100

    sort list of subnets for opensearch

--- terraform/opensearch/locals.tf
+++ terraform/opensearch/locals.tf
@@ -20,7 +20,7 @@ locals {
 
   instances  = coalesce(var.config.instances, 1)
   zone_count = var.config.enable_ha ? local.instances : null
-  subnets    = slice(tolist(data.aws_subnets.private-subnets.ids), 0, local.instances)
+  subnets = slice(sort(tolist(data.aws_subnets.private-subnets.ids)), 0, local.instances)
 
   auto_tune_desired_state       = startswith(var.config.instance, "t2") || startswith(var.config.instance, "t3") ? "DISABLED" : "ENABLED"
   auto_tune_rollback_on_disable = startswith(var.config.instance, "t2") || startswith(var.config.instance, "t3") ? "DEFAULT_ROLLBACK" : "NO_ROLLBACK"
