599a5a1e65c1ac86c9282f8e15863f26fef48b12
David Glover <david.glover@digital.trade.gov.uk>
2025-05-07 09:26:05 +0100

    feat: Update datadog terraform test and pre-commit format on test_terraform_manifest.py

--- tests/platform_helper/providers/test_terraform_manifest.py
+++ tests/platform_helper/providers/test_terraform_manifest.py
@@ -216,7 +216,10 @@ def test_generate_environment_config_creates_file(
         "services": '${local.config["extensions"]}',
         "env_config": "${local.env_config}",
     }
-    assert local["codebase_pipeline_repos"] == "${try({for k, v in local.config[\"codebase_pipelines\"]: k => v.repository}, null)}"
+    assert (
+        local["codebase_pipeline_repos"]
+        == '${try({for k, v in local.config["codebase_pipelines"]: k => v.repository}, null)}'
+    )
 
     terraform = json_content["terraform"]
     assert terraform["required_version"] == SUPPORTED_TERRAFORM_VERSION
