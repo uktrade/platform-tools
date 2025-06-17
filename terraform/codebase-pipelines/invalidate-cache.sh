1b0fff4e1052420b2058b38f7b3740a7e843254e
Kate Sugden <kate.sugden@madetech.com>
2025-06-17 12:04:20 +0100

    adapt invalidation script to work with CONFIG_JSON

--- terraform/codebase-pipelines/invalidate-cache.sh
+++ terraform/codebase-pipelines/invalidate-cache.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-# PATHS will be passed in as an argument
-# PATHS="/frontend/* /admin/*"
-
 CONFIG=${1}
 
-CONFIG='"{\"foo\":1, \"bar\":2, \"baz\":3}"'
+CONFIG='"{\"web.kate.demodjango.uktrade.digital\":[\"/hello/*\", \"/goodbye/*\"], \"api.kate.demodjango.uktrade.digital\":[\"/hello/*\", \"/goodbye/*\"]}"'
 
 DOMAINS=$(echo $CONFIG | jq -r 'fromjson | keys[]')
 
-DISTRIBUTION=$(aws cloudfront list-distributions --query 'DistributionList.Items' --output json | jq -r --arg domain ${1} '.[] | select(.Aliases.Items[0] == $domain) | .Id')
-
-echo -e "\nInvalidating the cache for paths ${2}\n"
-
-aws cloudfront create-invalidation \
-    --distribution-id $DISTRIBUTION \
-    --paths ${2}
-
-# With a list of domains:
-# export DOMAINS='["api.kate.demodjango.uktrade.digital", "web.kate.demodjango.uktrade.digital"]'
-
-# aws cloudfront list-distributions --query 'DistributionList.Items' --output json | jq -r --argjson domains "${DOMAINS}" '.[] | select(.Aliases.Items[0] as $d | $domains | index($d)) | .Id'
+for i in ${DOMAINS}; do
+    echo "Getting distribution ID for domain ${i}"
+    DISTRIBUTION_ID=$(aws cloudfront list-distributions --query 'DistributionList.Items' --output json | jq -r --arg domain ${i} '.[] | select(.Aliases.Items[0] == $domain) | .Id')
+    PATHS=$(echo $CONFIG | jq -r --arg domain ${i} 'fromjson | .[$domain] | join(" ")')
+    echo -e "\nInvalidating the cache in distibution id ${DISTRIBUTION_ID} for paths ${PATHS}\n"
+done
