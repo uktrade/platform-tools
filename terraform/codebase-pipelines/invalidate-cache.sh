ccc81579bb7aa4596de008c7cf6df6cb0a6a8995
Kate Sugden <kate.sugden@madetech.com>
2025-06-17 09:47:44 +0100

    pass parameters into script

--- terraform/codebase-pipelines/invalidate-cache.sh
+++ terraform/codebase-pipelines/invalidate-cache.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # PATHS will be passed in as an argument
-PATHS="/frontend/* /admin/*"
+# PATHS="/frontend/* /admin/*"
 
 DISTRIBUTION=$(aws cloudfront list-distributions --query 'DistributionList.Items' --output json | jq -r --arg domain ${1} '.[] | select(.Aliases.Items[0] == $domain) | .Id')
 
-echo -e "\nInvalidating the cache for paths ${PATHS}\n"
+echo -e "\nInvalidating the cache for paths ${2}\n"
 
 aws cloudfront create-invalidation \
     --distribution-id $DISTRIBUTION \
-    --paths $PATHS
+    --paths ${2}
 
 # With a list of domains:
 # export DOMAINS='["api.kate.demodjango.uktrade.digital", "web.kate.demodjango.uktrade.digital"]'
