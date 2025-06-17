9157a167b21c490c939a77f6db0619628c3edfca
David Glover <david.glover@digital.trade.gov.uk>
2025-06-17 16:05:03 +0100

    chore: terraform fmt

--- terraform/datadog/locals.tf
+++ terraform/datadog/locals.tf
@@ -1,6 +1,6 @@
 locals {
 
-# Local variables used to construct the metadata entity in datadog_software_catalog resource
+  # Local variables used to construct the metadata entity in datadog_software_catalog resource
 
   ## Datadog API / schema version to use for metadata
   api = <<EOF
@@ -46,8 +46,8 @@ EOF
 
   ## Create list of database types which can then be labelled as 'database' in the metadata
   db_list = ["postgres"]
-  
-# Map of 'services_to_monitor' to make it easier to iterate over when creating the datadog_software_catalog resources
+
+  # Map of 'services_to_monitor' to make it easier to iterate over when creating the datadog_software_catalog resources
   # use the coalesce function to provide a fallback value to use if backend service is null from platform-config.yml
   # i.e. this example where celery-worker is specified without backend services
   # services_to_monitor:
@@ -60,11 +60,11 @@ EOF
   #   ....
   services_to_monitor_map = merge([
     for front_service, back_services in var.config.services_to_monitor : {
-      for back_service in coalesce(back_services, []) : 
-        "${front_service}-${back_service}" => {
-          "back_service"   = back_service
-          "front_service" = front_service
-        }
+      for back_service in coalesce(back_services, []) :
+      "${front_service}-${back_service}" => {
+        "back_service"  = back_service
+        "front_service" = front_service
+      }
     }
   ]...)
 
