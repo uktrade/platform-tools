ea09848fb4ca41d7f2253be1f45367e30faaa4aa
Kate Sugden <kate.sugden@madetech.com>
2025-06-23 10:00:37 +0100

    PATHS variable is resolved within the codebuild job

--- terraform/codebase-pipelines/codebuild.tf
+++ terraform/codebase-pipelines/codebuild.tf
@@ -134,7 +134,7 @@ resource "aws_codebuild_webhook" "codebuild_webhook" {
 
 resource "aws_codebuild_project" "invalidate_cache" {
   name           = "${var.application}-${var.codebase}-invalidate-cache"
-  description    = "Invalidate the CDN cache paths"
+  description    = "Invalidate the CDN cached paths"
   build_timeout  = 30
   service_role   = aws_iam_role.codebase_deploy.arn
   encryption_key = aws_kms_key.artifact_store_kms_key.arn
@@ -153,11 +153,6 @@ resource "aws_codebuild_project" "invalidate_cache" {
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
-
-    environment_variable {
-      name  = "PATHS"
-      value = jsonencode(local.base_env_config)
-    }
   }
 
   logs_config {
