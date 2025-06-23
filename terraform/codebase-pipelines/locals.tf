1bcc82a389b4920d403f0bc9ecb871870bd40fcb
Kate Sugden <kate.sugden@madetech.com>
2025-06-23 09:40:16 +0100

    handle the case of no cache_invalidation config

--- terraform/codebase-pipelines/locals.tf
+++ terraform/codebase-pipelines/locals.tf
@@ -32,7 +32,7 @@ locals {
   dns_account_ids           = distinct([for env in local.base_env_config : env.dns_account])
   dns_account_assumed_roles = [for id in local.dns_account_ids : "arn:aws:iam::${id}:role/environment-pipeline-assumed-role"]
 
-  environments_requiring_cache_invalidation = distinct([for d in var.cache_invalidation.domains : d.environment])
+  environments_requiring_cache_invalidation = distinct([for d in try(values(var.cache_invalidation.domains),[]) : d.environment])
 
   cache_invalidation_enabled = length(local.environments_requiring_cache_invalidation) > 0
 
