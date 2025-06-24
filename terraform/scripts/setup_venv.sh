83ac9f22a012c7334346f7c69d374b229e6151af
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2025-06-24 14:01:49 +0100

    Move platform-helper install and venv setup into scripts

---
+++ terraform/scripts/setup_venv.sh
@@ -0,0 +1,7 @@
+## Set up python venv
+echo -e "Set up venv"
+rm -rf venv
+python -m venv --copies venv
+echo -e "\nUpdating paths"
+echo -e "\nActivating virtual environment"
+source venv/bin/activate
