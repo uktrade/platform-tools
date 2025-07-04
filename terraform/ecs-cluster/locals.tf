40fbe658354a9e85562fde7452eabf1a603d085d
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-07-04 14:05:02 +0100

    rename cluster

--- terraform/ecs-cluster/locals.tf
+++ terraform/ecs-cluster/locals.tf
@@ -5,7 +5,7 @@ locals {
     managed-by  = "DBT Platform - Terraform"
   }
 
-  cluster_name = "${var.application}-${var.environment}-tf"
+  cluster_name = "${var.application}-${var.environment}-cluster"
 
   is_production_env   = contains(["prod", "production", "PROD", "PRODUCTION"], var.environment)
   log_destination_arn = local.is_production_env ? "arn:aws:logs:eu-west-2:812359060647:destination:cwl_log_destination" : "arn:aws:logs:eu-west-2:812359060647:destination:platform-logging-logstash-distributor-non-production"
