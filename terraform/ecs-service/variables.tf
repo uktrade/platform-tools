75c6862a6124d02e5b9c414eac8d9bdf0524ee18
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-07-09 16:56:23 +0100

    ecs task definition

--- terraform/ecs-service/variables.tf
+++ terraform/ecs-service/variables.tf
@@ -10,25 +10,67 @@ variable "service_config" {
   type = object({
     name = string
     type = string
+
     http = object({
       path             = string
       target_container = string
       healthcheck = optional(object({
         path                = optional(string)
-        port                = optional(string)
+        port                = optional(number)
         success_codes       = optional(string)
-        healthy_threshold   = optional(string)
-        unhealthy_threshold = optional(string)
+        healthy_threshold   = optional(number)
+        unhealthy_threshold = optional(number)
         interval            = optional(string)
         timeout             = optional(string)
         grace_period        = optional(string)
       }))
     })
+
+    sidecars = optional(map(object({
+      port      = optional(number)
+      image     = optional(string)
+      variables = optional(map(string))
+      secrets   = optional(map(string))
+    })))
+
+    image = object({
+      port     = optional(number)
+      location = string
+    })
+
+    cpu    = number
+    memory = number
+    count  = number
+    exec   = optional(bool)
+
+    network = optional(object({
+      connect = optional(bool)
+      vpc = optional(object({
+        placement = optional(string)
+      }))
+    }))
+
+    storage = optional(object({
+      readonly_fs = bool
+    }))
+
+    variables = optional(map(any))
+    secrets   = optional(map(string))
+
+    environments = optional(map(object({
+      http = optional(object({
+        alb   = string
+        alias = optional(string)
+      }))
+      sidecars = optional(map(object({
+        variables = optional(map(any))
+      })))
+    })))
   })
 }
+
 
 variable "vpc_name" {
   type    = string
   default = "platform-sandbox-dev" #TODO - Remove hardcoding
 }
-
