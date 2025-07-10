9afd85a6da8fb94949cf6951610a01870c729a98
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-07-10 15:26:45 +0100

    remove unused locals

--- terraform/ecs-service/locals.tf
+++ terraform/ecs-service/locals.tf
@@ -6,10 +6,4 @@ locals {
   }
 
   service_name = var.service_config.name
-
-  # TODO
-  environment_overrides = lookup(var.service_config.environments, var.environment, {})
-  merged_variables      = merge(var.service_config["variables"], lookup(local.environment_overrides, "variables", {}))
-  merged_config         = merge(var.service_config, local.environment_overrides)
-
 }
