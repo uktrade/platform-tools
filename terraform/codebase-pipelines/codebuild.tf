e32fcf458e9e1450a62294b2bf4a4baac7577e6a
Kate Sugden <kate.sugden@madetech.com>
2025-07-30 12:16:53 +0100

    10 minutes should be long enough for cache invalidation build timeout

--- terraform/codebase-pipelines/codebuild.tf
+++ terraform/codebase-pipelines/codebuild.tf
@@ -135,7 +135,7 @@ resource "aws_codebuild_webhook" "codebuild_webhook" {
 resource "aws_codebuild_project" "invalidate_cache" {
   name           = "${var.application}-${var.codebase}-invalidate-cache"
   description    = "Invalidate the CDN cached paths"
-  build_timeout  = 30
+  build_timeout  = 10
   service_role   = aws_iam_role.codebase_deploy.arn
   encryption_key = aws_kms_key.artifact_store_kms_key.arn
 
