a74237cb07c62b686ded13bac9371db229a1e162
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-08-04 11:28:42 +0100

    add allow

--- terraform/extensions/iam.tf
+++ terraform/extensions/iam.tf
@@ -74,6 +74,7 @@ data "aws_iam_policy_document" "ecs_service_access_for_codebase" {
   }
 
   statement {
+    effect = "Allow"
     actions = [
       "ec2:DescribeVpcs"
     ]
@@ -83,6 +84,7 @@ data "aws_iam_policy_document" "ecs_service_access_for_codebase" {
   }
 
   statement {
+    effect = "Allow"
     actions = [
       "servicediscovery:ListNamespaces"
     ]
