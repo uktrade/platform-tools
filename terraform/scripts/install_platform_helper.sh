1d0f446e1093a4417f63d49e5f5bdb708745cfed
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-08-20 18:55:42 +0100

    revert + bump pyproject.toml

--- terraform/scripts/install_platform_helper.sh
+++ terraform/scripts/install_platform_helper.sh
@@ -1,12 +1,10 @@
 ## Build platform-helper
-#echo -e "\n\n### Build and install platform-helper cloned from ${PLATFORM_HELPER_VERSION}\n" #TODO
-echo -e "\n\n### Build and install platform-helper cloned from main\n"
+echo -e "\n\n### Build and install platform-helper cloned from ${PLATFORM_HELPER_VERSION}\n"
 echo -e "Installing dependencies"
 pip install poetry
 poetry install
 echo -e "\nBuild platform-helper\n"
-#poetry build --no-interaction --format sdist --no-ansi --local-version ${PLATFORM_HELPER_VERSION} #TODO
-poetry build --no-interaction --format sdist --no-ansi --local-version main
+poetry build --no-interaction --format sdist --no-ansi --local-version ${PLATFORM_HELPER_VERSION}
 echo -e "\nInstall platform-helper\n"
 most_recent_built_wheel_package=$(ls -t1 dist | grep ".whl" | head -1)
 pip install "dist/${most_recent_built_wheel_package}"
