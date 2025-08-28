4cf87b919406c106bb2176259480b658c78ad1f3
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-08-28 15:22:26 +0100

    add app to log group name

--- terraform/ecs-service/main.tf
+++ terraform/ecs-service/main.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_task_definition" "this" {
           logConfiguration = {
             logDriver = "awslogs"
             options = {
-              awslogs-group         = "/platform/ecs/service/${var.environment}/${var.service_config.name}"
+              awslogs-group         = "/platform/ecs/service/${var.application}/${var.environment}/${var.service_config.name}"
               awslogs-region        = data.aws_region.current.region
               awslogs-stream-prefix = "ecs"
             }
@@ -191,7 +191,7 @@ resource "aws_kms_key_policy" "ecs_service_logs_key_policy" {
 
 resource "aws_cloudwatch_log_group" "ecs_service_logs" {
   # checkov:skip=CKV_AWS_338:Retains logs for 30 days instead of 1 year
-  name              = "/platform/ecs/service/${var.environment}/${var.service_config.name}"
+  name              = "/platform/ecs/service/${var.application}/${var.environment}/${var.service_config.name}"
   retention_in_days = 30
   tags              = local.tags
   kms_key_id        = aws_kms_key.ecs_service_log_group_kms_key.arn
@@ -204,7 +204,7 @@ data "aws_ssm_parameter" "log-destination-arn" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "ecs_service_logs_filter" {
-  name            = "/platform/ecs/service/${var.environment}/${var.service_config.name}"
+  name            = "/platform/ecs/service/${var.application}/${var.environment}/${var.service_config.name}"
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.ecs_service_logs.name
   filter_pattern  = ""
