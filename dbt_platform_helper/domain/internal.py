285ab2dc8a70477b7460079c425eb8f1c47f84a3
Sam Brown <sambrown@DBT001166s-MacBook-Pro.local>
2025-08-28 17:06:55 +0100

    load service config into deploy command

--- dbt_platform_helper/domain/internal.py
+++ dbt_platform_helper/domain/internal.py
@@ -1,3 +1,5 @@
+from dbt_platform_helper.entities.service import ServiceConfig
+from dbt_platform_helper.providers.config import ConfigLoader
 from dbt_platform_helper.providers.config import ConfigProvider
 from dbt_platform_helper.providers.config_validator import ConfigValidator
 from dbt_platform_helper.providers.ecs import ECS
@@ -11,18 +13,25 @@ class Internal:
         ecs_provider=ECS,
         load_application=load_application,
         config_provider=ConfigProvider(ConfigValidator()),
+        loader: ConfigLoader = ConfigLoader(),
     ):
         self.ecs_provider = ecs_provider
         self.load_application = load_application
         self.config_provider = config_provider
+        self.loader = loader
 
     def deploy(self, service: str, environment: str, application: str, image_tag: str = None):
 
         task_def = self.ecs_provider.get_task_definition_arn(
             application=application, environment=environment, service=service
         )
+        service_config = self.loader.load_into_model(
+            f"terraform/services/{environment}/{service}/service-config.yml",
+            ServiceConfig,
+        )
+        print(f"service config name: {service_config.name}")
         if task_def:
-            # Update the task definition
+            # Register new revision
             print(f"found task def {task_def}")
         else:
             # Create new task definition
