70f530382604d8403132e7227775b456807a710d
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2025-10-07 22:19:02 +0100

    feat: Dynamically determine which iam role to use when generating aws config profiles (off-ticket) (#1057)

--- dbt_platform_helper/constants.py
+++ dbt_platform_helper/constants.py
@@ -45,8 +45,6 @@ SERVICE_NAME_SUFFIX = f"Service-{COPILOT_IDENTIFIER}"
 REFRESH_TOKEN_MESSAGE = (
     "To refresh this SSO session run `aws sso login` with the corresponding profile"
 )
-
-
 COPILOT_RULE_PRIORITY = 48000
 PLATFORM_RULE_STARTING_PRIORITY = 10000
 RULE_PRIORITY_INCREMENT = 100
@@ -55,3 +53,8 @@ MAINTENANCE_PAGE_TAGS = ["MaintenancePage", "AllowedIps", "BypassIpFilter", "All
 MAINTENANCE_PAGE_REASON = "MaintenancePage"
 MANAGED_BY_PLATFORM = "DBT Platform"
 MANAGED_BY_SERVICE_TERRAFORM = "DBT Platform - Service Terraform"
+STANDARD_PLATFORM_SSO_ROLES = [
+    "AdministratorAccess",
+    "DBTPlatformDeveloperWrite",
+    "DBTPlatformDeveloperRead",
+]
