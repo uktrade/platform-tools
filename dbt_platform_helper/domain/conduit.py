b83f771ad5f025cddc85e1f5a2abb2874dd27593
Sam Brown <sambrown@DBT001166s-MacBook-Pro.local>
2025-10-22 17:11:32 +0100

    update conduit to default to copilot if no platform cluster exists

--- dbt_platform_helper/domain/conduit.py
+++ dbt_platform_helper/domain/conduit.py
@@ -56,10 +56,11 @@ class TerraformConduitStrategy(ConduitECSStrategy):
 
     def get_data(self):
         self.io.info("Starting conduit in Terraform mode.")
+        cluster_arn = self.ecs_provider.get_cluster_arn_by_name(
+            f"{self.application.name}-{self.env}-cluster"
+        ) or self.ecs_provider.get_cluster_arn_by_name(f"{self.application.name}-{self.env}")
         return {
-            "cluster_arn": self.ecs_provider.get_cluster_arn_by_name(
-                f"{self.application.name}-{self.env}-cluster"
-            ),
+            "cluster_arn": cluster_arn,
             "task_def_family": self._generate_container_name(),
             "vpc_name": self._resolve_vpc_name(),
             "addon_type": self.addon_type,
@@ -94,8 +95,16 @@ class TerraformConduitStrategy(ConduitECSStrategy):
                 },
             ]
 
+        cluster_name = (
+            f"{self.application.name}-{self.env}-cluster"
+            if self.ecs_provider.get_cluster_arn_by_name(
+                f"{self.application.name}-{self.env}-cluster"
+            )
+            else f"{self.application.name}-{self.env}"
+        )
+
         self.ecs_provider.start_ecs_task(
-            f"{self.application.name}-{self.env}-cluster",
+            cluster_name,
             self._generate_container_name(),
             data_context["task_def_family"],
             vpc_config,
