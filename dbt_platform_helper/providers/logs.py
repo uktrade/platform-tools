f90a27f30b9fc402c3e83a7f8dd000bd96682a50
John Stainsby <john.stainsby@digital.trade.gov.uk>
2025-11-04 22:38:28 +0000

    Retrieve specific cloudwatch log stream events

--- dbt_platform_helper/providers/logs.py
+++ dbt_platform_helper/providers/logs.py
@@ -1,4 +1,5 @@
 import time
+from typing import Any
 
 import boto3
 from botocore.exceptions import ClientError
@@ -55,3 +56,17 @@ class LogsProvider:
         raise PlatformException(
             f"Timed out waiting for the following log streams to create: {missing_log_streams}"
         )
+
+    def get_log_stream_events(
+        self, log_group: str, log_stream: str, limit: int
+    ) -> list[dict[str, Any]]:
+        """Return events for a specific log stream."""
+
+        try:
+            self.check_log_streams_present(log_group=log_group, expected_log_streams=[log_stream])
+            response = self.client.get_log_events(
+                logGroupName=log_group, logStreamName=log_stream, limit=limit
+            )
+            return response["events"]
+        except ClientError as err:
+            raise PlatformException(f"Error retrieving log stream events: {err}")
