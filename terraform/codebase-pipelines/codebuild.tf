b3fda66aad461237d00cb959b62f7b93a90a8427
Adrian Clay <adieclay@gmail.com>
2025-11-25 11:17:09 +0000

    fix: Only create subscription_filter if log_group exists

--- terraform/codebase-pipelines/codebuild.tf
+++ terraform/codebase-pipelines/codebuild.tf
@@ -96,6 +96,7 @@ resource "aws_cloudwatch_log_stream" "codebase_image_build" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "codebase_image_build" {
+  for_each        = toset(var.requires_image_build ? [""] : [])
   name            = aws_cloudwatch_log_group.codebase_image_build[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.codebase_image_build[""].name
@@ -199,6 +200,7 @@ resource "aws_cloudwatch_log_stream" "codebase_install_tools" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "codebase_install_tools" {
+  for_each        = toset(local.platform_deployment_enabled ? [""] : [])
   name            = aws_cloudwatch_log_group.codebase_install_tools[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.codebase_install_tools[""].name
@@ -266,6 +268,7 @@ resource "aws_cloudwatch_log_stream" "codebase_service_terraform" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "codebase_service_terraform" {
+  for_each        = toset(local.platform_deployment_enabled ? [""] : [])
   name            = aws_cloudwatch_log_group.codebase_service_terraform[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.codebase_service_terraform[""].name
@@ -333,6 +336,7 @@ resource "aws_cloudwatch_log_stream" "codebase_traffic_switch" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "codebase_traffic_switch" {
+  for_each        = toset(local.traffic_switch_enabled ? [""] : [])
   name            = aws_cloudwatch_log_group.codebase_traffic_switch[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.codebase_traffic_switch[""].name
@@ -399,6 +403,7 @@ resource "aws_cloudwatch_log_stream" "invalidate_cache" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "invalidate_cache" {
+  for_each        = toset(local.cache_invalidation_enabled ? [""] : [])
   name            = aws_cloudwatch_log_group.invalidate_cache[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.invalidate_cache[""].name
@@ -475,6 +480,7 @@ resource "aws_cloudwatch_log_stream" "codebase_deploy" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "codebase_deploy" {
+  for_each        = toset(local.copilot_deployment_enabled ? [""] : [])
   name            = aws_cloudwatch_log_group.codebase_deploy[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.codebase_deploy[""].name
@@ -542,6 +548,7 @@ resource "aws_cloudwatch_log_stream" "codebase_deploy_platform" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "codebase_deploy_platform" {
+  for_each        = toset(local.platform_deployment_enabled ? [""] : [])
   name            = aws_cloudwatch_log_group.codebase_deploy_platform[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.codebase_deploy_platform[""].name
@@ -609,6 +616,7 @@ resource "aws_cloudwatch_log_stream" "codebase_service_terraform_plan" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "codebase_service_terraform_plan" {
+  for_each        = toset(local.platform_deployment_enabled ? [""] : [])
   name            = aws_cloudwatch_log_group.codebase_service_terraform_plan[""].name
   role_arn        = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/CWLtoSubscriptionFilterRole"
   log_group_name  = aws_cloudwatch_log_group.codebase_service_terraform_plan[""].name
