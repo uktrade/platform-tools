2b5c5020e96f570a0f29821d7ebb8183637ed069
Kate Sugden <kate.sugden@madetech.com>
2025-12-01 13:24:12 +0000

    correct the null ternary expression for pinned version

--- terraform/environment-pipelines/codepipeline.tf
+++ terraform/environment-pipelines/codepipeline.tf
@@ -95,7 +95,7 @@ resource "aws_codepipeline" "environment_pipeline" {
           { name : "REPOSITORY", value : var.repository },
           { name : "SLACK_CHANNEL_ID", value : var.slack_channel, type : "PARAMETER_STORE" },
           { name : "SLACK_THREAD_ID", value : "#{variables.SLACK_THREAD_ID}" },
-          { name : "PLATFORM_HELPER_VERSION_OVERRIDE", value : var.pinned_version ? null : "#{variables.PLATFORM_HELPER_VERSION_OVERRIDE}" },
+          { name : "PLATFORM_HELPER_VERSION_OVERRIDE", value : var.pinned_version != null ? null : "#{variables.PLATFORM_HELPER_VERSION_OVERRIDE}" },
           { name : "CODESTAR_CONNECTION_ARN", value : data.external.codestar_connections.result["ConnectionArn"] },
           { name : "PLATFORM_ORCHESTRATION_PINNED_VERSION", value : var.pinned_version },
         ])
--- terraform/environment-pipelines/locals.tf
+++ terraform/environment-pipelines/locals.tf
@@ -61,7 +61,7 @@ locals {
           { name : "SLACK_REF", value : "#{slack.SLACK_REF}" },
           { name : "NEEDS_APPROVAL", value : lookup(env, "requires_approval", false) ? "yes" : "no" },
           { name : "SLACK_THREAD_ID", value : "#{variables.SLACK_THREAD_ID}" },
-          { name : "PLATFORM_HELPER_VERSION_OVERRIDE", value : var.pinned_version ? null : "#{variables.PLATFORM_HELPER_VERSION_OVERRIDE}" },
+          { name : "PLATFORM_HELPER_VERSION_OVERRIDE", value : var.pinned_version != null ? null : "#{variables.PLATFORM_HELPER_VERSION_OVERRIDE}" },
           { name : "PLATFORM_ORCHESTRATION_PINNED_VERSION", value : var.pinned_version },
         ])
       }
