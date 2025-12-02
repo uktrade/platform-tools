b1a8b2e11bd831feff6a1ef098c6d78f987fdaa2
Kate Sugden <107400614+ksugden@users.noreply.github.com>
2025-12-02 12:35:18 +0000

    Update terraform/s3/locals.tf

--- terraform/s3/locals.tf
+++ terraform/s3/locals.tf
@@ -2,7 +2,7 @@ locals {
   tags = {
     application         = var.application
     environment         = var.environment
-    managed-by          = "Meeee!"
+    managed-by          = "DBT Platform - Terraform"
     copilot-application = var.application
     copilot-environment = var.environment
   }
