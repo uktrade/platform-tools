fd66281b4263f9b5cee9673bbe92b75bfdf81165
Sam Brown <106599828+SamBrownMT@users.noreply.github.com>
2025-12-10 02:17:07 -0800

    feat: Add back update ALB rules to pipeline (DBTP-2535) (#1165)
    Co-authored-by: Sam Brown <sambrown@DBT001166s-MacBook-Pro.local>
    Co-authored-by: John Stainsby <john.stainsby@digital.trade.gov.uk>

--- terraform/codebase-pipelines/locals.tf
+++ terraform/codebase-pipelines/locals.tf
@@ -126,12 +126,13 @@ locals {
                 }
               }] : [],
             )]),
-            strcontains(local.base_env_config[env.name].service_deployment_mode, "dual") ? [{
-              name : "traffic-switch",
+            local.base_env_config[env.name].service_deployment_mode != "copilot" ?
+            [{
+              name : "update-alb-rules",
               order : max([for svc in local.service_order_list : svc.order]...) + 2,
               input_artifacts : ["tools_output"],
               configuration = {
-                ProjectName   = aws_codebuild_project.codebase_traffic_switch[""].name
+                ProjectName   = aws_codebuild_project.codebase_update_alb_rules.name
                 PrimarySource = "tools_output"
                 EnvironmentVariables : jsonencode([
                   { name : "APPLICATION", value : var.application },
@@ -199,12 +200,12 @@ locals {
         }
       }] : [],
     )]),
-    local.traffic_switch_enabled ? [{
-      name : "traffic-switch",
+    [{
+      name : "update-alb-rules",
       order : max([for svc in local.service_order_list : svc.order]...) + 2,
       input_artifacts : ["tools_output"],
       configuration = {
-        ProjectName   = aws_codebuild_project.codebase_traffic_switch[""].name
+        ProjectName   = aws_codebuild_project.codebase_update_alb_rules.name
         PrimarySource = "tools_output"
         EnvironmentVariables : jsonencode([
           { name : "APPLICATION", value : var.application },
@@ -213,7 +214,7 @@ locals {
           { name : "AWS_ACCOUNT_ID", value : data.aws_caller_identity.current.account_id },
         ])
       }
-    }] : [],
+    }],
     local.cache_invalidation_enabled ? [{
       name : "invalidate-cache",
       order : max([for svc in local.service_order_list : svc.order]...) + 2,
@@ -257,6 +258,4 @@ locals {
   # Set to true if any environment contains a service-deployment-mode whose value is not 'platform'
   copilot_deployment_enabled = anytrue([for env in local.base_env_config : true if env.service_deployment_mode != "platform"])
 
-  # Set to true if any environment contains a dual service-deployment-mode
-  traffic_switch_enabled = anytrue([for env in local.base_env_config : true if strcontains(env.service_deployment_mode, "dual")])
 }
