66a15f4a9624196f32888de4b11d4d8b4d14f75c
Kate Sugden <107400614+ksugden@users.noreply.github.com>
2026-02-03 13:33:07 +0000

    Apply suggestion from @ksugden

--- terraform/codebase-pipelines/custom-codebuild.tf
+++ terraform/codebase-pipelines/custom-codebuild.tf
@@ -21,10 +21,6 @@ resource "aws_codebuild_project" "custom_pre_build" {
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
 
-    # environment_variable {
-    #   name  = "ENV_CONFIG"
-    #   value = jsonencode(local.base_env_config)
-    # }
   }
 
   logs_config {
