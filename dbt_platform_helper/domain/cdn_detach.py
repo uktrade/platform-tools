2feb3f08bf73388fa6ea77d071161ad0732ec7ca
Kier Davis <kier.davis@businessandtrade.gov.uk>
2026-02-05 17:30:21 +0000

    feat: make domain/cdn_detach invoke providers/terraform_state
    Signed-off-by: DBT pre-commit check

--- dbt_platform_helper/domain/cdn_detach.py
+++ dbt_platform_helper/domain/cdn_detach.py
@@ -1,12 +1,18 @@
 from dbt_platform_helper.domain.terraform_environment import TerraformEnvironment
+from dbt_platform_helper.providers.terraform_state import TerraformStateProvider
 
 
 class CDNDetach:
     def __init__(
         self,
         terraform_environment: TerraformEnvironment,
+        terraform_state_provider: TerraformStateProvider = None,
     ):
         self.terraform_environment = terraform_environment
+        self.terraform_state_provider = terraform_state_provider or TerraformStateProvider()
 
     def execute(self, environment_name):
+        # Writes to terraform/environments/{environment_name}
         self.terraform_environment.generate(environment_name)
+
+        self.terraform_state_provider.pull(f"terraform/environments/{environment_name}")
--- tests/platform_helper/domain/test_cdn_detach.py
+++ tests/platform_helper/domain/test_cdn_detach.py
@@ -2,15 +2,18 @@ from unittest.mock import Mock
 
 from dbt_platform_helper.domain.cdn_detach import CDNDetach
 from dbt_platform_helper.domain.terraform_environment import TerraformEnvironment
+from dbt_platform_helper.providers.terraform_state import TerraformStateProvider
 
 
 class CDNDetachMocks:
     def __init__(self):
         self.mock_terraform_environment = Mock(spec=TerraformEnvironment)
+        self.mock_terraform_state_provider = Mock(spec=TerraformStateProvider)
 
     def params(self):
         return {
             "terraform_environment": self.mock_terraform_environment,
+            "terraform_state_provider": self.mock_terraform_state_provider,
         }
 
 
@@ -24,3 +27,6 @@ class TestCDNDetach:
         cdn_detach.execute(environment_name)
 
         mocks.mock_terraform_environment.generate.assert_called_once_with(environment_name)
+        mocks.mock_terraform_state_provider.pull.assert_called_once_with(
+            f"terraform/environments/{environment_name}"
+        )
