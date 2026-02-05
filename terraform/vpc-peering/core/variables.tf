bb20664efa04ee24bbf01c93e4ceefbfbc313854
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2026-02-05 12:52:41 +0000

    add variable description

--- terraform/vpc-peering/core/variables.tf
+++ terraform/vpc-peering/core/variables.tf
@@ -10,18 +10,19 @@ variable "vpc_peering_connection_id" {
   type = string
 }
 
-# Only for applications deployed using AWS Copilot. Otherwise, use var.ecs_security_groups below.
+# Note: Only for applications deployed using AWS Copilot. Otherwise, use var.ecs_security_groups below.
 variable "security_group_map" {
   type = map(string)
 }
 
-# Only for applications that are not deployed using AWS Copilot.
+# Note: Only for applications that are not deployed using AWS Copilot.
 variable "ecs_security_groups" {
   type = map(object({
     port        = string
     application = string
     environment = string
   }))
+  description = "ECS security group(s) that will allow ingress VPC peering traffic into the destination (remote) AWS account."
 }
 
 variable "vpc_name" {
