55f8f6c4c3e74c18090009ec05c794b6b1166a39
Kier Davis <kier.davis@businessandtrade.gov.uk>
2026-02-05 17:10:55 +0000

    feat: Make providers/terraform_state invoke 'terraform state pull'
    Signed-off-by: DBT pre-commit check

--- dbt_platform_helper/providers/terraform_state.py
+++ dbt_platform_helper/providers/terraform_state.py
@@ -1,2 +1,12 @@
-def pull_terraform_state(terraform_config_dir):
-    pass
+import json
+import subprocess
+
+
+def pull_terraform_state(config_dir):
+    result = subprocess.run(
+        ["terraform", "state", "pull"],
+        cwd=config_dir,
+        stdout=subprocess.PIPE,
+        check=True,
+    )
+    return json.loads(result.stdout)
--- tests/platform_helper/providers/test_terraform_state.py
+++ tests/platform_helper/providers/test_terraform_state.py
@@ -1,6 +1,32 @@
+import json
+import subprocess
+from unittest.mock import patch
+
 from dbt_platform_helper.providers.terraform_state import pull_terraform_state
+
+MOCK_STATE = {
+    "version": 4,
+    "terraform_version": "1.12.2",
+    "serial": 76,
+}
 
 
 class TestPull:
-    def test_exists(self, tmp_path):
-        pull_terraform_state(tmp_path)
+    @patch("dbt_platform_helper.providers.terraform_state.subprocess.run", spec=True)
+    def test_success(self, mock_subprocess_run, tmp_path):
+        mock_subprocess_run.return_value = subprocess.CompletedProcess(
+            args=["terraform", "state", "pull"],
+            returncode=0,
+            stdout=json.dumps(MOCK_STATE),
+            stderr=None,
+        )
+
+        result = pull_terraform_state(tmp_path)
+
+        assert result == MOCK_STATE
+        mock_subprocess_run.assert_called_once_with(
+            ["terraform", "state", "pull"],
+            cwd=tmp_path,
+            stdout=subprocess.PIPE,
+            check=True,
+        )
