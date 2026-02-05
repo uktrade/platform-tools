a1c907d7e1c99d402f406d0ccaa396bf9d712691
Kier Davis <kier.davis@businessandtrade.gov.uk>
2026-02-05 16:44:33 +0000

    feat: Make CDNDetach.execute invoke TerraformEnvironment.generate
    Signed-off-by: DBT pre-commit check

--- dbt_platform_helper/domain/cdn_detach.py
+++ dbt_platform_helper/domain/cdn_detach.py
@@ -1,3 +1,12 @@
+from dbt_platform_helper.domain.terraform_environment import TerraformEnvironment
+
+
 class CDNDetach:
+    def __init__(
+        self,
+        terraform_environment: TerraformEnvironment,
+    ):
+        self.terraform_environment = terraform_environment
+
     def execute(self, environment_name):
-        pass
+        self.terraform_environment.generate(environment_name)
--- tests/platform_helper/domain/test_cdn_detach.py
+++ tests/platform_helper/domain/test_cdn_detach.py
@@ -1,9 +1,26 @@
+from unittest.mock import Mock
+
 from dbt_platform_helper.domain.cdn_detach import CDNDetach
+from dbt_platform_helper.domain.terraform_environment import TerraformEnvironment
+
+
+class CDNDetachMocks:
+    def __init__(self):
+        self.mock_terraform_environment = Mock(spec=TerraformEnvironment)
+
+    def params(self):
+        return {
+            "terraform_environment": self.mock_terraform_environment,
+        }
 
 
 class TestCDNDetach:
-    def test_exists(self):
+    def test_success(self):
         environment_name = "test"
-        cdn_detach = CDNDetach()
 
+        mocks = CDNDetachMocks()
+
+        cdn_detach = CDNDetach(**mocks.params())
         cdn_detach.execute(environment_name)
+
+        mocks.mock_terraform_environment.generate.assert_called_once_with(environment_name)
