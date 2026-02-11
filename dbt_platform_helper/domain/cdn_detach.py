3aba181b160e0710667f21219cac78b4e697c21d
Kier Ada Davis <kier.davis@businessandtrade.gov.uk>
2026-02-11 10:44:08 +0000

    refactor: CDN detach command: pull out pure logic into a separate class (DBTP-2651) (#1269)
    No behavioural changes.
    
    This PR divides the existing CDNDetach domain class into two:
    
    * CDNDetachLogic, which is the brains of the operation - it performs side-effect-free data manipulation.
    * CDNDetach, which is tasked with unintelligently fetching data from providers, feeding it through CDNDetachLogic and feeding result back into providers (e.g. writing to the console).
    
    I'm doing this because I want to have tests for several different combinations of data inputs and expected outputs, which is only relevant to the functionality of CDNDetachLogic. CDNDetach can have relatively few tests (basically just checking that data is plumbed to the right places and errors are handled). This refactor allows us to write lots of tests for different data scenarios without each of these tests needing to worry about mocking out providers or extracting the outputs from console logs.
    
    Signed-off-by: DBT pre-commit check

--- dbt_platform_helper/domain/cdn_detach.py
+++ dbt_platform_helper/domain/cdn_detach.py
@@ -1,11 +1,66 @@
 import json
 import re
+from dataclasses import dataclass
+from functools import cached_property
 
 from dbt_platform_helper.domain.terraform_environment import TerraformEnvironment
 from dbt_platform_helper.platform_exception import PlatformException
 from dbt_platform_helper.providers.config import ConfigProvider
 from dbt_platform_helper.providers.io import ClickIOProvider
 from dbt_platform_helper.providers.terraform import TerraformProvider
+
+
+@dataclass(frozen=True)
+class CDNDetachLogic:
+    platform_config: dict
+    environment_name: str
+    environment_tfstate: dict
+
+    @cached_property
+    def environment_tfstate_resources(self):
+        return [
+            {**resource_block, **instance}
+            for resource_block in self.environment_tfstate["resources"]
+            for instance in resource_block["instances"]
+        ]
+
+    @staticmethod
+    def is_resource_detachable(res):
+        return (
+            res["mode"] == "managed"
+            and res["provider"].endswith((".domain", ".domain-cdn"))
+            and "module.extensions.module.alb" not in res["module"]
+        )
+
+    @staticmethod
+    def extension_name_for_resource(res):
+        return re.match(r'^module\.extensions\.module\.\w+\["([^"]+)"\]', res["module"]).group(1)
+
+    @cached_property
+    def extensions_to_detach(self):
+        """The set of extension names for which managed_ingress is set to
+        true."""
+        result = set()
+        for ext_name, ext_config in self.platform_config.get("extensions", {}).items():
+            flattened_ext_config = {
+                **ext_config,
+                **(ext_config.get("environments", {}).get("*") or {}),
+                **(ext_config.get("environments", {}).get(self.environment_name) or {}),
+            }
+            if flattened_ext_config.get("managed_ingress", False):
+                result.add(ext_name)
+        return result
+
+    @cached_property
+    def resources_to_detach(self):
+        """The list of tfstate resources that are intended to be detached in
+        this session."""
+        return [
+            res
+            for res in self.environment_tfstate_resources
+            if self.is_resource_detachable(res)
+            and self.extension_name_for_resource(res) in self.extensions_to_detach
+        ]
 
 
 class CDNDetach:
@@ -15,67 +70,41 @@ class CDNDetach:
         config_provider: ConfigProvider,
         terraform_environment: TerraformEnvironment,
         terraform_provider: TerraformProvider = None,
+        logic_constructor=CDNDetachLogic,
     ):
         self.io = io
         self.config_provider = config_provider
         self.terraform_environment = terraform_environment
         self.terraform_provider = terraform_provider or TerraformProvider()
+        self.logic_constructor = logic_constructor
 
     def execute(self, environment_name, dry_run=True):
-        config = self.config_provider.get_enriched_config()
-        if environment_name not in config.get("environments", {}):
+        platform_config = self.config_provider.get_enriched_config()
+        if environment_name not in platform_config.get("environments", {}):
             raise PlatformException(
                 f"cannot detach CDN resources for environment {environment_name}. It does not exist in your configuration"
             )
 
-        # Populates ./terraform/environments/{environment_name}
+        environment_tfstate = self.fetch_environment_tfstate(environment_name)
+
+        logic_result = self.logic_constructor(
+            platform_config=platform_config,
+            environment_name=environment_name,
+            environment_tfstate=environment_tfstate,
+        )
+
+        self.log_resources_to_detach(logic_result.resources_to_detach, environment_name)
+
+        if not dry_run:
+            raise NotImplementedError("--no-dry-run mode is not yet implemented")
+
+    def fetch_environment_tfstate(self, environment_name):
         self.terraform_environment.generate(environment_name)
         terraform_config_dir = f"terraform/environments/{environment_name}"
 
         self.io.info(f"Fetching a copy of the {environment_name} environment's terraform state...")
         self.terraform_provider.init(terraform_config_dir)
-        state = self.terraform_provider.pull_state(terraform_config_dir)
-
-        resources = self.get_resources_to_detach(state, environment_name)
-        self.log_resources_to_detach(resources, environment_name)
-
-        if not dry_run:
-            raise NotImplementedError("--no-dry-run mode is not yet implemented")
-
-    def get_resources_to_detach(self, terraform_state, environment_name):
-        managed_ingress_extensions = self.get_extensions_with_managed_ingress(environment_name)
-        return [
-            r
-            for r in terraform_state["resources"]
-            if self.is_resource_detachable(r)
-            and self.extension_name_for_resource(r) in managed_ingress_extensions
-        ]
-
-    def get_extensions_with_managed_ingress(self, environment_name):
-        config = self.config_provider.get_enriched_config()
-        result = set()
-        for ext_name, ext_config in config.get("extensions", {}).items():
-            flattened_ext_config = {
-                **ext_config,
-                **(ext_config.get("environments", {}).get("*") or {}),
-                **(ext_config.get("environments", {}).get(environment_name) or {}),
-            }
-            if flattened_ext_config.get("managed_ingress", False):
-                result.add(ext_name)
-        return result
-
-    @staticmethod
-    def is_resource_detachable(resource):
-        return (
-            resource["mode"] == "managed"
-            and resource["provider"].endswith((".domain", ".domain-cdn"))
-            and "module.extensions.module.alb" not in resource["module"]
-        )
-
-    @staticmethod
-    def extension_name_for_resource(resource):
-        m = re.match(r'^module\.extensions\.module\.\w+\["([^"]+)"\]', resource["module"])
-        return m.group(1)
+        return self.terraform_provider.pull_state(terraform_config_dir)
 
     def log_resources_to_detach(self, resources, environment_name):
         self.io.info("")
@@ -83,20 +112,20 @@ class CDNDetach:
             self.io.info(
                 f"Will remove the following resources from the {environment_name} environment's terraform state:"
             )
-            for address in sorted(self.iter_addresses_for_resources(resources)):
+            addresses = sorted(address_for_tfstate_resource(res) for res in resources)
+            for address in addresses:
                 self.io.info(f"  {address}")
         else:
             self.io.info(
                 f"Will not remove any resources from the {environment_name} environment's terraform state."
             )
 
-    def iter_addresses_for_resources(self, resources):
-        for resource in resources:
-            base = ".".join((resource["module"], resource["type"], resource["name"]))
-            instances = resource["instances"]
-            if len(instances) == 1 and "index_key" not in instances[0]:
-                yield base
-            else:
-                for instance in instances:
-                    # XXX: does json.dumps escape special characters in strings the same way that terraform does?
-                    yield base + "[" + json.dumps(instance["index_key"]) + "]"
+
+def address_for_tfstate_resource(res):
+    s = res["module"] + "." + res["type"] + "." + res["name"]
+    try:
+        # XXX: does json.dumps escape special characters in strings the same way that terraform does?
+        s += "[" + json.dumps(res["index_key"]) + "]"
+    except KeyError:
+        pass
+    return s
--- tests/platform_helper/domain/test_cdn_detach.py
+++ tests/platform_helper/domain/test_cdn_detach.py
@@ -1,12 +1,13 @@
 import json
 from unittest.mock import Mock
 from unittest.mock import call
-from unittest.mock import patch
 
 import pytest
 import yaml
 
 from dbt_platform_helper.domain.cdn_detach import CDNDetach
+from dbt_platform_helper.domain.cdn_detach import CDNDetachLogic
+from dbt_platform_helper.domain.cdn_detach import address_for_tfstate_resource
 from dbt_platform_helper.domain.terraform_environment import TerraformEnvironment
 from dbt_platform_helper.platform_exception import PlatformException
 from dbt_platform_helper.providers.config import ConfigProvider
@@ -58,11 +59,23 @@ MOCK_RESOURCES_TO_DETACH = [
         "type": "aws_cloudfront_distribution",
         "name": "standard",
         "provider": 'module.extensions.provider["registry.terraform.io/hashicorp/aws"].domain-cdn',
-        "instances": [
-            {"index_key": "api.dev.demodjango.uktrade.digital"},
-            {"index_key": "ip-filter-test.dev.demodjango.uktrade.digital"},
-            {"index_key": "web.dev.demodjango.uktrade.digital"},
-        ],
+        "index_key": "api.dev.demodjango.uktrade.digital",
+    },
+    {
+        "module": 'module.extensions.module.cdn["demodjango-alb"]',
+        "mode": "managed",
+        "type": "aws_cloudfront_distribution",
+        "name": "standard",
+        "provider": 'module.extensions.provider["registry.terraform.io/hashicorp/aws"].domain-cdn',
+        "index_key": "ip-filter-test.dev.demodjango.uktrade.digital",
+    },
+    {
+        "module": 'module.extensions.module.cdn["demodjango-alb"]',
+        "mode": "managed",
+        "type": "aws_cloudfront_distribution",
+        "name": "standard",
+        "provider": 'module.extensions.provider["registry.terraform.io/hashicorp/aws"].domain-cdn',
+        "index_key": "web.dev.demodjango.uktrade.digital",
     },
     {
         "module": 'module.extensions.module.cdn["demodjango-alb"]',
@@ -70,18 +83,18 @@ MOCK_RESOURCES_TO_DETACH = [
         "type": "aws_cloudfront_cache_policy",
         "name": "cache_policy",
         "provider": 'module.extensions.provider["registry.terraform.io/hashicorp/aws"].domain-cdn',
-        "instances": [{}],
     },
 ]
 
 
 class CDNDetachMocks:
-    def __init__(self, platform_config=create_mock_platform_config()):
+    def __init__(self, **mock_logic_result_attrs):
         self.mock_io = Mock(spec=ClickIOProvider)
         self.mock_config_provider = Mock(spec=ConfigProvider)
-        self.mock_config_provider.get_enriched_config.return_value = platform_config
+        self.mock_config_provider.get_enriched_config.return_value = create_mock_platform_config()
         self.mock_terraform_environment = Mock(spec=TerraformEnvironment)
         self.mock_terraform_provider = Mock(spec=TerraformProvider)
+        self.mock_logic_constructor = Mock(return_value=Mock(**mock_logic_result_attrs))
 
     def params(self):
         return {
@@ -89,18 +102,17 @@ class CDNDetachMocks:
             "config_provider": self.mock_config_provider,
             "terraform_environment": self.mock_terraform_environment,
             "terraform_provider": self.mock_terraform_provider,
+            "logic_constructor": self.mock_logic_constructor,
         }
 
 
 class TestCDNDetach:
-    @patch(
-        "dbt_platform_helper.domain.cdn_detach.CDNDetach.get_resources_to_detach",
-        return_value=MOCK_RESOURCES_TO_DETACH,
-    )
-    def test_dry_run_success(self, mock_get_resources_to_detach):
-        mocks = CDNDetachMocks()
-        cdn_detach = CDNDetach(**mocks.params())
+    def test_dry_run_success(self):
+        mocks = CDNDetachMocks(
+            resources_to_detach=MOCK_RESOURCES_TO_DETACH,
+        )
 
+        cdn_detach = CDNDetach(**mocks.params())
         cdn_detach.execute(environment_name="staging", dry_run=True)
 
         mocks.mock_terraform_environment.generate.assert_called_once_with("staging")
@@ -108,7 +120,7 @@ class TestCDNDetach:
         mocks.mock_terraform_provider.pull_state.assert_called_once_with(
             "terraform/environments/staging"
         )
-        mock_get_resources_to_detach.assert_called_once()
+        mocks.mock_logic_constructor.assert_called_once()
 
         mocks.mock_io.info.assert_has_calls(
             [
@@ -130,22 +142,13 @@ class TestCDNDetach:
             ]
         )
 
-    @patch(
-        "dbt_platform_helper.domain.cdn_detach.CDNDetach.get_resources_to_detach",
-        return_value=[],
-    )
-    def test_dry_run_success_with_no_resources_to_remove(self, mock_get_resources_to_detach):
-        mocks = CDNDetachMocks()
-        cdn_detach = CDNDetach(**mocks.params())
-
-        cdn_detach.execute(environment_name="staging", dry_run=True)
-
-        mocks.mock_terraform_environment.generate.assert_called_once_with("staging")
-        mocks.mock_terraform_provider.init.assert_called_once_with("terraform/environments/staging")
-        mocks.mock_terraform_provider.pull_state.assert_called_once_with(
-            "terraform/environments/staging"
+    def test_dry_run_success_with_no_resources_to_detach(self):
+        mocks = CDNDetachMocks(
+            resources_to_detach=[],
         )
-        mock_get_resources_to_detach.assert_called_once()
+
+        cdn_detach = CDNDetach(**mocks.params())
+        cdn_detach.execute(environment_name="staging", dry_run=True)
 
         mocks.mock_io.info.assert_has_calls(
             [
@@ -155,24 +158,27 @@ class TestCDNDetach:
             ]
         )
 
-    @patch("dbt_platform_helper.domain.cdn_detach.CDNDetach.get_resources_to_detach", spec=True)
-    def test_real_run_not_implemented(self, mock_get_resources_to_detach):
-        mocks = CDNDetachMocks()
-        cdn_detach = CDNDetach(**mocks.params())
+    def test_real_run_not_implemented(self):
+        mocks = CDNDetachMocks(
+            resources_to_detach=MOCK_RESOURCES_TO_DETACH,
+        )
 
+        cdn_detach = CDNDetach(**mocks.params())
         with pytest.raises(NotImplementedError):
             cdn_detach.execute(environment_name="staging", dry_run=False)
 
     def test_exception_raised_if_env_not_in_config(self):
         mocks = CDNDetachMocks()
-        cdn_detach = CDNDetach(**mocks.params())
 
+        cdn_detach = CDNDetach(**mocks.params())
         with pytest.raises(
             PlatformException,
             match="cannot detach CDN resources for environment not-an-environment. It does not exist in your configuration",
         ):
             cdn_detach.execute(environment_name="not-an-environment", dry_run=True)
 
+
+class TestCDNDetachLogic:
     @pytest.mark.parametrize(
         "platform_config,expected_data_filename",
         [
@@ -191,18 +197,59 @@ class TestCDNDetach:
         ],
         ids=["alb", "s3", "alb+s3"],
     )
-    def test_get_resources_to_detach(self, platform_config, expected_data_filename):
+    def test_resource_blocks_to_detach(self, platform_config, expected_data_filename):
         with open(INPUT_DATA_DIR / "cdn_detach/terraform_state/typical.tfstate.json") as f:
-            mock_terraform_state = json.load(f)
+            environment_tfstate = json.load(f)
         with open(
             EXPECTED_DATA_DIR / "cdn_detach/resource_addrs_to_detach" / expected_data_filename
         ) as f:
             expected_resource_addrs = set(yaml.safe_load(f))
 
-        mocks = CDNDetachMocks(platform_config)
-        cdn_detach = CDNDetach(**mocks.params())
+        logic_result = CDNDetachLogic(
+            platform_config=platform_config,
+            environment_name="staging",
+            environment_tfstate=environment_tfstate,
+        )
 
-        resources = cdn_detach.get_resources_to_detach(mock_terraform_state, "staging")
-        resource_addrs = {r["module"] + "." + r["type"] + "." + r["name"] for r in resources}
-
+        resource_addrs = {address_for_tfstate_resource(r) for r in logic_result.resources_to_detach}
         assert resource_addrs == expected_resource_addrs
+
+
+class TestAddressForTerraformResource:
+    def test_no_index_key(self):
+        resource = {
+            "module": 'module.s3["my-ext"]',
+            "mode": "managed",
+            "type": "aws_s3_bucket_versioning",
+            "name": "this-versioning",
+        }
+
+        address = address_for_tfstate_resource(resource)
+
+        assert address == 'module.s3["my-ext"].aws_s3_bucket_versioning.this-versioning'
+
+    def test_string_index_key(self):
+        resource = {
+            "module": 'module.s3["my-ext"]',
+            "mode": "managed",
+            "type": "aws_s3_object",
+            "name": "object",
+            "index_key": "test.html",
+        }
+
+        address = address_for_tfstate_resource(resource)
+
+        assert address == 'module.s3["my-ext"].aws_s3_object.object["test.html"]'
+
+    def test_integer_index_key(self):
+        resource = {
+            "module": 'module.s3["my-ext"]',
+            "mode": "managed",
+            "type": "aws_ssm_parameter",
+            "name": "cloudfront_alias",
+            "index_key": 0,
+        }
+
+        address = address_for_tfstate_resource(resource)
+
+        assert address == 'module.s3["my-ext"].aws_ssm_parameter.cloudfront_alias[0]'
