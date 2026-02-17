9580519a241bfde87d9c538b2a460ada14429686
David Glover <david.glover@digital.trade.gov.uk>
2026-02-17 15:01:18 +0000

    feat: Get deploy repo from platform-config to add to Datadog metadata

--- dbt_platform_helper/providers/terraform_manifest.py
+++ dbt_platform_helper/providers/terraform_manifest.py
@@ -249,7 +249,7 @@ class TerraformManifestProvider:
                 "source": source,
                 "args": "${local.args}",
                 "environment": env,
-                "repos": "${local.codebase_pipeline_repos != null ? (distinct(values(local.codebase_pipeline_repos))) : null}",
+                "repos": "${concat(local.codebase_pipeline_repos != null ? (distinct(values(local.codebase_pipeline_repos))) : null, [local.config.deploy_repository])}",
             }
         }
 
