1b2bc65880879f072b8fcc6aaf21e5940aeb2b18
Kate Sugden <kate.sugden@madetech.com>
2026-03-18 10:23:22 +0000

    add test for failed command

--- tests/platform_helper/test_command_service.py
+++ tests/platform_helper/test_command_service.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from click.testing import CliRunner
 
 from dbt_platform_helper.commands.service import service
+from dbt_platform_helper.platform_exception import PlatformException
 
 
 @patch("dbt_platform_helper.commands.service.ServiceManager")
@@ -61,22 +62,26 @@ def test_service_exec_with_optional_arguments(mock_application, mock_service_man
     )
 
 
-# @patch("dbt_platform_helper.commands.service.ServiceManager")
-# @patch("dbt_platform_helper.commands.service.load_application")
-# @patch("dbt_platform_helper.commands.service.ClickIOProvider")
-# def test_command_migrate_platform_errors_cause_abort_with_error_message(
-#     mock_service_manager_object, mock_load_application, mock_io
-# ):
+@patch("dbt_platform_helper.commands.service.ServiceManager")
+@patch("dbt_platform_helper.commands.service.load_application")
+@patch("dbt_platform_helper.commands.service.ClickIOProvider")
+def test_service_exec_fail(mock_io, mock_application, mock_service_manager_object):
+    """Test that given an app, env and service name strings, the service exec
+    command calls exec with app, env and service name."""
 
-#     mock_service_manager_instance = mock_service_manager_object.return_value
+    mock_service_manager_instance = mock_service_manager_object.return_value
+    mock_service_manager_instance.service_exec.side_effect = PlatformException("Some weird error")
+    mock_io.return_value.abort_with_error.side_effect = SystemExit(1)
 
-#     mock_service_manager_instance.service_exec.side_effect = PlatformException("Some weird error")
-#     mock_io.return_value.abort_with_error.side_effect = SystemExit(1)
+    result = CliRunner().invoke(
+        service,
+        ["exec", "--app", "test-application", "--env", "development", "--name", "test-service"],
+    )
 
-#     result = CliRunner().invoke(
-#         service,
-#         ["exec", "--app", "test-application", "--env", "development", "--name", "test-service"],
-#     )
+    assert result.exit_code == 1
 
-#     assert result.exit_code == 1
-#     # mock_io.return_value.abort_with_error.assert_called_once_with("Some weird error")
+    mock_application.assert_called_once()
+    mock_service_manager_instance.service_exec.assert_called_with(
+        "test-application", "development", "test-service", None, None, None
+    )
+    mock_io.return_value.abort_with_error.assert_called_once_with("Some weird error")
