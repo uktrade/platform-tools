5ee2b552224206730d195851f6829019a6dec7e9
Kier Davis <kier.davis@businessandtrade.gov.uk>
2026-04-23 09:45:47 +0000

    ensure managed_ingress is always true
    Signed-off-by: DBT pre-commit check

--- terraform/extensions/main.tf
+++ terraform/extensions/main.tf
@@ -165,3 +165,24 @@ resource "aws_ssm_parameter" "environment_data" {
 data "aws_ssm_parameter" "log_destination_arn" {
   name = "/copilot/tools/central_log_groups"
 }
+
+resource "null_resource" "check_extension_uses_managed_ingress" {
+  for_each = merge(
+    {
+      for ext_name, ext_cfg in local.alb :
+      ext_name => lookup(ext_cfg, "managed_ingress", false)
+      if can(ext_cfg.cdn_domains_list)
+    },
+    {
+      for ext_name, ext_cfg in local.s3 :
+      ext_name => lookup(ext_cfg, "managed_ingress", false)
+      if lookup(ext_cfg, "serve_static_content", false)
+    },
+  )
+  lifecycle {
+    precondition {
+      condition     = each.value
+      error_message = "Expected managed_ingress to be set to true for extension '${each.key}', environment '${var.environment}'"
+    }
+  }
+}
