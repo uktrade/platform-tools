f23c247aaab17fbc5fbc916cc3a4eb34905328ff
Dragos Stoian <dragos.stoian@digital.trade.gov.uk>
2026-04-27 20:58:46 +0100

    add missing IAM permission

--- terraform/extensions/iam.tf
+++ terraform/extensions/iam.tf
@@ -425,7 +425,8 @@ data "aws_iam_policy_document" "validate_platform_config_for_codebase" {
       "ssm:ListTagsForResource"
     ]
     resources = [
-      "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/platform/applications/${var.args.application}/environments/${var.environment}/services/*"
+      "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/platform/applications/${var.args.application}/environments/${var.environment}/services/*",
+      "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/platform/version/application/${var.args.application}/environment/${var.environment}/service/*",
     ]
   }
 }
