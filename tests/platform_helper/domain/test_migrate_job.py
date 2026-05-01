aa045d6221294932e79c0a102a19ff18312eaaec
Kate Sugden <kate.sugden@madetech.com>
2026-05-01 16:57:58 +0100

    implement disable and enable schedule functions

--- tests/platform_helper/domain/test_migrate_job.py
+++ tests/platform_helper/domain/test_migrate_job.py
@@ -1,21 +1,106 @@
-from unittest.mock import Mock
 
-class ScheduledJob:
-    def __init__(self, sfn_client):
-        self.sfn_client = sfn_client
-        
-    def disable_schedule(name, environment):
-        return "done"
-    
-    def get_schedule(name, environment):
-        return None
+import boto3
+from moto import mock_aws
 
-def test_disable_copilot_schedule():
-    pass
-    mock_client = Mock()
-    
-    ScheduledJob.disable_schedule("my-job", "dev")
-    
-    result = ScheduledJob.get_schedule("my-job", "dev")
-    
+
+class ScheduleManager:
+    def __init__(self, scheduler_client):
+        self.scheduler_client = scheduler_client
+
+    def disable_schedule(self, name, environment):
+        schedule = self.scheduler_client.get_schedule(Name=name, GroupName="default")
+        self.scheduler_client.update_schedule(
+            Name=schedule["Name"],
+            GroupName=schedule["GroupName"],
+            FlexibleTimeWindow=schedule["FlexibleTimeWindow"],
+            Target=schedule["Target"],
+            ScheduleExpression=schedule["ScheduleExpression"],
+            State="DISABLED",
+        )
+
+    def enable_schedule(self, name, environment):
+        schedule = self.scheduler_client.get_schedule(Name=name, GroupName="default")
+        self.scheduler_client.update_schedule(
+            Name=schedule["Name"],
+            GroupName=schedule["GroupName"],
+            FlexibleTimeWindow=schedule["FlexibleTimeWindow"],
+            Target=schedule["Target"],
+            ScheduleExpression=schedule["ScheduleExpression"],
+            State="ENABLED",
+        )
+
+    def get_schedule(self, name, environment):
+        schedule = self.scheduler_client.get_schedule(Name=name, GroupName="default")
+        if schedule.get("State") == "ENABLED":
+            return schedule.get("ScheduleExpression")
+        else:
+            return None
+
+
+@mock_aws
+def test_get_schedule():
+    client = boto3.client("scheduler", region_name="eu-west-2")
+    test_rule = "my-enabled-rule"
+    client.create_schedule(
+        Name=test_rule,
+        GroupName="default",
+        FlexibleTimeWindow={"Mode": "OFF"},
+        Target={
+            "Arn": "arn:aws:states:eu-west-2:123456789012:stateMachine:dummy",
+            "RoleArn": "arn:aws:iam::123456789012:role/dummy",
+        },
+        ScheduleExpression="rate(5 minutes)",
+        State="ENABLED",
+    )
+
+    result = ScheduleManager(client).get_schedule("my-enabled-rule", "dev")
+
+    assert result == "rate(5 minutes)"
+
+
+@mock_aws
+def test_disable_schedule():
+    client = boto3.client("scheduler", region_name="eu-west-2")
+    test_rule = "my-rule"
+    client.create_schedule(
+        Name=test_rule,
+        GroupName="default",
+        FlexibleTimeWindow={"Mode": "OFF"},
+        Target={
+            "Arn": "arn:aws:states:eu-west-2:123456789012:stateMachine:dummy",
+            "RoleArn": "arn:aws:iam::123456789012:role/dummy",
+        },
+        ScheduleExpression="rate(5 minutes)",
+        State="ENABLED",
+    )
+
+    ScheduleManager(client).disable_schedule("my-rule", "dev")
+
+    result = ScheduleManager(client).get_schedule("my-rule", "dev")
+
     assert result is None
+
+
+@mock_aws
+def test_enable_schedule():
+    client = boto3.client("scheduler", region_name="eu-west-2")
+    test_rule = "my-job"
+    client.create_schedule(
+        Name=test_rule,
+        GroupName="default",
+        FlexibleTimeWindow={"Mode": "OFF"},
+        Target={
+            "Arn": "arn:aws:states:eu-west-2:123456789012:stateMachine:dummy",
+            "RoleArn": "arn:aws:iam::123456789012:role/dummy",
+        },
+        ScheduleExpression="rate(5 minutes)",
+        State="DISABLED",
+    )
+
+    assert ScheduleManager(client).get_schedule("my-job", "dev") is None
+
+    ScheduleManager(client).enable_schedule("my-job", "dev")
+
+    result = ScheduleManager(client).get_schedule("my-job", "dev")
+
+    assert result == "rate(5 minutes)"
