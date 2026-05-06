445942418d737e9c9d44ae7ad35c2c45854162ae
Kier Davis <kier.davis@businessandtrade.gov.uk>
2026-05-06 16:33:29 +0000

    empower codebase pipeline to invalidate caches for all environments, not just the ones listed in the `pipelines` block
    This is needed for the manual release pipeline, which is capable of deploying to any environment.

--- terraform/codebase-pipelines/locals.tf
+++ terraform/codebase-pipelines/locals.tf
@@ -33,17 +33,10 @@ locals {
 
   dns_account_ids = distinct([for env in local.base_env_config : env.dns_account])
 
-  cache_invalidation_assumed_roles = (
-    length(var.pipelines) > 0
-    ? setunion([
-      for i, pipeline in var.pipelines :
-      toset([
-        for env in pipeline.environments :
-        "arn:aws:iam::${local.base_env_config[env.name].dns_account}:role/${var.application}-${env.name}-pipeline-deployment-role"
-      ])
-    ]...)
-    : []
-  )
+  cache_invalidation_assumed_roles = toset([
+    for env_name, env_config in local.base_env_config :
+    "arn:aws:iam::${env_config.dns_account}:role/${var.application}-${env_name}-pipeline-deployment-role"
+  ])
 
   environments_requiring_cache_invalidation = distinct([for d in try(values(var.cache_invalidation.domains), []) : d.environment])
 
