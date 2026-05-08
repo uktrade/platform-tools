6d8ce6c95bec35801a63bed04fcc20381e612191
BenFielder <ben.fielderwork@gmail.com>
2026-05-08 15:58:42 +0100

    feat: Use new restricted roles for managing ALB DNS records + invalidating caches (DBTP-2268) (#1391)
    Co-authored-by: Kier Davis <kier.davis@businessandtrade.gov.uk>

--- terraform/codebase-pipelines/locals.tf
+++ terraform/codebase-pipelines/locals.tf
@@ -33,7 +33,10 @@ locals {
 
   dns_account_ids = distinct([for env in local.base_env_config : env.dns_account])
 
-  cache_invalidation_assumed_roles = [for id in local.dns_account_ids : "arn:aws:iam::${id}:role/cloudfront-invalidation-assumed-role"]
+  cache_invalidation_assumed_roles = toset([
+    for env_name, env_config in local.base_env_config :
+    "arn:aws:iam::${env_config.dns_account}:role/${var.application}-${env_name}-pipeline-deployment-role"
+  ])
 
   environments_requiring_cache_invalidation = distinct([for d in try(values(var.cache_invalidation.domains), []) : d.environment])
 
