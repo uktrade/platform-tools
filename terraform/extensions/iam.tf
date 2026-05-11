dc7e155a20494c0b9352f71778591c81f1a4d542
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2026-05-11 16:25:15 +0100

    Terraform format

--- terraform/extensions/iam.tf
+++ terraform/extensions/iam.tf
@@ -50,6 +50,7 @@ data "aws_iam_policy_document" "iam_access_for_codebase" {
       "iam:GetPolicy",
       "iam:GetPolicyVersion",
       "iam:CreatePolicyVersion",
+      "iam:DeletePolicyVersion",
       "iam:ListRolePolicies",
       "iam:GetRolePolicy",
       "iam:ListAttachedRolePolicies",
