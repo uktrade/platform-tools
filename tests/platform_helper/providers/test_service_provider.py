948b6a0759303aaac55764142ea146beac2b3f8c
Kate Sugden <kate.sugden@madetech.com>
2026-05-14 10:33:03 +0100

    List services by type

--- tests/platform_helper/providers/test_service_provider.py
+++ tests/platform_helper/providers/test_service_provider.py
@@ -3,6 +3,7 @@ import json
 import boto3
 from moto import mock_aws
 
+from dbt_platform_helper.entities.service import ServiceType
 from dbt_platform_helper.providers.parameter_store import ParameterStore
 from dbt_platform_helper.providers.service import Service, ServiceRepository
 
@@ -10,30 +11,44 @@ MOCK_SERVICE_PARAMS = [
     {
         "Name": f"/platform/applications/my-app/environments/my-env/services/job-1",
         "Value": json.dumps({"name": "job-1", "type": "Scheduled Job"}),
-        "Type": 'String',
+        "Type": "String",
     },
     {
         "Name": f"/platform/applications/my-app/environments/my-env/services/service-1",
         "Value": json.dumps({"name": "service-1", "type": "Load Balanced Web Service"}),
-        "Type": 'String',
+        "Type": "String",
     },
     {
         "Name": f"/platform/applications/my-app/environments/my-env/services/backend-1",
         "Value": json.dumps({"name": "backend-1", "type": "Backend Service"}),
-        "Type": 'String',
-    }
+        "Type": "String",
+    },
 ]
 
 
 @mock_aws
 def test_list_services():
     client = boto3.client("ssm", region_name="eu-west-2")
-    
+
     for param in MOCK_SERVICE_PARAMS:
         client.put_parameter(**param)
-    
-    
+
     service_repository = ServiceRepository(ParameterStore(client, True))
     services = service_repository.list_services("my-app", "my-env")
     assert len(services) == 3
-    # assert Service("job-1","Load Balanced Web Service") in services
+    assert Service("job-1","Scheduled Job") in services
+    assert Service("service-1","Load Balanced Web Service") in services
+    assert Service("backend-1","Backend Service") in services
+    
+@mock_aws
+def test_list_services_by_type():
+    client = boto3.client("ssm", region_name="eu-west-2")
+
+    for param in MOCK_SERVICE_PARAMS:
+        client.put_parameter(**param)
+
+    service_repository = ServiceRepository(ParameterStore(client, True))
+    service_type = ServiceType("Scheduled Job")
+    services = service_repository.list_services("my-app", "my-env", service_type)
+    assert len(services) == 1
+    assert Service("job-1","Scheduled Job") in services
