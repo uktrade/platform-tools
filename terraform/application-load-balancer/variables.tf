de6f0dd14a561cc973d89ba4fcbb18016cf08a12
andiempett <andi.empett@digital.trade.gov.uk>
2026-06-12 09:24:03 +0100

    remove unused cdn_domains_list terraform variable

--- terraform/application-load-balancer/variables.tf
+++ terraform/application-load-balancer/variables.tf
@@ -17,19 +17,11 @@ variable "dns_account_id" {
 variable "config" {
   type = object({
     domain_prefix                           = optional(string)
-    cdn_domains_list                        = optional(map(list(string)))
     additional_address_list                 = optional(list(string))
     slack_alert_channel_alb_secret_rotation = optional(string)
   })
 
   default = {}
-
-  validation {
-    condition = var.config.cdn_domains_list == null ? true : alltrue([
-      for k, v in var.config.cdn_domains_list : ((length(k) <= 63) && (length(k) >= 3))
-    ])
-    error_message = "Items in cdn_domains_list should be between 3 and 63 characters long."
-  }
 }
 
 variable "service_deployment_mode" {
