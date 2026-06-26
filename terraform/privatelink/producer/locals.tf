4bd6c8f05812a0eadb04d649f75af8464fd9f22e
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2026-06-26 16:37:54 +0100

    update local reference and update output format

--- terraform/privatelink/producer/locals.tf
+++ terraform/privatelink/producer/locals.tf
@@ -2,9 +2,9 @@ locals {
   tags = {
     managed-by     = "DBT Platform - Terraform"
     Name           = var.name
-    remote-vpc     = var.config.consumer_vpc_name
-    remote-account = var.config.consumer_account_id
-    application    = var.config.consumer_application
-    environment    = var.config.consumer_environment
+    remote-vpc     = var.config.producer_vpc_name
+    remote-account = var.config.producer_account_id
+    application    = var.config.producer_application
+    environment    = var.config.producer_environment
   }
 }
