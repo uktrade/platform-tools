e56b91955f9ef50cc8f5cd3e37ff590e7f5f8d9e
SamW94 <sam.wild@digital.trade.gov.uk>
2026-08-17 12:32:47 +0000

    feat: Tighten key policy permissions (SR-4918)
    Signed-off-by: DBT pre-commit check

--- terraform/statefile-backend/main.tf
+++ terraform/statefile-backend/main.tf
@@ -64,7 +64,7 @@ resource "aws_kms_key" "terraform-bucket-key" {
         "Sid" : "GHARolePermissions",
         "Effect" : "Allow",
         "Principal" : {
-          "AWS" : "arn:aws:sts::*:assumed-role/github-oidc-terraform-platform-repo-role-*/GitHubActions"
+          "AWS" : "arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/github-oidc-terraform-platform-repo-role-${var.aws_account_name}/GitHubActions"
         },
         "Action" : [
           "kms:Get*",
