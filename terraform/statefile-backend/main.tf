c2a6b0e449dd3d10b577d6323db45517c8dc1b39
SamW94 <sam.wild@digital.trade.gov.uk>
2026-08-17 14:00:49 +0000

    feat: Grant permissions to GHA roles for KMS key management (2) (SR-4918)
    Signed-off-by: DBT pre-commit check

--- terraform/statefile-backend/main.tf
+++ terraform/statefile-backend/main.tf
@@ -64,7 +64,7 @@ resource "aws_kms_key" "terraform-bucket-key" {
         "Sid" : "GHARolePermissions",
         "Effect" : "Allow",
         "Principal" : {
-          "AWS" : "arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/github-oidc-terraform-platform-repo-role-${var.aws_account_name}/GitHubActions"
+          "AWS" : "arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/github-oidc-terraform-platform-repo-role-*/GitHubActions"
         },
         "Action" : [
           "kms:Get*",
