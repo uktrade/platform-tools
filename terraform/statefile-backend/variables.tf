61edaee02629de3fdee685ff9198f3f9c3ff907b
Sam Wild <114504758+SamW94@users.noreply.github.com>
2026-08-19 07:23:17 +0100

    feat: Grant permissions to GHA roles for KMS key management (SR-4918) (#1620)
    Signed-off-by: DBT pre-commit check

--- terraform/statefile-backend/variables.tf
+++ terraform/statefile-backend/variables.tf
@@ -8,3 +8,9 @@ variable "custom_key_policy" {
   type        = string
   description = "Used to override the default key policy on the KMS key that encrypts the state file in cases it is required."
 }
+
+variable "key_policy_role_name_suffix" {
+  default     = null
+  type        = string
+  description = "Used to override the aws_account_name variable where it does not match the OIDC role name suffix."
+}
