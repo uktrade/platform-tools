bc2abb25f107eb7799ab6467b7aa2c4a7d11916e
David Glover <david.glover@digital.trade.gov.uk>
2026-08-27 17:16:40 +0100

    chore: Pre-commit test 2 (DBTP-2286)
    Signed-off-by: DBT pre-commit check

--- dbt_platform_helper/providers/terraform_manifest.py
+++ dbt_platform_helper/providers/terraform_manifest.py
@@ -287,7 +287,7 @@ class TerraformManifestProvider:
                     "region": "eu-west-2",
                     "encrypt": True,
                     "kms_key_id": f"alias/terraform-platform-state-s3-key-{account}",
-                    "dynamodb_table": f"terraform-platform-lockdb2-{account}",
+                    "dynamodb_table": f"terraform-platform-lockdb3-{account}",
                 }
             },
             "required_providers": {
