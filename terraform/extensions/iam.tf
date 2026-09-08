64b9cebfc3907af2bb78291f89e2acea1a92bcc3
David Glover <david.glover@digital.trade.gov.uk>
2026-09-08 16:26:46 +0100

    chore: Add s3 deletobject perms to codebase pipeline IAM role for tf state lock (DBTP-2286)

--- terraform/extensions/iam.tf
+++ terraform/extensions/iam.tf
@@ -461,7 +461,8 @@ data "aws_iam_policy_document" "state_bucket_access" {
     actions = [
       "s3:ListBucket",
       "s3:GetObject",
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:DeleteObject"
     ]
     resources = [
       data.aws_s3_bucket.state_bucket.arn,
