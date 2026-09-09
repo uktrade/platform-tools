66c3c9a6b3708b49ae94416caf4201678c877d8d
BenFielder <ben.fielder@businessandtrade.gov.uk>
2026-09-09 09:44:51 +0100

    feat: Remove old IAM role (DBTP-3408)

--- terraform/codebase-pipelines/ecr.tf
+++ terraform/codebase-pipelines/ecr.tf
@@ -51,7 +51,6 @@ data "aws_iam_policy_document" "ecr_policy" {
       values = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecr-housekeeping-role",
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-platform-image-build",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-repo-role" #TODO - Remove once all BYOD/scheduled job image build workflows no longer use this IAM role
       ]
     }
   }
