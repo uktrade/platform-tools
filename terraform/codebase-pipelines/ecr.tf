cc2befad24b7c93d118f9b0c271f7a02bda044ea
BenFielder <ben.fielder@businessandtrade.gov.uk>
2026-09-10 09:47:24 +0100

    feat: Remove old IAM role (DBTP-3408) (#1655)
    Signed-off-by: DBT pre-commit check

--- terraform/codebase-pipelines/ecr.tf
+++ terraform/codebase-pipelines/ecr.tf
@@ -51,7 +51,6 @@ data "aws_iam_policy_document" "ecr_policy" {
       values = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecr-housekeeping-role",
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-platform-image-build",
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-repo-role" #TODO - Remove once all BYOD/scheduled job image build workflows no longer use this IAM role
       ]
     }
   }
@@ -117,17 +116,10 @@ data "aws_iam_policy_document" "ecr_policy" {
       condition {
         test     = "ArnLike"
         variable = "aws:PrincipalArn"
-        values = concat(
-          flatten([
-            for id in local.deploy_account_ids : [
-              "arn:aws:iam::${id}:role/github-oidc-${var.application}-repo-role" # TODO: Remove once all BYOD/scheduled job image build workflows no longer use this IAM role
-            ]
-          ]),
-          [
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_AdministratorAccess_*",
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-platform-image-build"
-          ]
-        )
+        values = [
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_AdministratorAccess_*",
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-platform-image-build"
+        ]
       }
     }
   }
@@ -152,17 +144,10 @@ data "aws_iam_policy_document" "ecr_policy" {
       condition {
         test     = "ArnNotLike"
         variable = "aws:PrincipalArn"
-        values = concat(
-          flatten([
-            for id in local.deploy_account_ids : [
-              "arn:aws:iam::${id}:role/github-oidc-${var.application}-repo-role" # TODO: Remove once all BYOD/scheduled job image build workflows no longer use this IAM role
-            ]
-          ]),
-          [
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_AdministratorAccess_*",
-            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-platform-image-build"
-          ]
-        )
+        values = [
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_AdministratorAccess_*",
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-oidc-${var.application}-platform-image-build"
+        ]
       }
     }
   }
